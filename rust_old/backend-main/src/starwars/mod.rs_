afa8de8cb791f8f877ed4646a73a80a8e89e676e
Oyelowo Oyedayo <31687368+Oyelowo@users.noreply.github.com>
2021-11-13 18:13:42 +0200

    Remove old package

--- rust_old/backend-main/src/starwars/mod.rs_
+++
@@ -1,2 +0,0 @@
-pub mod resolvers;
-pub mod models;
--- rust_old/backend-second/src/main.rs
+++
@@ -1,7 +0,0 @@
-use common::{self, packages};
-
-fn main() {
-    packages::local_function();
-    common::get_shared_function();
-    println!("Hello, world!");
-}
