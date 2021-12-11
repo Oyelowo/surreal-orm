0198a79996371e5eb42dcb3fdf2aebf4f8f92f0a
Oyelowo Oyedayo <31687368+Oyelowo@users.noreply.github.com>
2021-12-11 20:55:04 +0200

    Scope packages to organization name - Oyelowo

--- my-turborepo/packages/ui/components/HelloWorld.test.tsx
+++
@@ -1,9 +0,0 @@
-import { HelloWorld } from "./HelloWorld";
-
-import { render, screen } from "@testing-library/react"; // (or /dom, /vue, ...)
-
-test("should show login form", () => {
-  render(<HelloWorld />);
-  const helloScreen = screen.getByText("Hello from the other side. I am Oyelowo");
-  // Events and assertions...
-});
