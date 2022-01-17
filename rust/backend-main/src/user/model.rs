0d0a4eeeedd44470600d83a8cb09bbefcfd00d71
Oyelowo Oyedayo <31687368+Oyelowo@users.noreply.github.com>
2022-01-17 08:54:18 +0200

    Add nested model to user

--- rust/backend-main/src/user/model.rs
+++ rust/backend-main/src/user/model.rs
@@ -1,6 +1,8 @@
 use async_graphql::*;
 
+use mongodb::Database;
 use serde::{Deserialize, Serialize};
+use futures::stream::StreamExt;
 use typed_builder::TypedBuilder;
 use validator::Validate;
 use wither::{
@@ -8,11 +10,25 @@ use wither::{
     prelude::Model,
 };
 
+
 #[derive(
     Model, SimpleObject, InputObject, Clone, Serialize, Deserialize, TypedBuilder, Validate, Debug,
 )]
 #[serde(rename_all = "camelCase")]
 #[graphql(input_name = "UserInput")]
+pub struct Book {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    #[builder(default)]
+    pub id: Option<ObjectId>,
+    pub author_id: ObjectId,
+    pub title: String,
+}
+
+#[derive(Model, SimpleObject, Serialize, Deserialize, TypedBuilder, Validate, Debug)]
+// #[derive(InputObject)]
+#[serde(rename_all = "camelCase")]
+// #[graphql(input_name = "UserInput")]
+#[graphql(complex)]
 #[model(index(keys = r#"doc!{"email": 1}"#, options = r#"doc!{"unique": true}"#))]
 pub struct User {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
@@ -36,14 +52,31 @@ pub struct User {
     pub social_media: Vec<String>,
 }
 
-pub type UserInput = User;
-// #[derive(InputObject, TypedBuilder)]
-// pub struct UserInput {
-//     pub last_name: String,
-//     pub first_name: String,
-//     pub email: String,
-//     pub age: u8,
-// }
+#[ComplexObject]
+impl User {
+    async fn books(&self, ctx: &Context<'_>) -> anyhow::Result<Vec<Book>> {
+        let db = ctx.data_unchecked::<Database>();
+        let mut cursor = Book::find(db, doc! {"_id": self.id}, None).await?;
+
+        let mut books = vec![];
+        while let Some(user) = cursor.next().await {
+            books.push(user.unwrap());
+        }
+
+        Ok(books)
+    }
+}
+
+
+// pub type UserInput = User;
+#[derive(InputObject, TypedBuilder)]
+pub struct UserInput {
+    pub last_name: String,
+    pub first_name: String,
+    pub email: String,
+    pub social_media: Vec<String>,
+    pub age: u8,
+}
 
 /*
 
--- rust/backend-main/src/user/mutation_root.rs
+++ rust/backend-main/src/user/mutation_root.rs
@@ -14,16 +14,17 @@ impl UserMutationRoot {
         ctx: &Context<'_>,
         #[graphql(desc = "user data")] user_input: UserInput,
     ) -> anyhow::Result<User> {
-        user_input.validate()?;
+        // user_input.validate()?;
         let db = ctx.data_unchecked::<Database>();
-        // let mut user1 = User::builder()
-        //     .first_name(user_input.first_name.into())
-        //     .last_name(user_input.last_name.into())
-        //     .email(user_input.email.into())
-        //     .age(user_input.age)
-        //     .social_media(user_input.social_media)
-        //     .build();
-        let mut user = User { ..user_input };
+        let mut user = User::builder()
+            .first_name(user_input.first_name.into())
+            .last_name(user_input.last_name.into())
+            .email(user_input.email.into())
+            .age(user_input.age)
+            .social_media(user_input.social_media)
+            .build();
+        // let mut user = User { ..user_input };
+        user.validate()?;
 
         user.save(db, None).await?;
         Ok(user)
