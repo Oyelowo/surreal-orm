bb00f130a05f293c063a4ee74beb516b1c6c1590
Oyelowo Oyedayo <31687368+Oyelowo@users.noreply.github.com>
2022-01-27 23:16:53 +0200

    sqlx macro Works

--- rust/backend-main-pg/src/main.rs
+++ rust/backend-main-pg/src/main.rs
@@ -7,49 +7,49 @@ use chrono::Utc;
 // use sqlx::{postgres::PgRow, query, Executor, Row};
 // use async_std::sync::RwLock;
 use dotenv::dotenv;
-use uuid::Uuid;
 use serde::{Deserialize, Serialize};
-use std::collections::hash_map::{Entry, HashMap};
-use std::sync::Arc;
 use sqlx::Pool;
+use sqlx::{postgres::PgPool, query, query_as};
+use std::collections::hash_map::{Entry, HashMap};
 use std::env;
-use sqlx::{query,postgres::PgPool,query_as};
-
-
+use std::sync::Arc;
+use uuid::Uuid;
 
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
-     //let Configs { application, .. } = Configs::init();
+    //let Configs { application, .. } = Configs::init();
     // let app_url = &application.get_url();
 
     // println!("Playground: {}", app_url);
-    dotenv().ok();
 
-     let conn_str =
+    dotenv().ok();
+    let conn_str =
         std::env::var("DATABASE_URL").expect("Env var DATABASE_URL is required for this example.");
 
     let pool = sqlx::PgPool::connect(&conn_str).await?;
     let mut transaction = pool.begin().await?;
 
-        let test_id = Uuid::new_v4();
+    let test_id = Uuid::new_v4();
 
-   let k = query_as!(User,
+    let k = query_as!(
+        User,
         r#"INSERT INTO users (id, first_name, last_name, email) VALUES 
         ( $1, $2, $3, $4) returning id, first_name, last_name, email
         "#,
-        test_id, "oyelowo".to_string(), "oyedayo".to_string(), "oye@gmail.com".to_string()
+        test_id,
+        "oyelowo",
+        "oyedayo",
+        "oyej2@gmail.com"
     )
     .fetch_one(&pool)
     .await?;
 
-
     // check that inserted todo can be fetched
-    let n = query!("SELECT first_name FROM users WHERE id = $1", test_id)
-
+    let n = query_as!(User, "SELECT * FROM users WHERE id = $1", test_id)
         .fetch_one(&mut transaction)
         .await?;
 
-        println!("#TRE{:?}",n);
+    println!("#TRE{:?}", n);
 
     transaction.rollback();
 
@@ -58,27 +58,23 @@ async fn main() -> anyhow::Result<()> {
     //     .fetch_one(&pool)
     //     .await;
 
-
     // let schema = GraphQlApp::setup()
     //     .await
     //     .expect("Problem setting up graphql");
 
     HttpServer::new(move || {
         App::new()
-            // .app_data(web::Data::new(schema.clone()))
-            // .service(web::resource("/").guard(guard::Post()).to(index))
-            // .service(web::resource("/").guard(guard::Get()).to(index_playground))
+        // .app_data(web::Data::new(schema.clone()))
+        // .service(web::resource("/").guard(guard::Post()).to(index))
+        // .service(web::resource("/").guard(guard::Get()).to(index_playground))
     })
     .bind("localhost:8000")?
     .run()
     .await?;
 
-
     println!("gfg");
     Ok(())
 }
-
-
 
 #[derive(sqlx::FromRow, Debug, Clone)]
 struct User {
