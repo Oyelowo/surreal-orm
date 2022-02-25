2199cf6022b51e13c58b21ad66ab8d65f137f4e2
Oyelowo Oyedayo <31687368+Oyelowo@users.noreply.github.com>
2022-02-25 15:07:12 +0200

    allow all origin temporarily

--- rust/graphql-mongo/src/main.rs
+++ rust/graphql-mongo/src/main.rs
@@ -24,6 +24,7 @@ async fn main() -> anyhow::Result<()> {
             // .allowed_origin("http://localhost:8000/")
             // .allowed_origin_fn(|origin, _req_head| origin.as_bytes().ends_with(b".localhost:3001"))
             // .allowed_origin_fn(|origin, _req_head| origin.as_bytes().ends_with(b".localhost:8000"))
+            // .send_wildcard()
             .allow_any_origin() // FIXME: // remove after testing
             .allowed_methods(vec!["GET", "POST"])
             .allowed_headers(vec![/* http::header::AUTHORIZATION, */ http::header::ACCEPT])
