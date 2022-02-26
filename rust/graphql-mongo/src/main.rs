872b680de1bdb23d2b93dba7a8eeb49cabeca4e1
Oyelowo Oyedayo <31687368+Oyelowo@users.noreply.github.com>
2022-02-26 10:15:19 +0200

    Format

--- rust/graphql-mongo/src/main.rs
+++ rust/graphql-mongo/src/main.rs
@@ -14,12 +14,12 @@ async fn main() -> anyhow::Result<()> {
         .await
         .expect("Problem setting up graphql");
 
-// https://javascript.info/fetch-crossorigin#cors-for-safe-requests
-// https://docs.rs/actix-cors/0.5.4/actix_cors/index.html
-// http://www.ruanyifeng.com/blog/2016/04/cors.html
-// Cors short for Cross-Origin Resource Sharing.
+    // https://javascript.info/fetch-crossorigin#cors-for-safe-requests
+    // https://docs.rs/actix-cors/0.5.4/actix_cors/index.html
+    // http://www.ruanyifeng.com/blog/2016/04/cors.html
+    // Cors short for Cross-Origin Resource Sharing.
     HttpServer::new(move || {
-        let cors = Cors::default()  // allowed_origin return access-control-allow-origin: * by default
+        let cors = Cors::default() // allowed_origin return access-control-allow-origin: * by default
             // .allowed_origin("http://localhost:3001/")
             // .allowed_origin("http://localhost:8000/")
             // .allowed_origin_fn(|origin, _req_head| origin.as_bytes().ends_with(b".localhost:3001"))
@@ -27,7 +27,9 @@ async fn main() -> anyhow::Result<()> {
             // .send_wildcard()
             .allow_any_origin() // FIXME: // remove after testing
             .allowed_methods(vec!["GET", "POST"])
-            .allowed_headers(vec![/* http::header::AUTHORIZATION, */ http::header::ACCEPT])
+            .allowed_headers(vec![
+                /* http::header::AUTHORIZATION, */ http::header::ACCEPT,
+            ])
             .allowed_header(http::header::CONTENT_TYPE)
             .max_age(3600);
 
