07dc49906b5c365f64785a3ec008c996bba0bb47
oyelowo <oyelowooyedayo@gmail.com>
2022-03-28 12:15:33 +0300

    Update workflows

--- kubernetes/resources/shared/validations.ts
+++ kubernetes/resources/shared/validations.ts
@@ -2,10 +2,11 @@ import * as z from "zod";
 
 const environmentVariablesValidator = z.object({
   // TAG_REACT_WEB: z.string().nonempty().or(z.undefined()),
-  TAG_REACT_WEB: z.string().nonempty(),
-  TAG_GRAPHQL_MONGO: z.string().nonempty(),
-  TAG_GRPC_MONGO: z.string().nonempty(),
-  TAG_GRAPHQL_POSTGRES: z.string().nonempty(),
+  // This is provided fro, within the CI pipeline where the manifests are generated and pushed to the repo
+  IMAGE_TAG_REACT_WEB: z.string().nonempty(),
+  IMAGE_TAG_GRAPHQL_MONGO: z.string().nonempty(),
+  IMAGE_TAG_GRPC_MONGO: z.string().nonempty(),
+  IMAGE_TAG_GRAPHQL_POSTGRES: z.string().nonempty(),
 });
 
 export const environmentVariables = environmentVariablesValidator.parse(
