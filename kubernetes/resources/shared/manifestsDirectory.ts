276197594d13602feb884489a8e1c6f13144b251
oyelowo <oyelowooyedayo@gmail.com>
2022-04-04 22:17:54 +0300

    Put sealed secrets in separate app's folder

--- kubernetes/resources/shared/manifestsDirectory.ts
+++ kubernetes/resources/shared/manifestsDirectory.ts
@@ -10,14 +10,25 @@ export const getManifestsOutputDirectory = (environment: Environment) =>
   path.join("manifests", "generated", environment);
 
 const MANIFESTS_BASE_DIR_FOR_ENV = getManifestsOutputDirectory(ENVIRONMENT);
-export const APPLICATION_DIR = path.join(MANIFESTS_BASE_DIR_FOR_ENV, "applications");
 
-export const getPathToApplicationDir = (appName: AppName) => path.join(APPLICATION_DIR, appName);
-export const getPathToNonApplicationDir = (toolName: string) =>
-  path.join(MANIFESTS_BASE_DIR_FOR_ENV, toolName);
+// const APPLICATION_DIR = path.join(MANIFESTS_BASE_DIR_FOR_ENV, "applications");
 
-export const getSealedSecretsDirForEnv = () =>
-  path.join(MANIFESTS_BASE_DIR_FOR_ENV, "sealed-secrets");
+export const getPathToApplicationDirForEnv = (appName: AppName, environment: Environment) => {
+  return path.join(getManifestsOutputDirectory(environment), "applications", appName);
+};
+
+export const getPathToApplicationDir = (appName: AppName) => {
+  return getPathToApplicationDirForEnv(appName, ENVIRONMENT);
+};
+// export const getPathToApplicationDir = (appName: AppName) => path.join(APPLICATION_DIR, appName);
+
+export const getPathToNonApplicationDir = (toolName: string) => {
+  return path.join(MANIFESTS_BASE_DIR_FOR_ENV, toolName);
+};
+
+export const getSealedSecretsDirForEnv = () => {
+  return path.join(MANIFESTS_BASE_DIR_FOR_ENV, "sealed-secrets");
+};
 
 export const nameSpacesProvider = new k8s.Provider("render-namespaces", {
   renderYamlToDirectory: `${MANIFESTS_BASE_DIR_FOR_ENV}/namespaces`,
--- kubernetes/scripts/generateManifests.ts
+++ kubernetes/scripts/generateManifests.ts
@@ -1,8 +1,9 @@
+import { getPathToApplicationDirForEnv } from "./../resources/shared/manifestsDirectory";
 import { ImageTags } from "./../resources/shared/validations";
 import sh from "shelljs";
 import { Environment } from "../resources/shared/types/own-types";
 import { manifestsDirForEnv, ENVIRONMENT, ARGV } from "./bootstrap";
-import path from "path";
+import p from "path";
 
 /*
 GENERATE ALL KUBERNETES MANIFESTS USING PULUMI
@@ -22,22 +23,12 @@ export async function generateManifests({
   sh.rm("-rf", "./login");
   sh.mkdir("./login");
 
-  // We are temporarily renaming existing files before generating new ones,
-  // So that we can retain the sealed secrets which is not managed by pulumi
-  // but by our cli tool/kubeseal
-  const temporaryDirSuffixString = "-temporaryxxxxxxxxxxx";
-  const temporaryDir = `${manifestsDirForEnv}${temporaryDirSuffixString}`;
-  // Rename old dir
-  sh.mv(manifestsDirForEnv, temporaryDir);
-
   sh.exec("pulumi login file://login");
   sh.exec("export PULUMI_CONFIG_PASSPHRASE='' && pulumi stack init --stack dev");
 
-  // image tag. All apps share same tag for now
   // Pulumi needs some environment variables set for generating deployments with image tag
-  /* `export ${IMAGE_TAG_REACT_WEB}="${ARGV.t}" && \ `
-     `export ${IMAGE_TAG_REACT_WEB}="${ARGV.t}" && \ `
-     */
+  /* `export ${IMAGE_TAG_REACT_WEB}=tag-web export ${IMAGE_TAG_GRAPHQL_MONGO}=tag-mongo`
+   */
   const imageEnvVarSetterForPulumi = Object.entries(imageTags)
     .map(([k, v]) => `export ${k}=${v}`)
     .join(" ");
@@ -51,22 +42,11 @@ export async function generateManifests({
       `
   );
 
-  // Write them back
-  const sealedSecretsFilePathsForEnvTemporary = getFilePathsThatMatch({
-    contextDir: temporaryDir,
-    pattern: "sealed-secret-*ml",
-  });
-
-  for (const sealedSecretFilePath of sealedSecretsFilePathsForEnvTemporary) {
-    const tempDirname = path.dirname(sealedSecretFilePath);
-    // remove the suffix to form original directory name
-    const originalDir = tempDirname.split(temporaryDirSuffixString).join("");
-    const fileName = path.basename(sealedSecretFilePath);
-    const constructFilePath = (dirname: string) => path.join(dirname, fileName);
-    sh.cp(constructFilePath(tempDirname), constructFilePath(originalDir));
-  }
-
-  sh.rm("-rf", temporaryDir);
+  // // Write them back
+  // const sealedSecretsFilePathsForEnvTemporary = getFilePathsThatMatch({
+  //   contextDir: manifestsDirForEnv,
+  //   pattern: "sealed-secret-*ml",
+  // });
 
   regenerateSealedSecretsManifests({
     keepSecretInputs,
@@ -79,7 +59,7 @@ export async function generateManifests({
 import c from "chalk";
 import { clearUnsealedInputTsSecretFilesContents } from "../secretsManagement/setupSecrets";
 
-export const MANIFESTS_DIR = path.join(__dirname, "..", "manifests");
+export const MANIFESTS_DIR = p.join(__dirname, "..", "manifests");
 
 /* 
 GENERATE BITNAMI'S SEALED SECRET FROM PLAIN SECRETS MANIFESTS GENERATED USING PULUMI.
@@ -99,27 +79,29 @@ export async function regenerateSealedSecretsManifests({
   keepSecretOutputs,
   regenerateSealedSecrets,
 }: GenSealedSecretsProps) {
-  const contextDir = path.join(__dirname, "..", "manifests", "generated", environment);
+  const contextDir = p.join(__dirname, "..", "manifests", "generated", environment);
   const unsealedSecretsFilePathsForEnv = getFilePathsThatMatch({
     contextDir,
     pattern: "secret-*ml",
   });
 
-  for (const unsealedSecretPath of unsealedSecretsFilePathsForEnv) {
+  for (const unsealedSecretManifestPath of unsealedSecretsFilePathsForEnv) {
     if (regenerateSealedSecrets) {
-      const unsealedSecretDir = path.dirname(unsealedSecretPath);
-      // Secrets are prefixed with "secret-"
-      const unsealedSecretFileName = path.basename(unsealedSecretPath);
-      const sealedSecretFileName = `sealed-${unsealedSecretFileName}`;
-      const sealedSecretFilePath = path.join(unsealedSecretDir, sealedSecretFileName);
+      // The path format is: kubernetes/manifests/generated/production/applications/graphql-mongo/1-manifest
+      const appManifestsDir = p.dirname(unsealedSecretManifestPath);
+      const appBaseDir = p.join(appManifestsDir, "..");
+      const unsealedSecretResourceFileName = p.basename(unsealedSecretManifestPath);
+      const sealedSecretDirForApp = p.join(appBaseDir, "sealed-secrets");
+      const sealedSecretFilePath = p.join(sealedSecretDirForApp, `sealed-${unsealedSecretResourceFileName}`);
+
+      sh.mkdir(sealedSecretDirForApp);
 
       sh.echo(
-        c.blueBright(
-          `Generating sealed secret for ${unsealedSecretFileName} at ${unsealedSecretDir}`
-        )
+        c.blueBright(`Generating sealed secret for ${unsealedSecretResourceFileName} at ${appBaseDir}`)
       );
 
-      const kubeSeal = sh.exec(`kubeseal <${unsealedSecretPath} -o yaml >${sealedSecretFilePath}`, {
+      // TODO: Should I delete old sealed secrets before creating new ones?
+      const kubeSeal = sh.exec(`kubeseal < ${unsealedSecretManifestPath} -o yaml >${sealedSecretFilePath}`, {
         silent: true,
       });
 
@@ -130,14 +112,14 @@ export async function regenerateSealedSecretsManifests({
         return;
       }
 
-      sh.echo(c.greenBright("Successfully generated sealed secret at", unsealedSecretPath));
+      sh.echo(c.greenBright("Successfully generated sealed secret at", unsealedSecretManifestPath));
     }
 
-    sh.echo(c.blueBright(`Removing unsealed plain secret manifest ${unsealedSecretPath}`));
+    sh.echo(c.blueBright(`Removing unsealed plain secret manifest ${unsealedSecretManifestPath}`));
 
     // Delete unsealed plain secret if specified
     if (!keepSecretOutputs) {
-      sh.rm("-rf", unsealedSecretPath);
+      sh.rm("-rf", unsealedSecretManifestPath);
     }
 
     if (!keepSecretInputs) {
@@ -145,14 +127,8 @@ export async function regenerateSealedSecretsManifests({
     }
   }
 }
-// const UNSEALED_SECRETS_MANIFESTS_FOR_ENV = sh.exec(`find ${contextDir} -name "secret-*ml"`, {
-export function getFilePathsThatMatch({
-  contextDir,
-  pattern,
-}: {
-  contextDir: string;
-  pattern: string;
-}) {
+
+export function getFilePathsThatMatch({ contextDir, pattern }: { contextDir: string; pattern: string }) {
   const UNSEALED_SECRETS_MANIFESTS_FOR_ENV = sh.exec(`find ${contextDir} -name "${pattern}"`, {
     silent: true,
   });
