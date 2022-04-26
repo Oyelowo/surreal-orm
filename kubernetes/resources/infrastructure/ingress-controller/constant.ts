af0332fd2131ee7a3e53fcb933f6a84d4f7daf10
oyelowo <oyelowooyedayo@gmail.com>
2022-04-26 03:54:50 +0300

    fix argocd url

--- kubernetes/resources/infrastructure/ingress-controller/constant.ts
+++ kubernetes/resources/infrastructure/ingress-controller/constant.ts
@@ -1,4 +1,4 @@
 export const DOMAIN_NAME_BASE = "oyelowo.dev";
-export const DOMAIN_NAME_SUB_ARGOCD = "oyelowo.dev";
+export const DOMAIN_NAME_SUB_ARGOCD = `argocd.${DOMAIN_NAME_BASE}`;
 const DNS_NAMES = ["oyelowo.dev"];
 
