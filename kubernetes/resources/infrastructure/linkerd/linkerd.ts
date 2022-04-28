56eec2ddf779bb3b6673308ccaaaca4434fedb30
oyelowo <oyelowooyedayo@gmail.com>
2022-04-28 20:33:24 +0300

    Some more fixes

--- kubernetes/resources/infrastructure/linkerd/linkerd.ts
+++ kubernetes/resources/infrastructure/linkerd/linkerd.ts
@@ -16,12 +16,25 @@ const Linkerd2Provider = new k8s.Provider(linkerd2Dir, {
   renderYamlToDirectory: linkerd2Dir,
 });
 
+/* 
+ --set-file identityTrustAnchorsPEM=ca.crt \
+  --set-file identity.issuer.tls.crtPEM=issuer.crt \
+  --set-file identity.issuer.tls.keyPEM=issuer.key \
+*/
 const Linkerd2Values: DeepPartial<Linkerd2HelmValues> = {
-
-
+  identityTrustAnchorsPEM: "ca",
+  identity: {
+    issuer: {
+      tls: {
+        crtPEM: "",
+        keyPEM: "",
+      },
+    }
+  },
+  // cniEnabled: true
 };
 
-export const linkerd2 = new k8s.helm.v3.Chart(
+export const linkerd = new k8s.helm.v3.Chart(
   linkerd2Name,
   {
     chart: helmChartsMetadata.linked2.linkerd.chart,
