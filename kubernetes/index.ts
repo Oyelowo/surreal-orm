30a8e65911d2998a9d8b68eb2cb7a155061333c4
oyelowo <oyelowooyedayo@gmail.com>
2022-05-17 10:15:41 +0300

    minor cleanup

--- kubernetes/index.ts
+++ kubernetes/index.ts
@@ -1,20 +1,14 @@
-// export * from "./resources/shared/namespaces";
-// export * from "./secretsManagement/setupSecrets";
-
-// import { setupUnsealedSecretFiles } from "./secretsManagement/setupSecrets";
-// setupUnsealedSecretFiles();
-
-// export * from "./resources/shared/cluster";
-
-// Ingress controller and ingress rule
+// INFRASTRUCTURE
 export * from './resources/namespaces'
 export * from './resources/infrastructure/ingress'
 export * from './resources/infrastructure/sealed-secrets'
 export * from './resources/infrastructure/argocd'
 export * from './resources/infrastructure/cert-manager'
 export * from './resources/infrastructure/linkerd'
-// export * from "./resources/infrastructure/linkerdViz";
 
+
+
+// SERVICES
 // Rust server backend with support for graphql, mongodb and postgres
 // RUST WORKSPACE APPS
 export * from './resources/services/graphql-mongo'
@@ -25,8 +19,3 @@ export * from './resources/services/graphql-mongo'
 // TYPESCRIPT WORKSPACE APPS
 // Web app. Nextjs with client and server support. Server is at /api
 export * from './resources/services/react-web'
-
-// export * from "./resources";
-
-// Turn this on if you want secret files values to be empty e.g within ./secretsManagement/secrets-unsealed-development.ts
-// clearSecretFilesContents()
