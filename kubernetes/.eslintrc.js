b04ebb9fc73523a42fe9b122e989ea3b9fe93545
oyelowo <oyelowooyedayo@gmail.com>
2022-05-18 06:41:12 +0300

    Configure typescript for eslint

--- kubernetes/.eslintrc.js
+++ kubernetes/.eslintrc.js
@@ -1,16 +1,21 @@
 module.exports = {
     env: {
         es6: true,
-        browser: true,
+        browser: false,
+        node: true,
         es2021: true,
     },
-    extends: ['standard', 'prettier'],
+    extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
     parserOptions: {
         ecmaVersion: 12,
         sourceType: 'module',
+        parser: '@typescript-eslint/parser',
     },
     rules: {
-        'prettier/prettier': 'error',
+        // 'prettier/prettier': 'error',
+        indent: 'off',
+        '@typescript-eslint/indent': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
     },
-    plugins: ['prettier'],
+    plugins: ['prettier', '@typescript-eslint'],
 };
