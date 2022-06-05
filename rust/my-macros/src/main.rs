044185e67f34d9afe7bc76cdd4b47213cd4a96d2
oyelowo <oyelowooyedayo@gmail.com>
2022-06-05 20:00:47 +0300

    Add tests for macros

--- rust/my-macros/src/main.rs
+++ rust/my-macros/src/main.rs
@@ -1,7 +1,7 @@
 // For testing macros made here
 
-use my_macros::{HelloMacro, MyTrait, KeyNamesGetter};
-use serde::{Serialize, Deserialize};
+use my_macros::{HelloMacro, KeyNamesGetter, MyTrait};
+use serde::{Deserialize, Serialize};
 // use serde::{Serialize, Deserialize};
 
 #[derive(HelloMacro)]
@@ -46,8 +46,9 @@ struct Make {
 }
 
 fn main() {
-
-    let ConsumingTypeKeyNames {  lowo_cool, lmsar,.. } = ConsumingType::get_field_names();
+    let ConsumingTypeKeyNames {
+        lowo_cool, lmsar, ..
+    } = ConsumingType::get_field_names();
 
     // println!("rerezzzzzzz{name_of_me}, {lmsar}")
     println!("rere{lowo_cool}, {lmsar}")
@@ -72,4 +73,78 @@ fn default() {
 fn getter() {
     assert_eq!(Bar::answer(), 20);
     assert!(Bar::level().contains("Low"));
+}
+
+#[test]
+fn keys_getter_1() {
+    #[derive(KeyNamesGetter, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Consumer {
+        #[serde(rename = "lowo_cool")]
+        pub name_of_me: String,
+
+        #[serde(rename = "age_count")]
+        pub age: u8,
+    }
+
+    let ConsumerKeyNames {
+        lowo_cool,
+        age_count,
+    } = Consumer::get_field_names();
+
+    assert_eq!(lowo_cool, "lowo_cool");
+    assert_eq!(age_count, "age_count");
+}
+
+#[test]
+fn keys_getter_4() {
+    #[derive(KeyNamesGetter, Serialize, Deserialize)]
+    #[key_getter(rename_all = "camelCase")]
+    pub struct Consumer {
+        pub name_of_me: String,
+
+        pub age: u8,
+    }
+
+    let ConsumerKeyNames { nameOfMe, age } = Consumer::get_field_names();
+
+    assert_eq!(nameOfMe, "nameOfMe");
+    assert_eq!(age, "age");
+}
+
+#[test]
+fn keys_getter_5() {
+    #[derive(KeyNamesGetter, Serialize, Deserialize)]
+    #[serde(rename_all = "snake_case")]
+    pub struct Consumer {
+        #[warn(non_snake_case)]
+        pub nameOfMe: String,
+
+        pub age: u8,
+    }
+
+    let ConsumerKeyNames { name_of_me, age } = Consumer::get_field_names();
+
+    assert_eq!(name_of_me, "name_of_me");
+    assert_eq!(age, "age");
+}
+
+#[test]
+fn keys_getter_6() {
+    #[derive(KeyNamesGetter, Serialize, Deserialize)]
+    #[serde(rename_all = "snake_case")]
+    pub struct Consumer {
+        #[warn(non_snake_case)]
+        pub name_of_me: String,
+
+        pub ageCount: u8,
+    }
+
+    let ConsumerKeyNames {
+        name_of_me,
+        age_count,
+    } = Consumer::get_field_names();
+
+    assert_eq!(name_of_me, "name_of_me");
+    assert_eq!(age_count, "age_count");
 }
