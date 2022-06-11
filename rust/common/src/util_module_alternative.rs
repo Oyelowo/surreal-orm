cea26f76a9d4d8a7942ed13a993c49d45d1400c3
oyelowo <oyelowooyedayo@gmail.com>
2022-06-11 15:29:20 +0300

    Some cleanups

--- rust/common/src/util_module_alternative.rs
+++
@@ -1,4 +0,0 @@
-// You can also name the module the name of the folder.
-// This is an alternative approach to using mod.rs to rexport modules
-// from folders.
-pub mod greeter_alt;
--- rust/common/src/utils/mod.rs
+++ rust/common/src/utils/mod.rs
@@ -1,23 +1,3 @@
 mod export_data;
-mod get_config;
-mod greet;
-pub mod maths;
 
 pub use export_data::*;
-pub use get_config::*;
-pub use greet::*;
-
-pub fn local_function() {
-    greet::good_morning();
-    local_indirect_access();
-    println!("called `my::function()`");
-}
-
-fn local_private_function() {
-    println!("called `my::private_function()`");
-}
-
-pub fn local_indirect_access() {
-    print!("called `my::indirect_access()`, that\n> ");
-    local_private_function();
-}
