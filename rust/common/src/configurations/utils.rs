72314707065a8e2abd6f3ea1c05cb81825783ec2
oyelowo <oyelowooyedayo@gmail.com>
2022-06-20 02:51:34 +0300

    Handle the error more idiomatically

--- rust/common/src/configurations/utils.rs
+++ rust/common/src/configurations/utils.rs
@@ -5,20 +5,14 @@ use serde::de::DeserializeOwned;
 pub fn get_env_vars_by_prefix<T: DeserializeOwned>(config_prefix: &str) -> T {
     envy::prefixed(config_prefix)
         .from_env::<T>()
+        .map_err(|e| match e {
+            envy::Error::MissingValue(value) => {
+                format!("{config_prefix}{}", value.to_ascii_uppercase())
+            }
+            envy::Error::Custom(e) => e,
+        })
         .unwrap_or_else(|e| {
-            // e.g if prefix is APP_ and error msg = "missing value for field port". ==> APP_PORT
-            let error_msg = e.to_string();
-            let env_variable = error_msg
-                .split(" ")
-                .last()
-                .expect("Couldn't get it")
-                .to_ascii_uppercase();
-                
-            let var_to_provide = format!("{config_prefix}{env_variable}");
-            log::error!(
-                "You are missing {var_to_provide}. Please provide as an environment variable.
-                Error {e:?}",
-            );
+            log::error!("You are missing {e}. Please provide it as an environment variable",);
             process::exit(1);
         })
 }
