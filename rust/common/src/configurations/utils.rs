78b3d65b1d685a8cad7ffd5a7f29a16cc5c248a8
oyelowo <oyelowooyedayo@gmail.com>
2022-06-20 02:43:42 +0300

    Improve environment variable getter error message

--- rust/common/src/configurations/utils.rs
+++ rust/common/src/configurations/utils.rs
@@ -6,9 +6,18 @@ pub fn get_env_vars_by_prefix<T: DeserializeOwned>(config_prefix: &str) -> T {
     envy::prefixed(config_prefix)
         .from_env::<T>()
         .unwrap_or_else(|e| {
+            // e.g if prefix is APP_ and error msg = "missing value for field port". ==> APP_PORT
+            let error_msg = e.to_string();
+            let env_variable = error_msg
+                .split(" ")
+                .last()
+                .expect("Couldn't get it")
+                .to_ascii_uppercase();
+                
+            let var_to_provide = format!("{config_prefix}{env_variable}");
             log::error!(
-                "problem with {config_prefix:?} environment variables(s). 
-                Check that the prefix is correctly spelt and the configs are complete. Error {e:?}"
+                "You are missing {var_to_provide}. Please provide as an environment variable.
+                Error {e:?}",
             );
             process::exit(1);
         })
