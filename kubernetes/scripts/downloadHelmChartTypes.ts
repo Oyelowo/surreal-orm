0d170b76e6de70080c86a666321f518d5cac1114
oyelowo <oyelowooyedayo@gmail.com>
2022-06-21 13:18:22 +0300

    Remove unncessary coment

--- kubernetes/scripts/downloadHelmChartTypes.ts
+++ kubernetes/scripts/downloadHelmChartTypes.ts
@@ -13,12 +13,10 @@ sh.exec(`mkdir -p ${helmChartsDir}`)
 Object.entries(helmChartsInfo).map(([repoName, repoValues]) => {
     const { repo: repoUrl } = repoValues;
     sh.echo(chalk.blueBright(`Syncing helm chart - ${repoName} from ${repoUrl}`));
-    
-    // helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
+
     sh.exec(`helm repo add ${repoName} ${repoUrl}`);
 
     Object.values(repoValues.charts).forEach(({ chart, version }) => {
-        // helm show values bitnami/mongodb --version 12
         let valuesJson = sh.exec(`helm show values ${repoName}/${chart} --version ${version} | yq -o=json -I=0`, { silent: true }).stdout;
 
         const typeFileName = _.camelCase(`${chart}${_.capitalize(repoName)}`);
@@ -31,7 +29,6 @@ Object.entries(helmChartsInfo).map(([repoName, repoValues]) => {
         }).join("\n");
 
         sh.exec(`echo ${JSON.stringify(tsDec)} > ${helmChartsDir}/${typeFileName}.ts`)
-
     });
 })
 
