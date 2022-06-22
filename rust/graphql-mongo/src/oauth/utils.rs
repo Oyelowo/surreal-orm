7dd11eff158ce6987d70710ba6d379a8befe653b
oyelowo <oyelowooyedayo@gmail.com>
2022-06-22 09:49:53 +0300

    Update uri path

--- rust/graphql-mongo/src/oauth/utils.rs
+++ rust/graphql-mongo/src/oauth/utils.rs
@@ -17,7 +17,7 @@ use url::Url;
 use crate::app::user::{OauthProvider, User};
 
 // TODO: Get the url as an environment variable. Should be EXTERNAL_URL
-pub(crate) const REDIRECT_URL: &str = "http://localhost:8000/api/oauth/callback";
+pub(crate) const REDIRECT_URL: &str = "http://localhost:8000/oauth/callback";
 
 #[derive(Debug, thiserror::Error)]
 pub(crate) enum OauthError {
