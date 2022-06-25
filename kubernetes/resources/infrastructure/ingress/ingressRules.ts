18ae10dc840ebc076be30856cdf16b3ddf90aa52
oyelowo <oyelowooyedayo@gmail.com>
2022-06-26 02:15:11 +0300

    Update ingress rules

--- kubernetes/resources/infrastructure/ingress/ingressRules.ts
+++ kubernetes/resources/infrastructure/ingress/ingressRules.ts
@@ -20,6 +20,7 @@ const name = 'oyelowo-ingress';
 
 
 const getHosts = (environemnt: Environment) => Object.values(hosts[environemnt]) as string[];
+const isLocal = ENVIRONMENT === "local";
 
 type CertManagerAnnotations = {
     // NOTE: Make sure you specify the right one, if using cluster-issuer, user cluster-issuer annotations, otherwise, use mere issuer
@@ -30,21 +31,19 @@ type CertManagerAnnotations = {
 
 type IngressAnnotations = NginxConfiguration & CertManagerAnnotations;
 export const annotations: Partial<IngressAnnotations> = {
-    // 'nginx.ingress.kubernetes.io/ssl-redirect': 'false',
-    // 'nginx.ingress.kubernetes.io/use-regex': 'true',
-    // 'nginx.ingress.kubernetes.io/rewrite-target': "/*",
+    'nginx.ingress.kubernetes.io/ssl-redirect': isLocal ? 'false' : 'true',
+    'nginx.ingress.kubernetes.io/use-regex': 'true',
     'cert-manager.io/cluster-issuer': CLUSTER_ISSUER_NAME,
-    // 'nginx.ingress.kubernetes.io/cors-allow-credentials': 'true',
-    // 'nginx.ingress.kubernetes.io/cors-allow-origin': 'http://localhost:8080',
-    // 'nginx.ingress.kubernetes.io/enable-cors': 'false',
+    'nginx.ingress.kubernetes.io/enable-cors': isLocal ? "false" : "true",
 };
+
 export const appIngress = new k8s.networking.v1.Ingress(
     name,
     {
         metadata: {
             name,
             namespace: namespaceNames.applications,
-            annotations: annotations as any,
+            annotations: annotations as Record<string, string>,
         },
         spec: {
             ingressClassName: INGRESS_CLASSNAME_NGINX,
@@ -56,12 +55,28 @@ export const appIngress = new k8s.networking.v1.Ingress(
             ],
             rules: [
                 {
+                    // NOTE: The convention I intend to follow is to see each service as a Unit(i.e Frontend and backend)
+                    // 1:1 mapping i.e 1 domain/subdomain =>  Unit(i.e Frontend and backend)
+                    // Frontent served at base route and come last, while backend served at /api and come first to match specifically.
+                    // e.g if you have two services: 
+                    // For service a... servicea.mydomain.com   => Frontend(serivea.mydomain.com), Backend(servicea.mydomain.com)
+                    // For service b... servicea.mydomain.com   => Frontend(seriveb.mydomain.com), Backend(serviceb.mydomain.com)
                     host: hosts[ENVIRONMENT].base,
                     http: {
                         paths: [
+                            // Put the specific Path first before the base path below where the frontend is served
                             {
                                 pathType: 'Prefix',
-                                // path: "/?(.*)",
+                                path: '/api ',
+                                backend: {
+                                    service: {
+                                        name: graphqlMongoSettings.metadata.name,
+                                        port: { number: Number(graphqlMongoSettings.envVars.APP_PORT) },
+                                    },
+                                },
+                            },
+                            {
+                                pathType: 'Prefix',
                                 path: '/',
                                 backend: {
                                     service: {
@@ -70,22 +85,11 @@ export const appIngress = new k8s.networking.v1.Ingress(
                                     },
                                 },
                             },
-                            {
-                                pathType: 'Prefix',
-                                // path: "/?(.*)",
-                                path: '/graphql ',
-                                backend: {
-                                    service: {
-                                        name: graphqlMongoSettings.metadata.name,
-                                        port: { number: Number(graphqlMongoSettings.envVars.APP_PORT) },
-                                    },
-                                },
-                            },
                         ],
                     },
                 },
                 // {
-                //     host: hosts[ENVIRONMENT].api,
+                //     host: hosts[ENVIRONMENT].another-service,
                 //     http: {
                 //         paths: [
                 //             {
@@ -93,8 +97,8 @@ export const appIngress = new k8s.networking.v1.Ingress(
                 //                 path: '/',
                 //                 backend: {
                 //                     service: {
-                //                         name: graphqlMongoSettings.metadata.name,
-                //                         port: { number: Number(graphqlMongoSettings.envVars.APP_PORT) },
+                //                         name: ..,
+                //                         port: ..,
                 //                     },
                 //                 },
                 //             },
