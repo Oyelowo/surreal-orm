fe57b10afaa2efe7a46e140b4621e4201ae99893
oyelowo <oyelowooyedayo@gmail.com>
2022-07-27 20:31:35 +0300

    Fix sync configs

--- kubernetes/scripts/syncConfigs.ts
+++ kubernetes/scripts/syncConfigs.ts
@@ -4,11 +4,18 @@ import { PlainSecretJsonConfig } from './utils/plainSecretJsonConfig';
 import { syncEtcHostsWithCustomHosts } from './utils/syncEtcHostsWithCustomHosts';
 import { syncHelmChartTypesDeclarations } from './utils/syncHelmChartTypesDeclarations';
 
-syncEtcHostsWithCustomHosts();
-syncHelmChartTypesDeclarations();
+async function main() {
+    syncEtcHostsWithCustomHosts();
+    syncHelmChartTypesDeclarations();
 
-PlainSecretJsonConfig.syncAll();
+    PlainSecretJsonConfig.syncAll();
 
-// Use local manifests to syn/generate new CRD codes
-const crds = new KubeObject('local').getOfAKind('CustomResourceDefinition');
-syncCrdsCode(crds);
+    
+    // Use local manifests to syn/generate new CRD codes
+    const kubeObjectInstance = new KubeObject('local')
+    await kubeObjectInstance.generateManifests()
+    const crds = kubeObjectInstance.getOfAKind('CustomResourceDefinition');
+    syncCrdsCode(crds);
+}
+
+main().catch(e=> `problem syncing configs. Error: ${e}`)
