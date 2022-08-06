fd4e5946dbeb3bc5f7feb78aff6dfe2070c934f6
oyelowo <oyelowooyedayo@gmail.com>
2022-08-06 14:04:40 +0300

    Reuse tpp level await for getting port

--- kubernetes/resources/infrastructure/ingress/hosts.ts
+++ kubernetes/resources/infrastructure/ingress/hosts.ts
@@ -1,14 +1,14 @@
 import { Environment } from '../../types/own-types.js';
 import { DOMAIN_NAME_BASE } from './constant.js';
-// import getPort, { portNumbers } from 'get-port';
+import getPort, { portNumbers } from 'get-port';
 
 // type DomainBase = 'oyelowo.local' | typeof DOMAIN_NAME_BASE;
 type DomainBase = 'localhost' | typeof DOMAIN_NAME_BASE;
 
 // Incrementally find next available port from 8080 for exposing ingress,
 // otherwise fall back to a random port
-export const INGRESS_EXTERNAL_PORT_LOCAL = 8080;
-// export const INGRESS_EXTERNAL_PORT_LOCAL = await getPort({ port: portNumbers(8080, 8200) });
+// export const INGRESS_EXTERNAL_PORT_LOCAL = 8080;
+export const INGRESS_EXTERNAL_PORT_LOCAL = await getPort({ port: portNumbers(8080, 8200) });
 
 interface Hosts {
     base: DomainBase;
