d6396d113fde0a626166ca777774af30fdac0187
oyelowo <oyelowooyedayo@gmail.com>
2022-08-11 19:26:54 +0300

    SWC-jest works

--- kubernetes/jest.config.cjs
+++ kubernetes/jest.config.cjs
@@ -1,15 +1,20 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-    preset: 'ts-jest/presets/default-esm', // or other ESM presets
-    globals: {
-        'ts-jest': {
-            useESM: true,
-            diagnostics: false,
-        },
+    transform: {
+        '^.+\\.(t|j)sx?$': ['@swc/jest'],
     },
+    extensionsToTreatAsEsm: ['.ts', '.tsx'],
     moduleNameMapper: {
         '^(\\.{1,2}/.*)\\.js$': '$1',
         '#(.*)': '<rootDir>/node_modules/$1',
     },
-    testEnvironment: 'node',
+    // ts-jest config
+    // preset: 'ts-jest/presets/default-esm', // or other ESM presets
+    // globals: {
+    //     'ts-jest': {
+    //         useESM: true,
+    //         diagnostics: false,
+    //     },
+    // },
+    // testEnvironment: 'node',
 };
