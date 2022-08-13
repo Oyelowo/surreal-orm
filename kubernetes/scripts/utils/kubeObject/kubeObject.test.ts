3f6d6a19e4c40d3bd7f0f48552d88ebc91dbb545
oyelowo <oyelowooyedayo@gmail.com>
2022-08-13 23:22:07 +0300

    Update tests

--- kubernetes/scripts/utils/kubeObject/kubeObject.test.ts
+++ kubernetes/scripts/utils/kubeObject/kubeObject.test.ts
@@ -1,3 +1,4 @@
+import sh from 'shelljs';
 import { getMainBaseDir } from './../../../src/resources/shared/directoriesManager';
 import path from 'node:path';
 import { KubeObject } from './kubeObject.js';
@@ -10,15 +11,25 @@ Remove teh absolute root to make the snapshot deterministic
     from -> "path": "/Users/oyelowo/Desktop/dev/modern-distributed-app-template/kubernetes/generatedManifests/local/infrastructure/namespaces/1-manifest/v1-namespace-cert-manager-cert-manager.yaml",
     to   ->  "path": "generatedManifests/local/infrastructure/namespaces/1-manifest/v1-namespace-cert-manager-cert-manager.yaml",
 */
-const removeNonDeterministicRootDir = (p: TKubeObject) => ({ ...p, path: path.relative(getMainBaseDir(), p.path) });
-
+const removeNonDeterministicRootDir = (p: TKubeObject) => {
+    return {
+        ...p,
+        path: path.relative(getMainBaseDir(), p.path),
+        resourceBaseDir: path.relative(getMainBaseDir(), p.resourceBaseDir),
+    } as TKubeObject;
+}
 describe('KubeObject', () => {
+    beforeEach(() => {
+        new KubeObject('test').getOfAKind("SealedSecret").forEach(ss => {
+            sh.rm('-rf', ss.path)
+        })
+    })
     test('Can sync resources', () => {
         const kubeInstance = new KubeObject('test');
 
         const inst = kubeInstance.getAll().map(removeNonDeterministicRootDir);
         expect(inst).toMatchSnapshot();
-        expect(inst).toHaveLength(267);
+        expect(inst).toHaveLength(255);
 
         const inst2 = kubeInstance.getOfAKind('Deployment').map(removeNonDeterministicRootDir);
         expect(inst2).toMatchSnapshot();
@@ -35,7 +46,7 @@ describe('KubeObject', () => {
         const kubeInstance = new KubeObject('test');
         const graphqlMongo = kubeInstance.getForApp('services/graphql-mongo').map(removeNonDeterministicRootDir);
         expect(graphqlMongo).toMatchSnapshot();
-        expect(graphqlMongo).toHaveLength(22);
+        expect(graphqlMongo).toHaveLength(19);
 
         const reactWeb = kubeInstance.getForApp('services/react-web').map(removeNonDeterministicRootDir);
         expect(reactWeb).toMatchSnapshot();
@@ -43,11 +54,11 @@ describe('KubeObject', () => {
 
         const argocd = kubeInstance.getForApp('infrastructure/argocd').map(removeNonDeterministicRootDir);
         expect(argocd).toMatchSnapshot();
-        expect(argocd).toHaveLength(36);
+        expect(argocd).toHaveLength(34);
 
         const linkerd = kubeInstance.getForApp('infrastructure/linkerd').map(removeNonDeterministicRootDir);
         expect(linkerd).toMatchSnapshot();
-        expect(linkerd).toHaveLength(44);
+        expect(linkerd).toHaveLength(41);
 
         const certManager = kubeInstance.getForApp('infrastructure/cert-manager').map(removeNonDeterministicRootDir);
         expect(certManager).toMatchSnapshot();
