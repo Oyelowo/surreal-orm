1a9993480047611dc71348f6fe3ff929d5f8629c
oyelowo <oyelowooyedayo@gmail.com>
2022-08-14 23:39:33 +0300

    Switch reducer to foreach for kubeobject syncing

--- kubernetes/scripts/utils/kubeObject/kubeObject.ts
+++ kubernetes/scripts/utils/kubeObject/kubeObject.ts
@@ -66,8 +66,8 @@ export type TKubeObjectBaseCommonProps<K extends ResourceKind> = KubeObjectSchem
 // have to add as above
 type KubeObjectCustom =
     | (TKubeObjectBaseCommonProps<'Secret'> & {
-        selectedSecretsForUpdate?: string[] | null;
-    })
+          selectedSecretsForUpdate?: string[] | null;
+      })
     | TKubeObjectBaseCommonProps<'SealedSecret'>
     | TKubeObjectBaseCommonProps<'CustomResourceDefinition'>
     | TKubeObjectBaseCommonProps<'Deployment'>
@@ -105,7 +105,7 @@ export class KubeObject {
     };
 
     generateManifests = async (): Promise<void> => {
-        PlainSecretJsonConfig.syncAll()
+        PlainSecretJsonConfig.syncAll();
         await generateManifests(this);
         this.syncAll();
         syncCrdsCode(this.getOfAKind('CustomResourceDefinition'));
@@ -114,20 +114,17 @@ export class KubeObject {
     /** Extract information from all the manifests for an environment(local, staging etc)  */
     private syncAll = (): this => {
         const envDir = getGeneratedEnvManifestsDir(this.environment);
-
         const manifestsPaths = this.getManifestsPathWithinDir(envDir);
-
-        // create a new progress bar instance and use shades_classic theme
         const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
-        // start the progress bar with a total value of 200 and start value of 0
-        bar1.start(manifestsPaths.length, 0);
         console.log('Extracting kubeobject from manifest');
-        // eslint-disable-next-line unicorn/no-array-reduce
-        this.kubeObjectsAll = manifestsPaths.reduce<TKubeObject[]>((acc, manifestPath, i) => {
-            if (!manifestPath) return acc;
+        bar1.start(manifestsPaths.length, 0);
+
+        const kubeObjects: TKubeObject[] = [];
+        manifestsPaths.forEach((manifestPath, i) => {
+            if (!manifestPath) return;
             const kubeObject = yaml.parse(fs.readFileSync(manifestPath, 'utf8')) as TKubeObject;
-            if (_.isEmpty(kubeObject)) return acc;
+            if (_.isEmpty(kubeObject)) throw new Error('Manifest is empty. Check the directory that all is well');
 
             // let's mutate to make it a bit faster and should be okay since we only do it here
             kubeObject.path = manifestPath;
@@ -143,14 +140,12 @@ export class KubeObject {
                 kubeObject.data = R.mergeDeepRight(kubeObject.data ?? {}, encodedStringData);
             }
 
-            const updatedPath = kubeObjectSchema.parse(kubeObject) as TKubeObject;
-
-            acc.push(updatedPath);
+            kubeObjects.push(kubeObjectSchema.parse(kubeObject) as TKubeObject);
             bar1.update(i);
-            return acc;
-        }, []);
+        });
 
-        bar1.stop()
+        this.kubeObjectsAll = kubeObjects;
+        bar1.stop();
 
         return this;
     };
