422790fad67eaee18107ff6847300c1cccef6104
oyelowo <oyelowooyedayo@gmail.com>
2022-08-15 00:39:18 +0300

    Comment out certificate code to serve only as reference/example

--- kubernetes/src/resources/infrastructure/ingress/certificate.ts
+++ kubernetes/src/resources/infrastructure/ingress/certificate.ts
@@ -1,22 +1,32 @@
-import crds from '../../../../generatedCrdsTs/index.js';
-import { CLUSTER_ISSUER_NAME } from "../cert-manager/clusterIssuer.js";
-import { namespaces } from '../namespaces/index.js';
-import { nginxIngressProvider } from "./settings.js";
+/* eslint-disable unicorn/no-empty-file */
+/* An alternative for creating secrets for nginx-ingress. However, this is
+automatically handled in the cert-manager cluster issuer.
+This is left as a reference in case you want to create
+arbitrary certificate for any other object using cert-manager
+*/
+// import crds from '../../../../generatedCrdsTs/index.js';
+// import { CLUSTER_ISSUER_NAME } from '../cert-manager/clusterIssuer.js';
+// import { namespaces } from '../namespaces/index.js';
+// import { nginxIngressProvider } from './settings.js';
 
-const DNS_NAME_LINODE_BASE = "oyelowo.dev";
-export const SECRET_NAME_NGINX = "nginx-ingress-tls";
+// const DNS_NAME_LINODE_BASE = 'oyelowo.dev';
+// export const SECRET_NAME_NGINX = 'nginx-ingress-tls';
 
-export const certificateNginx = new crds.certmanager.v1.Certificate("certificate-nginx", {
-    metadata: {
-        name: "certificate-nginx",
-        namespace: namespaces.default
-    },
-    spec: {
-        dnsNames: [DNS_NAME_LINODE_BASE],
-        secretName: SECRET_NAME_NGINX,
-        issuerRef: {
-            name: CLUSTER_ISSUER_NAME,
-            kind: "ClusterIssuer"
-        }
-    }
-}, { provider: nginxIngressProvider })
+// export const certificateNginx = new crds.certmanager.v1.Certificate(
+//     'certificate-nginx',
+//     {
+//         metadata: {
+//             name: 'certificate-nginx',
+//             namespace: namespaces.default,
+//         },
+//         spec: {
+//             dnsNames: [DNS_NAME_LINODE_BASE],
+//             secretName: SECRET_NAME_NGINX,
+//             issuerRef: {
+//                 name: CLUSTER_ISSUER_NAME,
+//                 kind: 'ClusterIssuer',
+//             },
+//         },
+//     },
+//     { provider: nginxIngressProvider }
+// );
