69e87b8a19372d52334633ddc29e6c0873573372
oyelowo <oyelowooyedayo@gmail.com>
2022-08-16 17:08:20 +0300

    Implement cloud infra deployment

--- kubernetes/scripts/deployCloudInfra.ts
+++ kubernetes/scripts/deployCloudInfra.ts
@@ -1,30 +1,63 @@
-
 import path from 'node:path';
 import sh from 'shelljs';
+import yargs from 'yargs';
 import { getMainBaseDir } from '../src/resources/shared/directoriesManager.js';
 import { ARGV_ENVIRONMENTS, tsConfigPath } from './utils/argv.js';
 import { handleShellError } from './utils/shared.js';
 
+export const Argv = yargs(process.argv.slice(2))
+    .options({
+        'skip-preview': {
+            alias: 'sp',
+            boolean: true,
+            describe: 'Skip showing preview. Useful for CI(Continuous Integration) pipeline.',
+            // demandOption: true,
+        },
+        LINODE_TOKEN: {
+            alias: 'lt',
+            string: true,
+            describe: 'Skip showing preview. Useful for CI(Continuous Integration) pipeline.',
+            demandOption: true,
+        },
+        'pulumi-passphrase': {
+            alias: 'pp',
+            string: true,
+            describe: 'Skip showing preview. Useful for CI(Continuous Integration) pipeline.',
+            demandOption: true,
+        },
+    })
+    .parseSync();
+
 function main() {
-    const { environment } = ARGV_ENVIRONMENTS
+    const { environment } = ARGV_ENVIRONMENTS;
+
     const baseDir = getMainBaseDir();
-    const cloudManifestsDir = path.join(baseDir, 'generatedCloudInfra', environment);
+    const cloudManifestsDir = path.join(baseDir, 'generatedCloudInfra');
     sh.mkdir('-p', cloudManifestsDir);
     // https://www.pulumi.com/docs/intro/concepts/state/#logging-into-the-local-filesystem-backend
     sh.exec(`pulumi login file://${cloudManifestsDir}`);
 
+    try {
+        handleShellError(sh.rm('-rf', `${path.join(baseDir, `Pulumi.${environment}.yaml`)}`));
+        handleShellError(
+            sh.exec(`export PULUMI_CONFIG_PASSPHRASE='not-needed' && pulumi stack init --stack ${environment}`)
+        );
+    } catch {
+        console.log(`Already created`);
+    }
+
     handleShellError(
         sh.exec(
             `
-        export PULUMI_CONFIG_PASSPHRASE="oyelowo"
+        export PULUMI_CONFIG_PASSPHRASE="not-needed"
         export PULUMI_NODEJS_TRANSPILE_ONLY=true
         export PULUMI_SKIP_CONFIRMATIONS=true
         export PULUMI_NODEJS_TSCONFIG_PATH=${tsConfigPath}
-        export LINODE_TOKEN=c4ebdc329a825fd7b372e43f1de367dd2277aa39dc00536dfb9d63eb15b8d6c4
-        pulumi up --yes --skip-preview --stack lke --cwd ${baseDir}
+        export LINODE_TOKEN=${Argv.LINODE_TOKEN}
+        pulumi up --yes --skip-preview=${Argv.skipPreview} --stack ${environment}
        `
         )
     );
 }
 
-main()
+main();
