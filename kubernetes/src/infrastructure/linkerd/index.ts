959c00792a6c36f133d52bd3d45add5d9102cd74
oyelowo <oyelowooyedayo@gmail.com>
2022-10-08 02:57:57 +0300

    Fix and update linkerd code

--- kubernetes/src/infrastructure/linkerd/index.ts
+++ kubernetes/src/infrastructure/linkerd/index.ts
@@ -2,5 +2,6 @@ export * from './argoApp.js';
 export * from './certManagerCaIssuer.js';
 export * from './certManagerIdentityIssuer.js';
 export * from './certManagerTrust.js';
-export * from './linkerd.js';
+export * from './linkerdCrds.js';
+export * from './linkerdControlPlane.js';
 export * from './linkerdViz.js';
