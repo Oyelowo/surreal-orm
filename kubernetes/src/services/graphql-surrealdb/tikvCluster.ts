dca7ec3e09d82de21407321ac9e2b922c718e0ae
oyelowo <oyelowooyedayo@gmail.com>
2022-10-07 09:01:10 +0300

    Format code

--- kubernetes/src/services/graphql-surrealdb/tikvCluster.ts
+++ kubernetes/src/services/graphql-surrealdb/tikvCluster.ts
@@ -4,46 +4,50 @@ import { surrealdbSettings } from './surrealdb.js';
 
 // TiKV acts as the persistent layer for surrealdb. Surrealdb also supports in-memory, file-based,
 // foundationdb, rocksdb etc
-const surrealDBTikvCluster = new pc.pingcap.v1alpha1.TidbCluster('tikv-cluster', {
-    metadata: {
-        name: surrealdbSettings.envVars.TIKV_NAME,
-        namespace: surrealdbSettings.metadata.namespace,
-        // clusterName: "",
-    },
-    spec: {
-        timezone: 'UTC',
-        configUpdateStrategy: 'RollingUpdate',
-        pvReclaimPolicy: 'Retain',
-        enableDynamicConfiguration: true,
-        statefulSetUpdateStrategy: 'RollingUpdate',
-        pd: {
-            baseImage: 'pingcap/pd',
-            service: {
-                port: Number(surrealdbSettings.envVars.TIKV_PORT),
-            },
-            maxFailoverCount: 0,
-            replicas: 3,
-            requests: {
-                storage: '10Gi',
-            },
-            storageClassName: 'local-storage',
-            config: `
+const surrealDBTikvCluster = new pc.pingcap.v1alpha1.TidbCluster(
+    'tikv-cluster',
+    {
+        metadata: {
+            name: surrealdbSettings.envVars.TIKV_NAME,
+            namespace: surrealdbSettings.metadata.namespace,
+            // clusterName: "",
+        },
+        spec: {
+            timezone: 'UTC',
+            configUpdateStrategy: 'RollingUpdate',
+            pvReclaimPolicy: 'Retain',
+            enableDynamicConfiguration: true,
+            statefulSetUpdateStrategy: 'RollingUpdate',
+            pd: {
+                baseImage: 'pingcap/pd',
+                service: {
+                    port: Number(surrealdbSettings.envVars.TIKV_PORT),
+                },
+                maxFailoverCount: 0,
+                replicas: 3,
+                requests: {
+                    storage: '10Gi',
+                },
+                storageClassName: 'local-storage',
+                config: `
                 [dashboard]
                     internal-proxy = true
               ` as any,
-        },
-        tikv: {
-            baseImage: 'pingcap/tikv',
-            maxFailoverCount: 0,
-            storageClassName: 'local-storage',
-            replicas: 3,
-            requests: {
-                storage: '100Gi',
             },
-            config: {},
+            tikv: {
+                baseImage: 'pingcap/tikv',
+                maxFailoverCount: 0,
+                storageClassName: 'local-storage',
+                replicas: 3,
+                requests: {
+                    storage: '100Gi',
+                },
+                config: {},
+            },
         },
     },
-}, { provider: graphqlSurrealdb.getProvider()});
+    { provider: graphqlSurrealdb.getProvider() }
+);
 
 /* const tidbClusterAutoScaler = new pc.pingcap.v1alpha1.TidbClusterAutoScaler('er', {
     apiVersion: 'pingcap.com/v1alpha1',
