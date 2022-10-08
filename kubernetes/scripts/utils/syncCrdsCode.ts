e000bf739a7aea293be71555afefb0ed9acc3618
oyelowo <oyelowooyedayo@gmail.com>
2022-10-08 16:40:13 +0300

    Crds generation now works properly

--- kubernetes/scripts/utils/syncCrdsCode.ts
+++ kubernetes/scripts/utils/syncCrdsCode.ts
@@ -3,6 +3,7 @@ import { helmChartsInfo } from '../../src/shared/helmChartInfo.js';
 import { getGeneratedCrdsCodeDir } from '../../src/shared/directoriesManager.js';
 import chalk from 'chalk';
 import yaml from 'yaml';
+import fs from 'node:fs';
 
 export function syncCrdsCode() {
     const outDir = getGeneratedCrdsCodeDir();
@@ -19,44 +20,47 @@ export function syncCrdsCode() {
             sh.echo(
                 chalk.blueBright(`Syncing Crds from helm chart ${repoName}/${chart} version=${version} from ${repo}`)
             );
-
             const cmdRenderTemplateResources = `helm template ${chart}  --include-crds ${repoName}/${chart} --version ${version} --set installCRDs=true --set externalCA=true`;
-            const cmdCrd2pulumi = `crd2pulumi --nodejsPath ${outDir} - --force`;
-
             const renderedTemlate = sh.exec(cmdRenderTemplateResources, { silent: true });
+
             if (renderedTemlate.stderr) {
                 throw new Error(
-                    chalk.redBright(
-                        `Problem rendering helm chart to kubernetes resources. Check that the chart name, repo and version are correct. Error: ${renderedTemlate.stderr}`
-                    )
+                    chalk.redBright`Problem rendering helm chart to kubernetes resources. Check that the chart name, repo and version are correct. Error: ${renderedTemlate.stderr}`
                 );
             }
 
-            // Try 1 for best effort. This does not work for linkerd-crd due to crd2pulumi cli not being
-            // able to handle things like complex default values. So, Try 2 takes care of that.
-            sh.exec(`${cmdRenderTemplateResources} | ${cmdCrd2pulumi}`, { silent: true });
+            const renderedFromHelmChart = yaml.parseAllDocuments(renderedTemlate.stdout);
+            // Some helm charts e.g tikv/tidb don't include their crds into the chart.
+            const renderedFromExternalCrds = externalCrds.flatMap(crdUrl => {
+                const rendered = sh.exec(`curl ${crdUrl} > ${outDir}/${chart}.yaml`, { silent: true })
 
-            // Try 2.
-            // Crd2pulumi is not yet able to handle some values e.g in linkerd-crds. This parser helps with transformation
-            // to make it possible for crd2pulumi to handle
-            // TODO: This and one later below can be removed when this issue is resolved: https://github.com/pulumi/crd2pulumi/issues/102
-            yaml.parseAllDocuments(renderedTemlate.stdout).forEach((parsedKubeResource) => {
-                sh.exec(`echo '${parsedKubeResource}' | ${cmdCrd2pulumi}`, { silent: true });
+                if (rendered.stderr) throw new Error(chalk.redBright`Problem fetching crd. Error: ${rendered.stderr}`);
+
+                return yaml.parseAllDocuments(rendered.stdout);
+
             });
 
-            // Some helm charts e.g tikv/tidb don't include their crds into the chart.
-            if (externalCrds) {
-                externalCrds.forEach((crdUrl) => {
-                    // Try1
-                    sh.exec(`curl ${crdUrl} | ${cmdCrd2pulumi}`, { silent: true });
+            const renderedCrds = [...renderedFromHelmChart, ...renderedFromExternalCrds].filter(t => t.toString().includes("kind: CustomResourceDefinition"));
 
-                    // Try2
-                    const remoteCrd = sh.exec(`curl ${crdUrl}`).stdout;
-                    yaml.parseAllDocuments(remoteCrd).forEach((parsedKubeResource) => {
-                        sh.exec(`echo '${parsedKubeResource}' | ${cmdCrd2pulumi}`, { silent: true });
-                    });
+            renderedCrds.forEach((parsedKubeResource, i) => {
+                const data = yaml.parse(parsedKubeResource.toString(), (k, v) => {
+                    // Crd2pulumi is not yet able to handle some values e.g in linkerd-crds. This parser helps with transformation
+                    // to make it possible for crd2pulumi to handle
+                    /* It appears to happen on fields where the default contains a nested value, like: status: default: observedGeneration: -1*/
+                    // TODO: This can be removed when this issue is resolved: https://github.com/pulumi/crd2pulumi/issues/102
+                    return (typeof v === 'object' && k === "default")
+                        ? undefined : v // else return the value
                 });
-            }
+
+                fs.writeFile(`${outDir}/${repoName}${chart}${i}.yaml`, yaml.stringify(data), (err) => {
+                    if (err) throw err;
+                })
+            });
         });
     });
+
+    // const paths = sh.exec(`grep --include=\*.{yaml,yml} -Rnwl '${outDir}' -e 'kind: CustomResourceDefinition'`, { silent: true }).stdout
+    // console.log("paths", paths)
+    // // sh.exec(`crd2pulumi --nodejsPath ./xxx  ${outDir}/* --force`);
+    // sh.exec(`crd2pulumi --nodejsPath ./xxx  ${paths.split("\n").join(" ")} --force`);
 }
