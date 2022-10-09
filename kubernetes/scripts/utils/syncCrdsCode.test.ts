9351c27f00ba52e3fbc1a2f3ab1a2c201d3f4f98
oyelowo <oyelowooyedayo@gmail.com>
2022-10-09 07:30:32 +0300

    Test type def sanitizer

---
+++ kubernetes/scripts/utils/syncCrdsCode.test.ts
@@ -0,0 +1,40 @@
+import { sanitizePulumiTypeDefinitions } from './syncCrdsCode';
+
+describe('syncCrdsCode', () => {
+    test('Can sanitize badly formatted Pulumi typescript definition', () => {
+        const data = `export interface TidbClu-sterStatusArgs {
+            auto-scaler?: pulumi.Input<inputs.pingcap.v1alpha1.TidbClusterStatusAuto-ScalerArgs>;
+            cluste?: pulumi.Input<string>;
+            co-ndi-ti-ons-?: pulumi.Input<pulumi.Input<inputs.pingcap.v1alpha1.TidbCluster-Status-ConditionsArgs>[]>;
+            p-d: pulumi.Input<inputs.pin-gcap.v1alpha1.TidbClusterStatusPdArgs>;
+            pump?: pulumi.Input<inputs.pingcap.v1alpha1.TidbClusterStatusPumpArgs>;
+            ticdc?: pulumi.Input<inputs.pingcap.v1alpha1.TidbClusterStatusTicdcArgs>;
+            tidb?: pulumi.Input<inputs.pingcap.v1alpha1.TidbClusterStatusTidbArgs>;
+            tiflash?: pulumi.Input<inputs.pingcap.v1alpha1.TidbClusterStatusTiflashArgs>;
+            tikv?: pulumi.Input<inputs.pingcap.v1alpha1.TidbClusterStatusTikvArgs>;
+        }
+
+        export interface TidbClusterSta-tusAuto-ScalerArgs {
+            name: pulumi.Input<string>;
+            namespace: pulumi.Input<string>;
+        }`;
+        const sanitized = sanitizePulumiTypeDefinitions({ data });
+
+        expect(sanitized).toBe(`export interface TidbClusterStatusArgs {
+            "auto-scaler"?: pulumi.Input<inputs.pingcap.v1alpha1.TidbClusterStatusAutoScalerArgs>;
+            cluste?: pulumi.Input<string>;
+            "co-ndi-ti-ons-"?: pulumi.Input<pulumi.Input<inputs.pingcap.v1alpha1.TidbClusterStatusConditionsArgs>[]>;
+            "p-d": pulumi.Input<inputs.pingcap.v1alpha1.TidbClusterStatusPdArgs>;
+            pump?: pulumi.Input<inputs.pingcap.v1alpha1.TidbClusterStatusPumpArgs>;
+            ticdc?: pulumi.Input<inputs.pingcap.v1alpha1.TidbClusterStatusTicdcArgs>;
+            tidb?: pulumi.Input<inputs.pingcap.v1alpha1.TidbClusterStatusTidbArgs>;
+            tiflash?: pulumi.Input<inputs.pingcap.v1alpha1.TidbClusterStatusTiflashArgs>;
+            tikv?: pulumi.Input<inputs.pingcap.v1alpha1.TidbClusterStatusTikvArgs>;
+        }
+
+        export interface TidbClusterStatusAutoScalerArgs {
+            name: pulumi.Input<string>;
+            namespace: pulumi.Input<string>;
+        }`);
+    });
+});
