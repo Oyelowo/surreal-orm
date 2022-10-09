3d2ab65d8f06451711693a63e6cf1411809fe5a1
oyelowo <oyelowooyedayo@gmail.com>
2022-10-09 16:18:51 +0300

    Export topic

--- kubernetes/src/services/graphql-surrealdb/fluvioApp.ts
+++ kubernetes/src/services/graphql-surrealdb/fluvioApp.ts
@@ -1,19 +1,19 @@
-import crds from "../../../generatedCrdsTs/index.js";
-import { surrealdbSettings } from "./surrealdb.js";
-crds.fluvio.v2.Topic
+import crds from '../../../generatedCrdsTs/index.js';
+import { surrealdbSettings } from './surrealdb.js';
+crds.fluvio.v2.Topic;
 
-export const userLocationTopic = new crds.fluvio.v2.Topic("", {
+export const userLocationTopic = new crds.fluvio.v2.Topic('', {
     apiVersion: 'fluvio.infinyon.com/v2',
-    kind: "Topic",
+    kind: 'Topic',
     metadata: {
-        name: "user-location",
+        name: 'user-location',
         namespace: surrealdbSettings.metadata.namespace,
     },
     spec: {
         replicas: 1,
         storage: {
             maxPartitionSize: 3,
-            segmentSize: 3
-        }
-    }
-})
+            segmentSize: 3,
+        },
+    },
+});
