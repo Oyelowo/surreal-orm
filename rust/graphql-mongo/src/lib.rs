99be04c1008ac7dff926fa0adb50432ac0b69df6
oyelowo <oyelowooyedayo@gmail.com>
2022-10-12 00:05:51 +0300

    Delete graphql mongo

--- rust/graphql-mongo/src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub mod app;
-pub mod handlers;
-pub mod utils;
--- rust/graphql-mongo/src/utils/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod configuration;
-pub mod graphql;
-pub mod mongodb;
-pub mod token;
-
-// pub use self::{configuration::*, cors::*, graphql::*, mongodb::*, session::*, token::*};
