ce5394e87dfee7995b9a6c343c310514d3e4ae39
oyelowo <oyelowooyedayo@gmail.com>
2023-03-12 20:53:05 +0200

    Add test for sleep statement and fix the build

--- rust/surrealdb-macros/src/query_sleep.rs
+++ rust/surrealdb-macros/src/query_sleep.rs
@@ -5,6 +5,8 @@
  * Licensed under the MIT license
  */
 
+use std::fmt::Display;
+
 use surrealdb::sql;
 
 use crate::{query_insert::Buildable, query_remove::Runnable, query_select::Duration, Queryable};
@@ -25,10 +27,28 @@ impl SleepStatement {
 }
 impl Buildable for SleepStatement {
     fn build(&self) -> String {
-        self.0.to_string()
+        format!("SLEEP {};", self.0.to_string())
     }
 }
+impl Queryable for SleepStatement {}
 
 impl Runnable for SleepStatement {}
 
-impl Queryable for SleepStatement {}
+impl Display for SleepStatement {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.build())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use std::time::Duration;
+
+    use super::*;
+
+    #[test]
+    fn test_sleep_statement() {
+        assert_eq!(sleep(Duration::from_secs(43)).build(), "SLEEP 43s;");
+    }
+}
