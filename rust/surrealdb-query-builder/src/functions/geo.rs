ecfb94b67b838f3eb85efd4ea99a4bea49d7a4d9
oyelowo <oyelowooyedayo@gmail.com>
2023-03-27 21:11:19 +0300

    Implement all geo functions builders

--- rust/surrealdb-query-builder/src/functions/geo.rs
+++ rust/surrealdb-query-builder/src/functions/geo.rs
@@ -58,18 +58,109 @@ use super::array::Function;
 //         Self::Geometry(value)
 //     }
 // }
-struct Geometry(sql::Value);
-
-pub fn area(geometry: impl Into<GeometryOrField>) -> Function {
+pub(crate) fn create_geo_with_single_arg(
+    geometry: impl Into<GeometryOrField>,
+    fn_suffix: &str,
+) -> Function {
     let binding = Binding::new(geometry.into());
     let string = binding.get_param_dollarised();
 
     Function {
-        query_string: format!("geo::area({})", string),
+        query_string: format!("geo::{fn_suffix}({})", string),
         bindings: vec![binding],
     }
 }
 
+fn create_geo_fn_with_two_args(
+    point1: impl Into<sql::Geometry>,
+    point2: impl Into<sql::Geometry>,
+    fn_suffix: &str,
+) -> Function {
+    let binding1 = Binding::new(point1.into());
+    let string1 = binding1.get_param_dollarised();
+
+    let binding2 = Binding::new(point2.into());
+    let string2 = binding2.get_param_dollarised();
+
+    Function {
+        query_string: format!("geo::{fn_suffix}({}, {})", string1, string2),
+        bindings: vec![binding1, binding2],
+    }
+}
+
+pub fn area(geometry: impl Into<GeometryOrField>) -> Function {
+    create_geo_with_single_arg(geometry, "area")
+}
+
+pub fn bearing(point1: impl Into<sql::Geometry>, point2: impl Into<sql::Geometry>) -> Function {
+    create_geo_fn_with_two_args(point1, point2, "bearing")
+}
+
+pub fn centroid(geometry: impl Into<GeometryOrField>) -> Function {
+    create_geo_with_single_arg(geometry, "centroid")
+}
+
+pub fn distance(point1: impl Into<sql::Geometry>, point2: impl Into<sql::Geometry>) -> Function {
+    create_geo_fn_with_two_args(point1, point2, "distance")
+}
+
+pub mod hash {
+    pub enum Accuracy {
+        Empty,
+        Number(surrealdb::sql::Number),
+    }
+
+    impl<T> From<T> for Accuracy
+    where
+        T: Into<sql::Number>,
+    {
+        fn from(value: T) -> Self {
+            let value: sql::Number = value.into();
+            Self::Number(value)
+        }
+    }
+
+    impl From<Empty> for Accuracy {
+        fn from(value: Empty) -> Self {
+            Self::Empty
+        }
+    }
+
+    use surrealdb::sql;
+
+    use super::create_geo_with_single_arg;
+    use crate::{
+        field::GeometryOrField,
+        functions::array::Function,
+        sql::{Binding, Empty},
+    };
+
+    pub fn decode(geometry: impl Into<GeometryOrField>) -> Function {
+        create_geo_with_single_arg(geometry, "hash::decode")
+    }
+
+    pub fn encode(geometry: impl Into<GeometryOrField>, accuracy: Accuracy) -> Function {
+        let binding = Binding::new(geometry.into());
+        let geometry_param = binding.get_param_dollarised();
+
+        let mut bindings = vec![binding];
+
+        let str = match accuracy {
+            Accuracy::Empty => format!("geo::hash::encode({geometry_param})",),
+            Accuracy::Number(num) => {
+                let binding = Binding::new(num);
+                let accuracy_param = binding.get_param_dollarised();
+                bindings.push(binding);
+
+                format!("geo::hash::encode({geometry_param}, {accuracy_param})",)
+            }
+        };
+        Function {
+            query_string: str,
+            bindings,
+        }
+    }
+}
 #[test]
 fn test_area_with_field() {
     let city = Field::new("city");
