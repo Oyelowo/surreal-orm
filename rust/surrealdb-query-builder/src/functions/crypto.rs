2cb49557a0a43765e202a7b60ced9b47f4e7d61a
oyelowo <oyelowooyedayo@gmail.com>
2023-03-27 20:09:34 +0300

    Test argon2 generate

--- rust/surrealdb-query-builder/src/functions/crypto.rs
+++ rust/surrealdb-query-builder/src/functions/crypto.rs
@@ -156,7 +156,6 @@ fn test_sha512() {
 
 #[test]
 fn test_argon2_compare() {
-    // LET $hash = "";
     let hash = Param::new("$argon2id$v=19$m=4096,t=3,p=1$pbZ6yJ2rPJKk4pyEMVwslQ$jHzpsiB+3S/H+kwFXEcr10vmOiDkBkydVCSMfRxV7CA");
     let pass = Param::new("the strongest password");
     let result = argon2::compare("Oyelowo", "Oyedayo");
@@ -172,7 +171,6 @@ fn test_argon2_compare() {
 
 #[test]
 fn test_argon2_compare_with_param() {
-    // LET $hash = "";
     let hash = let_("hash").equal("$argon2id$v=19$m=4096,t=3,p=1$pbZ6yJ2rPJKk4pyEMVwslQ$jHzpsiB+3S/H+kwFXEcr10vmOiDkBkydVCSMfRxV7CA");
     let pass = let_("pass").equal("the strongest password");
 
@@ -184,5 +182,18 @@ fn test_argon2_compare_with_param() {
     assert_eq!(
         result.to_raw().to_string(),
         "crypto::argon2::compare($hash, $pass)"
+    );
+}
+
+#[test]
+fn test_argon2_generate() {
+    let result = argon2::generate("Oyelowo");
+    assert_eq!(
+        result.fine_tune_params(),
+        "crypto::argon2::generate($_param_00000001)"
+    );
+    assert_eq!(
+        result.to_raw().to_string(),
+        "crypto::argon2::generate('Oyelowo')"
     );
 }
