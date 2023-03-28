e1c18a6d78436d2570a9386cc9b23f2b03cead7b
oyelowo <oyelowooyedayo@gmail.com>
2023-03-28 14:32:33 +0300

    Implement math:min builder

--- rust/surrealdb-query-builder/src/functions/math.rs
+++ rust/surrealdb-query-builder/src/functions/math.rs
@@ -117,6 +117,10 @@ fn median(number: impl Into<Array>) -> Function {
     create_fn_with_single_array_arg(number, "median")
 }
 
+fn min(number: impl Into<Array>) -> Function {
+    create_fn_with_single_array_arg(number, "min")
+}
+
 fn fixed(number: impl Into<Number>, decimal_number: impl Into<Number>) -> Function {
     let num_binding = Binding::new(number.into());
     let decimal_place_binding = Binding::new(decimal_number.into());
@@ -205,6 +209,7 @@ macro_rules! create_test_for_fn_with_single_array_arg {
 create_test_for_fn_with_single_array_arg!(max, "max");
 create_test_for_fn_with_single_array_arg!(mean, "mean");
 create_test_for_fn_with_single_array_arg!(median, "median");
+create_test_for_fn_with_single_array_arg!(min, "min");
 
 #[test]
 fn test_fixed_fn_with_field_data() {
