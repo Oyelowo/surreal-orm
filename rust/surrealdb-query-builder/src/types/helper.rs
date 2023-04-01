3dfc3562fa1a3ccdff0ad2b3929f340f03b1c9b0
oyelowo <oyelowooyedayo@gmail.com>
2023-04-02 00:43:28 +0300

    Create sql types-likes

---
+++ rust/surrealdb-query-builder/src/types/helper.rs
@@ -0,0 +1,73 @@
+macro_rules! create_value_like {
+    () => {
+        #[derive(serde::Serialize, Debug, Clone)]
+        pub enum StrandLike {
+            Strand(sql::Strand),
+            Field(sql::Idiom),
+            Param(sql::Param),
+        }
+        // macro_rules! impl_geometry_like_from {
+        //     ($($t:ty),*) => {
+        //         $(impl From<$t> for GeometryLike {
+        //             fn from(value: $t) -> Self {
+        //                 Self::Geometry(sql::Geometry::from(value))
+        //             }
+        //         })*
+        //     };
+        // }
+        //
+        // impl_geometry_like_from!(
+        //     geo::Polygon,
+        //     geo::Point,
+        //     geo::LineString,
+        //     geo::MultiPoint,
+        //     geo::MultiPolygon,
+        //     geo::MultiLineString
+        // );
+
+        use surrealdb::sql;
+
+        use super::{Field, Param};
+
+        impl<T: Into<sql::Strand>> From<T> for StrandLike {
+            fn from(value: T) -> Self {
+                let value: sql::Geometry = value.into();
+                Self::Geometry(value.into())
+            }
+        }
+
+        impl From<Field> for StrandLike {
+            fn from(val: Field) -> Self {
+                StrandLike::Field(val.into())
+            }
+        }
+
+        impl From<Param> for StrandLike {
+            fn from(val: Param) -> Self {
+                StrandLike::Param(val.into())
+            }
+        }
+
+        impl From<&Field> for StrandLike {
+            fn from(val: &Field) -> Self {
+                StrandLike::Field(val.into())
+            }
+        }
+
+        impl From<sql::Value> for StrandLike {
+            fn from(value: sql::Value) -> StrandLike {
+                Self::Geometry(value)
+            }
+        }
+
+        impl From<StrandLike> for sql::Value {
+            fn from(val: StrandLike) -> sql::Value {
+                match val {
+                    StrandLike::StrandLike(g) => g.into(),
+                    StrandLike::Field(f) => f.into(),
+                    StrandLike::Param(p) => p.into(),
+                }
+            }
+        }
+    };
+}
--- rust/surrealdb-query-builder/src/types/mod.rs
+++ rust/surrealdb-query-builder/src/types/mod.rs
@@ -1,23 +1,120 @@
+pub(crate) mod array;
 pub(crate) mod clause;
 pub(crate) mod expression;
 pub(crate) mod field;
 pub(crate) mod field_updater;
 pub(crate) mod filter;
 pub(crate) mod geometry;
+pub(crate) mod helper;
+pub(crate) mod idiom;
 pub(crate) mod numbers;
 pub(crate) mod ordinal;
 pub(crate) mod param;
 pub(crate) mod return_;
-pub(crate) mod sql_components;
+pub(crate) mod strand;
 pub(crate) mod surreal_id;
 pub(crate) mod token_target;
+pub(crate) mod value;
 
+pub use array::*;
 pub use clause::*;
 pub use field::*;
 pub use field_updater::*;
 pub use filter::*;
 pub use geometry::*;
+pub use idiom::*;
+pub use numbers::*;
 pub use numbers::*;
 pub use ordinal::*;
+pub use ordinal::*;
 pub use param::*;
-pub use sql_components::*;
+pub use param::*;
+pub use return_::*;
+pub use strand::*;
+pub use surreal_id::*;
+pub use token_target::*;
+pub use value::*;
+
+macro_rules! create_value_like_struct {
+    ($sql_type_name:expr) => {
+        paste::paste! {
+            #[derive(serde::Serialize, Debug, Clone)]
+            pub enum [<$sql_type_name Like>] {
+                [<$sql_type_name>](sql::[<$sql_type_name>]),
+                Field(sql::Idiom),
+                Param(sql::Param),
+            }
+            // macro_rules! impl_geometry_like_from {
+            //     ($($t:ty),*) => {
+            //         $(impl From<$t> for GeometryLike {
+            //             fn from(value: $t) -> Self {
+            //                 Self::Geometry(sql::Geometry::from(value))
+            //             }
+            //         })*
+            //     };
+            // }
+            //
+            // impl_geometry_like_from!(
+            //     geo::Polygon,
+            //     geo::Point,
+            //     geo::LineString,
+            //     geo::MultiPoint,
+            //     geo::MultiPolygon,
+            //     geo::MultiLineString
+            // );
+
+            use surrealdb::sql;
+
+            use super::{Field, Param};
+
+            impl<T: Into<sql::[<$sql_type_name>]>> From<T> for [<$sql_type_name Like>] {
+                fn from(value: T) -> Self {
+                    let value: sql::[<$sql_type_name>] = value.into();
+                    Self::Geometry(value.into())
+                }
+            }
+
+            impl From<Field> for [<$sql_type_name Like>] {
+                fn from(val: Field) -> Self {
+                    [<$sql_type_name Like>]::Field(val.into())
+                }
+            }
+
+            impl From<Param> for [<$sql_type_name Like>] {
+                fn from(val: Param) -> Self {
+                    [<$sql_type_name Like>]::Param(val.into())
+                }
+            }
+
+            impl From<&Field> for [<$sql_type_name Like>] {
+                fn from(val: &Field) -> Self {
+                    [<$sql_type_name Like>]::Field(val.into())
+                }
+            }
+
+            impl From<sql::Value> for [<$sql_type_name Like>] {
+                fn from(value: sql::Value) -> [<$sql_type_name Like>] {
+                    Self::Geometry(value)
+                }
+            }
+
+            impl From<[<$sql_type_name Like>]> for sql::Value {
+                fn from(val: StrandLike) -> sql::Value {
+                    match val {
+                        [<$sql_type_name Like>]::[<$sql_type_name>](g) => g.into(),
+                        [<$sql_type_name Like>]::Field(f) => f.into(),
+                        [<$sql_type_name Like>]::Param(p) => p.into(),
+                    }
+                }
+            }
+        };
+    };
+}
+
+create_value_like_struct!("Number");
+create_value_like_struct!("Strand");
+create_value_like_struct!("Geometry");
+create_value_like_struct!("Array");
+create_value_like_struct!("Idiom");
+create_value_like_struct!("Duration");
+create_value_like_struct!("Datetime");
