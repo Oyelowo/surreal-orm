e0c4205688d5bd69ece927623eb5dfa9cee4ebdb
oyelowo <oyelowooyedayo@gmail.com>
2023-04-02 23:31:03 +0300

    Add variations of runnable return types

--- rust/surrealdb-query-builder/src/traits/statements.rs
+++ rust/surrealdb-query-builder/src/traits/statements.rs
@@ -29,6 +29,82 @@ use super::{Buildable, Parametric};
 //
 // Runnable: method: run() ...Just run. Don't care about return type
 // run()
+
+// RunnableStandard
+#[async_trait]
+pub trait RunnableStandard<T>
+where
+    Self: Parametric + Buildable,
+    T: Serialize + DeserializeOwned,
+{
+    // async fn return_none(&self, db: Surreal<Db>)->surrealdb::Result<T>;
+    // return_one(),
+    // return_many(),
+    // return_one_before(),
+    // return_one_after(),
+    // return_many_before(),
+    // return_many_after(),
+    // return_one_diff(),
+    // return_many_diff(),
+    // return_one_projections(),
+    // return_many_projections()
+    // async fn return_none(&self, db: Surreal<Db>) -> surrealdb::Result<T>;
+    // async fn return_one(&self, db: Surreal<Db>) -> surrealdb::Result<T>;
+    // async fn return_many(&self, db: Surreal<Db>) -> surrealdb::Result<Vec<T>>;
+    // async fn return_one_before(&self, db: Surreal<Db>) -> surrealdb::Result<Option<T>>;
+    // async fn return_one_after(&self, db: Surreal<Db>) -> surrealdb::Result<Option<T>>;
+    // async fn return_many_before(&self, db: Surreal<Db>) -> surrealdb::Result<Vec<T>>;
+    // async fn return_many_after(&self, db: Surreal<Db>) -> surrealdb::Result<Vec<T>>;
+    // async fn return_one_diff(&self, db: Surreal<Db>) -> surrealdb::Result<Option<T>>;
+    // async fn return_many_diff(&self, db: Surreal<Db>) -> surrealdb::Result<Vec<T>>;
+    // async fn return_one_projections(&self, db: Surreal<Db>) -> surrealdb::Result<Option<T>>;
+    // async fn return_many_projections(&self, db: Surreal<Db>) -> surrealdb::Result<Vec<T>>;
+
+    async fn return_one(&self, db: Surreal<Db>) -> surrealdb::Result<T> {
+        let query = self.build();
+        let query = db.query(query);
+
+        let mut query = self.get_bindings().iter().fold(query, |acc, val| {
+            acc.bind((val.get_param(), val.get_value()))
+        });
+
+        let mut response = query.await?;
+
+        // If it errors, try to check if multiple entries have been inputed, hence, suurealdb
+        // trying to return Vec<T> rather than Option<T>, then pick the first of the returned
+        // Ok<T>.
+        let mut returned_val = match response.take::<Option<T>>(0) {
+            Ok(one) => vec![one.unwrap()],
+            Err(err) => response.take::<Vec<T>>(0)?,
+        };
+
+        // TODO:: Handle error if nothing is returned
+        let only_or_last = returned_val.pop().unwrap();
+        Ok(only_or_last)
+    }
+
+    async fn return_many(&self, db: Surreal<Db>) -> surrealdb::Result<Vec<T>> {
+        let query = self.build();
+        let query = db.query(query);
+
+        let mut query = self.get_bindings().iter().fold(query, |acc, val| {
+            acc.bind((val.get_param(), val.get_value()))
+        });
+
+        let mut response = query.await?;
+        // This does the reverse of get_one
+        // If it errors, try to check if only single entry has been inputed, hence, suurealdb
+        // trying to return Option<T>, then pick the return the only item as Vec<T>.
+        let mut returned_val = match response.take::<Vec<T>>(0) {
+            Ok(many) => many,
+            Err(err) => vec![response.take::<Option<T>>(0)?.unwrap()],
+        };
+
+        // TODO:: Handle error if nothing is returned
+        Ok(returned_val)
+    }
+}
+
 #[async_trait]
 pub trait Runnable<T>
 where
