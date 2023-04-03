348a618005d26c5c0c4c103e703e7212a13e3f24
oyelowo <oyelowooyedayo@gmail.com>
2023-04-03 15:22:38 +0300

    Fix import in attributes macro

--- rust/surrealdb-derive/src/models/attributes.rs
+++ rust/surrealdb-derive/src/models/attributes.rs
@@ -630,7 +630,7 @@ impl ReferencedNodeMeta {
                 let ref_node_type = format_ident!("{ref_node_type}");
                 define_array_field_content_methods
                         // .push(quote!(.type_(#crate_name::FieldType::Record)));
-                            .push(quote!(.type_(#crate_name::statements::FieldType::Record(#ref_node_type::table_name()))));
+                            .push(quote!(.type_(#crate_name::FieldType::Record(#ref_node_type::table_name()))));
             }
             _ => {}
         }
