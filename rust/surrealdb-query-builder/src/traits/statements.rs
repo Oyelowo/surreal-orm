35925fae5d5d410e8283644c365523ed387a2a4b
oyelowo <oyelowooyedayo@gmail.com>
2023-04-03 14:59:01 +0300

    Uncomment runnable methods

--- rust/surrealdb-query-builder/src/traits/statements.rs
+++ rust/surrealdb-query-builder/src/traits/statements.rs
@@ -62,100 +62,100 @@ where
     // async fn return_one_projections(&self, db: Surreal<Db>) -> surrealdb::Result<Option<T>>;
     // async fn return_many_projections(&self, db: Surreal<Db>) -> surrealdb::Result<Vec<T>>;
 
-    //     async fn return_one(&self, db: Surreal<Db>) -> surrealdb::Result<T> {
-    //         let query = self.build();
-    //         let query = db.query(query);
-    //
-    //         let mut query = self.get_bindings().iter().fold(query, |acc, val| {
-    //             acc.bind((val.get_param(), val.get_value()))
-    //         });
-    //
-    //         let mut response = query.await?;
-    //
-    //         // If it errors, try to check if multiple entries have been inputed, hence, suurealdb
-    //         // trying to return Vec<T> rather than Option<T>, then pick the first of the returned
-    //         // Ok<T>.
-    //         let mut returned_val = match response.take::<Option<T>>(0) {
-    //             Ok(one) => vec![one.unwrap()],
-    //             Err(err) => response.take::<Vec<T>>(0)?,
-    //         };
-    //
-    //         // TODO:: Handle error if nothing is returned
-    //         let only_or_last = returned_val.pop().unwrap();
-    //         Ok(only_or_last)
-    //     }
-    //
-    //     async fn return_many(&self, db: Surreal<Db>) -> surrealdb::Result<Vec<T>> {
-    //         let query = self.build();
-    //         let query = db.query(query);
-    //
-    //         let mut query = self.get_bindings().iter().fold(query, |acc, val| {
-    //             acc.bind((val.get_param(), val.get_value()))
-    //         });
-    //
-    //         let mut response = query.await?;
-    //         // This does the reverse of get_one
-    //         // If it errors, try to check if only single entry has been inputed, hence, suurealdb
-    //         // trying to return Option<T>, then pick the return the only item as Vec<T>.
-    //         let mut returned_val = match response.take::<Vec<T>>(0) {
-    //             Ok(many) => many,
-    //             Err(err) => vec![response.take::<Option<T>>(0)?.unwrap()],
-    //         };
-    //
-    //         // TODO:: Handle error if nothing is returned
-    //         Ok(returned_val)
-    //     }
-    // }
-    //
-    // #[async_trait]
-    // pub trait Runnable<T>
-    // where
-    //     Self: Parametric + Buildable,
-    //     T: Serialize + DeserializeOwned,
-    // {
-    //     async fn return_one(&self, db: Surreal<Db>) -> surrealdb::Result<T> {
-    //         let query = self.build();
-    //         let query = db.query(query);
-    //
-    //         let mut query = self.get_bindings().iter().fold(query, |acc, val| {
-    //             acc.bind((val.get_param(), val.get_value()))
-    //         });
-    //
-    //         let mut response = query.await?;
-    //
-    //         // If it errors, try to check if multiple entries have been inputed, hence, suurealdb
-    //         // trying to return Vec<T> rather than Option<T>, then pick the first of the returned
-    //         // Ok<T>.
-    //         let mut returned_val = match response.take::<Option<T>>(0) {
-    //             Ok(one) => vec![one.unwrap()],
-    //             Err(err) => response.take::<Vec<T>>(0)?,
-    //         };
-    //
-    //         // TODO:: Handle error if nothing is returned
-    //         let only_or_last = returned_val.pop().unwrap();
-    //         Ok(only_or_last)
-    //     }
-    //
-    //     async fn return_many<M: Into<Vec<T>>>(&self, db: Surreal<Db>) -> surrealdb::Result<M> {
-    //         let query = self.build();
-    //         let query = db.query(query);
-    //
-    //         let mut query = self.get_bindings().iter().fold(query, |acc, val| {
-    //             acc.bind((val.get_param(), val.get_value()))
-    //         });
-    //
-    //         let mut response = query.await?;
-    //         // This does the reverse of get_one
-    //         // If it errors, try to check if only single entry has been inputed, hence, suurealdb
-    //         // trying to return Option<T>, then pick the return the only item as Vec<T>.
-    //         let mut returned_val = match response.take::<Vec<T>>(0) {
-    //             Ok(many) => many,
-    //             Err(err) => vec![response.take::<Option<T>>(0)?.unwrap()],
-    //         };
-    //
-    //         // TODO:: Handle error if nothing is returned
-    //         Ok(returned_val)
-    //     }
+    async fn return_one(&self, db: Surreal<Db>) -> surrealdb::Result<T> {
+        let query = self.build();
+        let query = db.query(query);
+
+        let mut query = self.get_bindings().iter().fold(query, |acc, val| {
+            acc.bind((val.get_param(), val.get_value()))
+        });
+
+        let mut response = query.await?;
+
+        // If it errors, try to check if multiple entries have been inputed, hence, suurealdb
+        // trying to return Vec<T> rather than Option<T>, then pick the first of the returned
+        // Ok<T>.
+        let mut returned_val = match response.take::<Option<T>>(0) {
+            Ok(one) => vec![one.unwrap()],
+            Err(err) => response.take::<Vec<T>>(0)?,
+        };
+
+        // TODO:: Handle error if nothing is returned
+        let only_or_last = returned_val.pop().unwrap();
+        Ok(only_or_last)
+    }
+
+    async fn return_many(&self, db: Surreal<Db>) -> surrealdb::Result<Vec<T>> {
+        let query = self.build();
+        let query = db.query(query);
+
+        let mut query = self.get_bindings().iter().fold(query, |acc, val| {
+            acc.bind((val.get_param(), val.get_value()))
+        });
+
+        let mut response = query.await?;
+        // This does the reverse of get_one
+        // If it errors, try to check if only single entry has been inputed, hence, suurealdb
+        // trying to return Option<T>, then pick the return the only item as Vec<T>.
+        let mut returned_val = match response.take::<Vec<T>>(0) {
+            Ok(many) => many,
+            Err(err) => vec![response.take::<Option<T>>(0)?.unwrap()],
+        };
+
+        // TODO:: Handle error if nothing is returned
+        Ok(returned_val)
+    }
+}
+
+#[async_trait]
+pub trait Runnable<T>
+where
+    Self: Parametric + Buildable,
+    T: Serialize + DeserializeOwned,
+{
+    async fn return_one(&self, db: Surreal<Db>) -> surrealdb::Result<T> {
+        let query = self.build();
+        let query = db.query(query);
+
+        let mut query = self.get_bindings().iter().fold(query, |acc, val| {
+            acc.bind((val.get_param(), val.get_value()))
+        });
+
+        let mut response = query.await?;
+
+        // If it errors, try to check if multiple entries have been inputed, hence, suurealdb
+        // trying to return Vec<T> rather than Option<T>, then pick the first of the returned
+        // Ok<T>.
+        let mut returned_val = match response.take::<Option<T>>(0) {
+            Ok(one) => vec![one.unwrap()],
+            Err(err) => response.take::<Vec<T>>(0)?,
+        };
+
+        // TODO:: Handle error if nothing is returned
+        let only_or_last = returned_val.pop().unwrap();
+        Ok(only_or_last)
+    }
+
+    async fn return_many<M: Into<Vec<T>>>(&self, db: Surreal<Db>) -> surrealdb::Result<M> {
+        let query = self.build();
+        let query = db.query(query);
+
+        let mut query = self.get_bindings().iter().fold(query, |acc, val| {
+            acc.bind((val.get_param(), val.get_value()))
+        });
+
+        let mut response = query.await?;
+        // This does the reverse of get_one
+        // If it errors, try to check if only single entry has been inputed, hence, suurealdb
+        // trying to return Option<T>, then pick the return the only item as Vec<T>.
+        let mut returned_val = match response.take::<Vec<T>>(0) {
+            Ok(many) => many,
+            Err(err) => vec![response.take::<Option<T>>(0)?.unwrap()],
+        };
+
+        // TODO:: Handle error if nothing is returned
+        Ok(returned_val)
+    }
 }
 
 #[async_trait::async_trait]
