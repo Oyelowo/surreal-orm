b15da077dca0f2eafee938e913999e4a249044ea
oyelowo <oyelowooyedayo@gmail.com>
2023-04-11 09:52:31 +0300

    Cleanup attributes module

--- rust/surrealdb-derive/src/models/attributes.rs
+++ rust/surrealdb-derive/src/models/attributes.rs
@@ -777,25 +777,15 @@ impl ReferencedNodeMeta {
                 pub fn #normalized_field_name(self, clause: impl Into<#crate_name::NodeClause>) -> #schema_type_ident {
                      let clause: #crate_name::NodeClause = clause.into();
                     let normalized_field_name_str = if self.get_connection().is_empty(){
-                        // #normalized_field_name_str.to_string()
-                        // "".to_string()
-                        // clause.with_field(#normalized_field_name_str.into())
                         #normalized_field_name_str.to_string()
                     }else {
-                        // format!("{}.{}",self.get_connection(), #normalized_field_name_str)
-                        // self.get_connection()
-                        // clause.with_field(.#normalized_field_name_str.into()),
                         format!(".{}", #normalized_field_name_str)
                     };
 
 
                     #schema_type_ident::#__________connect_node_to_graph_traversal_string(
-                        // store,
                         self,
                         clause.with_field(normalized_field_name_str)
-                        // false,
-                        // self.get_bindings(),
-                        // self.get_errors()
                     )
 
                 }
@@ -840,22 +830,17 @@ impl ReferencedNodeMeta {
 
             record_link_default_alias_as_method: quote!(
                 pub fn #normalized_field_name(self, clause: impl Into<#crate_name::NestedClause>) -> #schema_type_ident {
-                    let clause: #crate_name::NestedClause = clause.into();
-                    // let store = if self.get_connection().is_empty(){
-                    //     // #normalized_field_name_str.to_string()
-                    //     "".to_string()
-                    // }else {
-                    //     // format!("{}.{}",self.get_connection(), #normalized_field_name_str)
-                    //     self.get_connection()
-                    // };
+                     let clause: #crate_name::NestedClause = clause.into();
+                    let normalized_field_name_str = if self.get_connection().is_empty(){
+                        #normalized_field_name_str.to_string()
+                    }else {
+                        format!(".{}", #normalized_field_name_str)
+                    };
+
 
                     #schema_type_ident::#__________connect_object_to_graph_traversal_string(
-                        // store,
                         self,
-                        clause.with_field(#normalized_field_name_str.into()),
-                        // false,
-                        // self.get_bindings(),
-                        // self.get_errors()
+                        clause.with_field(normalized_field_name_str)
                     )
 
                 }
