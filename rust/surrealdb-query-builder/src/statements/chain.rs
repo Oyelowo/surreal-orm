388e0125443cb1521c76047634f8468fbbaacd12
oyelowo <oyelowooyedayo@gmail.com>
2023-04-12 23:36:06 +0300

    Add initial documentation for chain helper statement

--- rust/surrealdb-query-builder/src/statements/chain.rs
+++ rust/surrealdb-query-builder/src/statements/chain.rs
@@ -9,6 +9,25 @@ use std::fmt::{self, Display};
 
 use crate::traits::{BindingsList, Buildable, Erroneous, ErrorList, Parametric, Queryable};
 
+/// Chains together multiple queries into a single `QueryChain`.
+///
+/// # Arguments
+///
+/// * `query` - The first query in the chain. Subsequent queries can be added using the `chain` method on the returned `QueryChain`.
+///
+/// # Example Usage
+///
+/// ```rust
+/// use surrealdb_query_builder::{chain, SurrealId, statements::select, All, Field, cond};
+///
+/// let user_id = Field::new("user_id");
+/// let age = Field::new("age");
+///
+/// let query1 = select(All).from(users).where_(cond(age.gt(30)));
+/// let query2 = select(All).from(orders).where_(cond(user_id.eq(123)));
+///
+/// let query_chain = chain(query1).chain(query2);
+/// ```
 pub fn chain(query: impl Queryable + Parametric + Display) -> QueryChain {
     QueryChain {
         queries: vec![query.to_string()],
@@ -24,6 +43,42 @@ pub struct QueryChain {
 }
 
 impl QueryChain {
+    /// Appends a new query to the end of the chain.
+    ///
+    /// This method takes a query that implements the `Queryable`, `Parametric`, and `Display` traits
+    /// and appends it to the end of the query chain. The `get_bindings()` and `get_errors()` methods of
+    /// the provided query are called to retrieve its bindings and errors, which are then merged with
+    /// those of the current query chain. The string representation of the query is also added to the
+    /// list of queries in the chain.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use surrealdb_query_builder::{
+    ///     Queryable,
+    ///     Parametric,
+    ///     chain,
+    ///     statements::select,
+    ///     SurrealId,
+    ///     All
+    /// };
+    ///
+    /// // Create a query chain with a single query
+    /// let user_lowo = SurrealId::try_from("user:oyelowo").unwrap();
+    /// let user_dayo = SurrealId::try_from("user:oyedayo").unwrap();
+    ///
+    /// // Append a new query to the chain
+    /// let query1 = select(All).from(user_lowo);
+    /// let query2 = select(All).from(user_dayo).limit(10);
+    /// let chain = chain(query1).chain(query2);
+    ///
+    /// // The resulting chain contains both queries
+    /// assert_eq!(chain.build(), "SELECT * FROM users;\nSELECT * FROM orders LIMIT 10;");
+    /// ```
+    ///
+    /// # Panics
+    ///
+    /// This method does not panic.
     pub fn chain(mut self, query: impl Queryable + Parametric + Display) -> Self {
         self.bindings.extend(query.get_bindings());
         self.errors.extend(query.get_errors());
