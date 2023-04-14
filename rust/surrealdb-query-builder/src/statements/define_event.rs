432f6b2979f2a34882b327552e7be7a45ad36fb1
oyelowo <oyelowooyedayo@gmail.com>
2023-04-14 14:34:44 +0300

    Update and document define event function

--- rust/surrealdb-query-builder/src/statements/define_event.rs
+++ rust/surrealdb-query-builder/src/statements/define_event.rs
@@ -5,18 +5,12 @@
  * Licensed under the MIT license
  */
 
-use std::{
-    fmt::{self, Display},
-    ops::Deref,
-};
-
-use insta::{assert_debug_snapshot, assert_display_snapshot};
-use serde::{de::DeserializeOwned, Deserialize, Serialize};
-use surrealdb::sql::{self, statements::DefineStatement};
+use std::fmt::{self, Display};
 
 use crate::{
-    traits::{Binding, BindingsList, Buildable, Erroneous, Parametric, Queryable, Runnable},
+    traits::{Buildable, Erroneous, Parametric, Queryable},
     types::{Event, Filter, Table},
+    Conditional, BindingsList,
 };
 
 // DEFINE EVENT statement
@@ -43,12 +37,60 @@ pub struct EventBuilder {
     bindings: BindingsList,
 }
 
+/**
+Define a new event.
+Events can be triggered after any change or modification to the data in a record. Each trigger is able to see the $before and $after value of the record, enabling advanced custom logic with each trigger.
+
+Requirements
+You must be authenticated as a root, namespace, or database user before you can use the DEFINE EVENT statement.
+You must select your namespace and database before you can use the DEFINE EVENT statement.
+
+# Example
+```rust
+ # use surrealdb_query_builder as surrealdb_orm;
+use surrealdb_orm::{*, statements::{define_event, select}};
+
+# let age = Field::new("age");
+# let city = Field::new("city");
+# let fake_id = SurrealId::try_from("user:oyelowo").unwrap();
+# let user_table = Table::new("user");
+# let email_event = Event::new("email");
+ let query = define_event(email_event)
+    .on_table(user_table)
+     .when(age.greater_than_or_equal(18))
+     .then(
+         select(All)
+             .from(fake_id)
+             .where_(
+                 cond(city.is("Prince Edward Island"))
+                     .and(city.is("NewFoundland"))
+                     .or(city.like("Toronto")),
+             )
+             .limit(153)
+             .start(10)
+             .parallel(),
+     );
+
+ assert!(!query.build().is_empty());
+ assert_eq!(
+     query.fine_tune_params(),
+     "DEFINE EVENT email ON TABLE user WHEN age >= $_param_00000001 \
+THEN SELECT * FROM $_param_00000002 WHERE (city IS $_param_00000003) AND (city IS $_param_00000004) OR (city ~ $_param_00000005) LIMIT $_param_00000006 START AT $_param_00000007 PARALLEL;",
+);
+
+ assert_eq!(
+     query.to_raw().build(),
+     "DEFINE EVENT email ON TABLE user WHEN age >= 18 THEN SELECT * FROM user:oyelowo WHERE (city IS 'Prince Edward Island') AND (city IS 'NewFoundland') OR \
+(city ~ 'Toronto') LIMIT 153 START AT 10 PARALLEL;",
+);
+```
+*/
 pub fn define_event(event_name: impl Into<Event>) -> EventBuilder {
     EventBuilder::new(event_name)
 }
 
 impl EventBuilder {
-    // Set the event name
+    /// Set the event name
     fn new(event_name: impl Into<Event>) -> Self {
         Self {
             event: event_name.into().to_string(),
@@ -59,15 +101,15 @@ impl EventBuilder {
         }
     }
 
-    // Set the event table
+    /// Set the event table
     pub fn on_table(mut self, table: impl Into<Table>) -> Self {
         self.on_table = Some(table.into().to_string());
         self
     }
 
-    // Set the event trigger
-    pub fn when(mut self, condition: impl Into<Filter>) -> Then {
-        let cond: Filter = condition.into();
+    /// Set the event trigger
+    pub fn when(mut self, condition: impl Conditional) -> Then {
+        let cond = Filter::new(condition);
         self.when = Some(format!("{}", &cond));
         self.bindings.extend(cond.get_bindings());
         Then(self)
@@ -84,6 +126,7 @@ impl Then {
     }
 }
 
+/// A statement for defining an event.
 pub struct DefineEventStatement(EventBuilder);
 
 // DEFINE EVENT @name ON [ TABLE ] @table WHEN @expression THEN @expression
@@ -124,26 +167,15 @@ impl Queryable for DefineEventStatement {}
 impl Erroneous for DefineEventStatement {}
 
 #[cfg(test)]
-#[cfg(feature = "mock")]
 mod tests {
-
-    use std::time::Duration;
-
-    use crate::{
-        sql::{All, SurrealId, Table},
-        statements::select,
-        utils::cond,
-        Field, Operatable,
-    };
-
     use super::*;
+    use crate::{statements::select, *};
 
     #[test]
     fn test_define_event_statement_state_machine() {
         let age = Field::new("age");
         let city = Field::new("city");
         let fake_id = SurrealId::try_from("user:oyelowo").unwrap();
-        // TODO: Probably use try_from_converter for String<->Table and for other types
         let user_table = Table::new("user");
         let email_event = Event::new("email");
 
@@ -164,9 +196,16 @@ mod tests {
             );
 
         assert_eq!(
-            query.to_string(),
-            "DEFINE EVENT email ON TABLE user WHEN age >= $_param_00000000 THEN SELECT * FROM $_param_00000000 WHERE (city IS $_param_00000000) AND (city IS $_param_00000000) OR (city ~ $_param_00000000) LIMIT 153 START AT 10 PARALLEL;",
+            query.fine_tune_params(),
+            "DEFINE EVENT email ON TABLE user WHEN age >= $_param_00000001 THEN SELECT * FROM $_param_00000002 WHERE (city IS $_param_00000003) AND (city IS $_param_00000004) OR (city ~ $_param_00000005) LIMIT $_param_00000006 START AT $_param_00000007 PARALLEL;",
         );
-        insta::assert_debug_snapshot!(query.get_bindings());
+
+        assert_eq!(
+            query.to_raw().build(),
+            "DEFINE EVENT email ON TABLE user \
+                WHEN age >= 18 THEN \
+                SELECT * FROM user:oyelowo WHERE (city IS 'Prince Edward Island') AND \
+                (city IS 'NewFoundland') OR (city ~ 'Toronto') LIMIT 153 START AT 10 PARALLEL;",
+        );
     }
 }
