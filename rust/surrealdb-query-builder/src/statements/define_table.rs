571b9ce1750025e2d7f8125b6fa23b4ef917226b
oyelowo <oyelowooyedayo@gmail.com>
2023-04-15 15:45:13 +0300

    Update and fix define_table tests

--- rust/surrealdb-query-builder/src/statements/define_table.rs
+++ rust/surrealdb-query-builder/src/statements/define_table.rs
@@ -101,6 +101,7 @@ use super::{for_::PermissionType, select::SelectStatement};
 // ;
 //
 
+/// Define the API for the Table builder
 pub struct DefineTableStatement {
     table_name: String,
     drop: Option<bool>,
@@ -112,6 +113,55 @@ pub struct DefineTableStatement {
     bindings: BindingsList,
 }
 
+/// Define a new table.
+///
+/// The DEFINE TABLE statement allows you to declare your table by name,
+/// enabling you to apply strict controls to a table's schema by making it SCHEMAFULL,
+/// create a foreign table view, and set permissions specifying what operations can be performed on the field.
+///
+/// Requirements
+/// You must be authenticated as a root, namespace, or database user before you can use the DEFINE TABLE statement.
+/// You must select your namespace and database before you can use the DEFINE TABLE statement.
+///
+/// Examples:
+///
+/// ```rust
+/// # use surrealdb_query_builder as surrealdb_orm;
+/// use surrealdb_orm::{*, functions::crypto, statements::{define_table, for_, order, select}};
+/// use CrudType::*;
+/// use std::time::Duration;
+///
+/// let name = Field::new("name");
+/// let user_table = Table::from("user");
+/// let age = Field::new("age");
+/// let country = Field::new("country");
+/// let fake_id2 = SurrealId::try_from("user:oyedayo").unwrap();
+///
+/// let statement = define_table(user_table)
+///     .drop()
+///     .as_(
+///         select(All)
+///             .from(fake_id2)
+///             .where_(country.is("INDONESIA"))
+///             .order_by(order(&age).numeric().desc())
+///             .limit(20)
+///             .start(5),
+///     )
+///     .schemafull()
+///     .permissions_for(for_(Select).where_(age.greater_than_or_equal(18))) // Single works
+///     .permissions_for(for_(&[Create, Delete]).where_(name.is("Oyedayo"))) //Multiple
+///     .permissions_for(&[
+///         for_(&[Create, Delete]).where_(name.is("Oyedayo")),
+///         for_(Update).where_(age.less_than_or_equal(130)),
+///     ]);
+///
+/// assert_eq!(
+///     statement.to_string(),
+///     "DEFINE TABLE user DROP SCHEMAFULL AS \n\tSELECT * FROM $_param_00000000 WHERE country IS $_param_00000000 ORDER BY age NUMERIC DESC LIMIT 20 START AT 5;\nPERMISSIONS\nFOR select\n\tWHERE age >= $_param_00000000\nFOR create, delete\n\tWHERE name IS $_param_00000000\nFOR create, delete\n\tWHERE name IS $_param_00000000\nFOR update\n\tWHERE age <= $_param_00000000;".to_string()
+/// );
+///
+/// assert!(!statement.build().is_empty());
+/// ```
 pub fn define_table(table_name: impl Into<Table>) -> DefineTableStatement {
     let table: Table = table_name.into();
     DefineTableStatement {
@@ -258,50 +308,51 @@ enum SchemaType {
 }
 
 #[cfg(test)]
-#[cfg(feature = "mock")]
 mod tests {
-
     use super::*;
-    use std::time::Duration;
 
     use crate::{
-        query_for::ForCrudType,
-        sql::{All, SurrealId},
         statements::{for_, order, select},
-        Field, Operatable,
+        *,
     };
-
-    use super::*;
+    use CrudType::*;
 
     #[test]
     fn test_define_statement_schemaless_permissions_none() {
-        let user_table = Table::from("user");
-        let statement = define_table(user_table).schemaless().permissions_none();
+        let user = Table::from("user");
+        let statement = define_table(user).schemaless().permissions_none();
 
         assert_eq!(
-            statement.to_string(),
+            statement.fine_tune_params(),
             "DEFINE TABLE user SCHEMALESS PERMISSIONS NONE;"
         );
-        insta::assert_display_snapshot!(statement);
-        insta::assert_debug_snapshot!(statement.get_bindings());
+        assert_eq!(
+            statement.to_raw().build(),
+            "DEFINE TABLE user SCHEMALESS PERMISSIONS NONE;"
+        );
+        assert_eq!(statement.get_bindings().len(), 0);
     }
 
     #[test]
     fn test_define_statement_schemaless() {
-        let user_table = Table::from("user");
-        let statement = define_table(user_table).schemaless().permissions_full();
+        let user = Table::from("user");
+        let statement = define_table(user).schemaless().permissions_full();
 
         assert_eq!(
-            statement.to_string(),
+            statement.fine_tune_params(),
             "DEFINE TABLE user SCHEMALESS PERMISSIONS FULL;"
         );
-        insta::assert_display_snapshot!(statement);
-        insta::assert_debug_snapshot!(statement.get_bindings());
+
+        assert_eq!(
+            statement.to_raw().build(),
+            "DEFINE TABLE user SCHEMALESS PERMISSIONS FULL;"
+        );
+
+        assert_eq!(statement.get_bindings().len(), 0);
     }
 
     #[test]
     fn test_define_statement_multiple() {
-        use ForCrudType::*;
         let name = Field::new("name");
         let user_table = Table::from("user");
         let age = Field::new("age");
@@ -327,10 +378,23 @@ mod tests {
             ]);
 
         assert_eq!(
-            statement.to_string(),
-            "DEFINE TABLE user DROP SCHEMAFULL AS \n\tSELECT * FROM $_param_00000000 WHERE country IS $_param_00000000 ORDER BY age NUMERIC DESC LIMIT 20 START AT 5;\nPERMISSIONS\nFOR select\n\tWHERE age >= $_param_00000000\nFOR create, delete\n\tWHERE name IS $_param_00000000\nFOR create, delete\n\tWHERE name IS $_param_00000000\nFOR update\n\tWHERE age <= $_param_00000000;".to_string()
+            statement.fine_tune_params(),
+            "DEFINE TABLE user DROP SCHEMAFULL AS \n\tSELECT * FROM $_param_00000001 \
+                WHERE country IS $_param_00000002 ORDER BY age NUMERIC DESC \
+                LIMIT $_param_00000003 START AT $_param_00000004\nPERMISSIONS\n\
+                FOR select\n\tWHERE age >= $_param_00000005\nFOR create, delete\n\tWHERE name IS $_param_00000006\n\
+                FOR create, delete\n\tWHERE name IS $_param_00000007\nFOR update\n\t\
+                WHERE age <= $_param_00000008;"
         );
-        insta::assert_display_snapshot!(statement);
-        insta::assert_debug_snapshot!(statement.get_bindings());
+
+        assert_eq!(
+            statement.to_raw().build(),
+            "DEFINE TABLE user DROP SCHEMAFULL AS \n\tSELECT * FROM user:oyedayo \
+                WHERE country IS 'INDONESIA' ORDER BY age NUMERIC DESC LIMIT 20 \
+                START AT 5\nPERMISSIONS\nFOR select\n\tWHERE age >= 18\n\
+                FOR create, delete\n\tWHERE name IS 'Oyedayo'\n\
+                FOR create, delete\n\tWHERE name IS 'Oyedayo'\nFOR update\n\tWHERE age <= 130;"
+        );
+        assert_eq!(statement.get_bindings().len(), 8);
     }
 }
