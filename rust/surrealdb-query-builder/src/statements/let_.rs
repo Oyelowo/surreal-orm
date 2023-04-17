04713dc0b6603fa6fc93b3f328d65ac2c2eddd2a
oyelowo <oyelowooyedayo@gmail.com>
2023-04-17 17:23:44 +0300

    Document let statement

--- rust/surrealdb-query-builder/src/statements/let_.rs
+++ rust/surrealdb-query-builder/src/statements/let_.rs
@@ -7,18 +7,37 @@
 
 use std::fmt::Display;
 
-use surrealdb::sql::{self, Ident};
-
 use crate::{
-    traits::{BindingsList, Buildable, Erroneous, Parametric, Queryable, Runnable},
+    traits::{BindingsList, Buildable, Erroneous, Parametric, Queryable},
     types::expression::Expression,
     Param,
 };
 
+/// Builds LET statement.
+///
+/// The LET statement sets and stores a value which can then be used in a subsequent query.
+/// A parameter can store any value, including the result of a query.
+///
+/// Examples
+/// ```rust
+/// # use surrealdb_query_builder as surrealdb_orm;
+/// use surrealdb_orm::{*, statements::{let_, select}};
+/// # let user = Table::new("user");
+/// // You can assign a value surrealdb value
+/// let_("name").equal(5);
+///
+/// // and even a select statement
+/// let_("users").equal(select(All).from(user));
 pub fn let_(parameter: impl Into<Param>) -> LetStatement {
-    LetStatement::new(parameter)
+    let param: Param = parameter.into();
+    LetStatement {
+        value: None,
+        bindings: vec![],
+        parameter: param,
+    }
 }
 
+/// Let statement builder
 pub struct LetStatement {
     parameter: Param,
     value: Option<Expression>,
@@ -26,14 +45,19 @@ pub struct LetStatement {
 }
 
 impl LetStatement {
-    pub fn new(parameter: impl Into<Param>) -> Self {
-        let param: Param = parameter.into();
-        Self {
-            value: None,
-            bindings: vec![],
-            parameter: param,
-        }
-    }
+    /// Assigned expression
+    /// Examples
+    /// ```rust
+    /// # use surrealdb_query_builder as surrealdb_orm;
+    /// use surrealdb_orm::{*, statements::{let_, select}};
+    /// # let user = Table::new("user");
+    /// // You can assign a value surrealdb value
+    /// # let_("name")
+    /// .equal(5);
+    ///
+    /// // and even a select statement
+    /// # let_("users")
+    /// .equal(select(All).from(user));
     pub fn equal(mut self, value: impl Into<Expression>) -> Self {
         let value: Expression = value.into();
         self.bindings.extend(value.get_bindings());
@@ -41,6 +65,7 @@ impl LetStatement {
         self
     }
 
+    /// helper function for getting assigned param from the LET statement
     pub fn get_param(&self) -> Param {
         self.parameter.clone()
     }
@@ -48,7 +73,7 @@ impl LetStatement {
 
 impl Buildable for LetStatement {
     fn build(&self) -> String {
-        let mut query = format!("LET {}", self.get_param());
+        let mut query = format!("LET {}", self.get_param().build());
 
         if let Some(value) = &self.value {
             query = format!("{query} = {};", value.build());
@@ -74,18 +99,40 @@ impl Queryable for LetStatement {}
 impl Erroneous for LetStatement {}
 
 #[cfg(test)]
-#[cfg(feature = "mock")]
 mod tests {
+    use crate::{statements::select, All, Table, ToRaw};
 
     use super::*;
 
     #[test]
     fn test_let_statement() {
+        let statement = let_("name").equal(5);
+
         assert_eq!(
-            let_("name").equal(5).build(),
-            "LET $name = _param_00000000;"
+            statement.fine_tune_params(),
+            "LET $name = $_param_00000001;"
         );
 
-        assert_eq!(let_("name".to_string()).equal(5).get_param(), "$name");
+        assert_eq!(statement.to_raw().build(), "LET $name = 5;");
+
+        assert_eq!(statement.get_param().build(), "$name");
+    }
+
+    #[test]
+    fn test_let_statement_with_select_statement() {
+        let user = Table::new("user");
+        let statement = let_("name").equal(select(All).from(user));
+
+        assert_eq!(
+            statement.fine_tune_params(),
+            "LET $name = SELECT * FROM user;"
+        );
+
+        assert_eq!(
+            statement.to_raw().build(),
+            "LET $name = SELECT * FROM user;"
+        );
+
+        assert_eq!(statement.get_param().build(), "$name");
     }
 }
