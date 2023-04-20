37bc6785081822500fe47d0d6ad9526855449ee6
oyelowo <oyelowooyedayo@gmail.com>
2023-04-20 23:04:44 +0300

    Add rust doc for filter

--- rust/surrealdb-query-builder/src/types/filter.rs
+++ rust/surrealdb-query-builder/src/types/filter.rs
@@ -13,6 +13,29 @@ use crate::traits::{
 
 use super::Empty;
 
+/// Creates a new filter from a given `filterable` input.
+///
+/// # Arguments
+///
+/// * `filterable` - A value that can be converted into a `Filter`.
+///
+/// # Example
+///
+/// ```rust
+/// # use surrealdb_query_builder as surrealdb_orm;
+/// # use surrealdb_orm::*;
+/// # let age = Field::new("age");
+/// # let name = Field::new("name");
+/// # let title = Field::new("title");
+///
+/// let filter = cond(age.greater_than(18))
+///                 .and(name.like("%Oyelowo%"))
+///                 .or(title.equal("Professor"));
+/// ```
+pub fn cond(filterable: impl Conditional) -> Filter {
+    Filter::new(filterable)
+}
+
 /// This module provides functionality for building complex filters for database queries.
 ///
 /// A `Filter` struct represents a filter that can be composed of subfilters using logical
@@ -24,27 +47,6 @@ use super::Empty;
 ///
 /// Methods on a `Filter` instance are used to combine filters with logical operators or to
 /// modify the filter using methods like `bracketed`.
-///
-/// # Examples
-///
-/// ```
-/// use crate::query::filter::{Filter, cond};
-///
-/// let filter1 = Filter::new("name = 'John'".to_string());
-/// let filter2 = Filter::new("age > 18".to_string());
-///
-/// // Combine two filters using the 'AND' operator
-/// let combined_filter = filter1.and(filter2);
-///
-/// assert_eq!(combined_filter.to_string(), "(name = 'John') AND (age > 18)");
-///
-/// // Create a filter from a string
-/// let filter3 = Filter::new("name like '%Doe%'".to_string());
-///
-/// // Combine multiple filters using the 'OR' operator
-/// let all_filters = cond(filter1).or(filter2).or(filter3);
-///
-/// assert_eq!(all_filters.to_string(), "(name = 'John') OR (age > 18) OR (name like '%Doe%')");
 /// ```
 #[derive(Debug, Clone, Default)]
 pub struct Filter {
@@ -52,95 +54,11 @@ pub struct Filter {
     bindings: BindingsList,
 }
 
-impl Buildable for Filter {
-    fn build(&self) -> String {
-        self.query_string.to_string()
-    }
-}
-
-impl Conditional for Filter {}
-
-impl From<Empty> for Filter {
-    fn from(value: Empty) -> Self {
-        Filter::new(Empty)
-    }
-}
-
-impl Parametric for Filter {
-    fn get_bindings(&self) -> BindingsList {
-        self.bindings.to_vec()
-    }
-}
-///
-/// Creates a new filter from a given `filterable` input.
-///
-/// # Arguments
-///
-/// * `filterable` - A value that can be converted into a `Filter`.
-///
-/// # Example
-///
-/// ```
-/// use crate::query::filter::{Filter, cond};
-///
-/// let filter = Filter::new("name = 'John'".to_string());
-///
-/// let combined_filter = cond(filter).and("age > 18");
-///
-/// assert_eq!(combined_filter.to_string(), "(name = 'John') AND (age > 18)");
-/// ```
-pub fn cond(filterable: impl Conditional) -> Filter {
-    Filter::new(filterable)
-}
-
-// /// Creates an empty filter.
-// ///
-// /// # Example
-// ///
-// /// ```
-// /// use crate::query::filter::Filter;
-// ///
-// /// let empty_filter = Filter::empty();
-// ///
-// /// assert_eq!(empty_filter.to_string(), "");
-// ///
-// pub fn empty() -> Empty {
-//     Empty
-// }
-
-impl Erroneous for Filter {
-    fn get_errors(&self) -> Vec<String> {
-        vec![]
-    }
-}
-
 impl Filter {
     /// Creates a new `Filter` instance.
-    ///
-    /// # Arguments
-    ///
-    /// * `query_string` - The query string used to initialize the filter.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use crate::query::filter::Filter;
-    ///
-    /// let filter = Filter::new("name = 'John'".to_string());
-    ///
-    /// assert_eq!(filter.to_string(), "name = 'John'");
-    /// ```
-    // pub fn new(query_string: String) -> Self {
     pub fn new(query: impl Conditional) -> Self {
-        let query_string = format!("{}", query.build());
-        // let query_string = if query_string.is_empty() {
-        //     "".into()
-        // } else {
-        //     format!("({query_string})")
-        // };
-
         Self {
-            query_string,
+            query_string: query.build(),
             bindings: query.get_bindings(),
         }
     }
@@ -153,14 +71,13 @@ impl Filter {
     ///
     /// # Example
     ///
-    /// ```
-    /// use crate::query::filter::{Filter, cond};
-    ///
-    /// let filter = cond(Filter::new("name = 'John'".to_string())).or(
-    ///     cond(Filter::new("age > 30".to_string()))
-    /// );
-    ///
-    /// assert_eq!(filter.to_string(), "(name = 'John') OR (age > 30)");
+    /// ```rust
+    /// # use surrealdb_query_builder as surrealdb_orm;
+    /// # use surrealdb_orm::*;
+    /// # let age = Field::new("age");
+    /// # let title = Field::new("title");
+    /// cond(age.greater_than(18))
+    ///     .or(title.equal("Professor"));
     /// ```
     pub fn or(self, filter: Operation) -> Self {
         let precendence = self._______bracket_if_not_already();
@@ -183,14 +100,13 @@ impl Filter {
     ///
     /// # Example
     ///
-    /// ```
-    /// use crate::query::filter::{Filter, cond};
-    ///
-    /// let filter1 = cond(Filter::new("name = 'John'"));
-    /// let filter2 = cond(Filter::new("age > 30"));
-    /// let combined = filter1.and(filter2);
-    ///
-    /// assert_eq!(combined.to_string(), "(name = 'John') AND (age > 30)");
+    /// ```rust
+    /// # use surrealdb_query_builder as surrealdb_orm;
+    /// # use surrealdb_orm::*;
+    /// # let age = Field::new("age");
+    /// # let title = Field::new("title");
+    /// cond(age.greater_than(18))
+    ///     .and(title.equal("Professor"));
     /// ```
     pub fn and(self, filter: impl Buildable + Parametric) -> Self {
         let precendence = self._______bracket_if_not_already();
@@ -205,31 +121,20 @@ impl Filter {
         }
     }
 
-    pub(crate) fn ___update_bindings(self, filter: &impl Parametric) -> Vec<Binding> {
-        // let new_params = self
-        //     .params
-        //     .to_owned()
-        //     .into_iter()
-        //     .chain(filter.get_params().into_iter())
-        //     .collect::<Vec<_>>(); // Consumed
-        // let mut new_bindings = vec![];
-        // new_bindings.extend(self.bindings);
-        // new_bindings.extend(filter.get_bindings());
-        // new_bindings
-        [self.bindings.as_slice(), filter.get_bindings().as_slice()].concat()
-    }
-
     /// Wraps this `Filter` instance in a set of brackets.
     ///
     /// # Example
     ///
-    /// ```
-    /// use crate::query::filter::{Filter, cond};
+    /// ```rust
+    /// # use surrealdb_query_builder as surrealdb_orm;
+    /// # use surrealdb_orm::*;
+    /// # let age = Field::new("age");
+    /// # let title = Field::new("title");
     ///
-    /// let filter = cond(Filter::new("name = 'John'")).or(cond(Filter::new("age > 30")));
-    /// let wrapped = filter.bracketed();
-    ///
-    /// assert_eq!(wrapped.to_string(), "((name = 'John') OR (age > 30))");
+    /// let filter = cond(age.greater_than(18))
+    ///     .or(title.equal("Professor"));
+    /// let bracketed_filter = filter.bracketed();
+    /// assert_eq!(bracketed_filter.to_raw().build(), "((age > 18) OR (title = 'Professor'))");
     /// ```
     pub fn bracketed(&self) -> Self {
         Filter {
@@ -245,6 +150,10 @@ impl Filter {
             (true, true) => format!("{self}"),
             _ => format!("({self})"),
         }
+    }
+
+    pub(crate) fn ___update_bindings(self, filter: &impl Parametric) -> Vec<Binding> {
+        [self.bindings.as_slice(), filter.get_bindings().as_slice()].concat()
     }
 }
 
@@ -275,8 +184,34 @@ impl From<String> for Filter {
     }
 }
 
+impl Buildable for Filter {
+    fn build(&self) -> String {
+        self.query_string.to_string()
+    }
+}
+
+impl Conditional for Filter {}
+
+impl From<Empty> for Filter {
+    fn from(_value: Empty) -> Self {
+        Filter::new(Empty)
+    }
+}
+
+impl Erroneous for Filter {
+    fn get_errors(&self) -> Vec<String> {
+        vec![]
+    }
+}
+
+impl Parametric for Filter {
+    fn get_bindings(&self) -> BindingsList {
+        self.bindings.to_vec()
+    }
+}
+
 impl std::fmt::Display for Filter {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("{}", self.query_string))
+        write!(f, "{}", self.build())
     }
 }
