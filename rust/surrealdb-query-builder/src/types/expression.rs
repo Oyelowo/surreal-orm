ebc2fdf7b26eee3b847549118f53cea25f8eec50
oyelowo <oyelowooyedayo@gmail.com>
2023-04-20 21:32:56 +0300

    Simplify expression

--- rust/surrealdb-query-builder/src/types/expression.rs
+++ rust/surrealdb-query-builder/src/types/expression.rs
@@ -5,84 +5,33 @@
  * Licensed under the MIT license
  */
 
-use std::fmt::{self, Display};
-
-use surrealdb::sql;
-
-use crate::{
-    statements::SelectStatement,
-    traits::{Binding, BindingsList, Parametric},
-    Buildable,
-};
+use crate::{statements::SelectStatement, Buildable, Parametric, Valuex};
 
 #[derive(Clone, Debug)]
-pub struct ValueWithBinding {
-    value: String,
-    binding: Binding,
-}
+pub struct Expression(Valuex);
 
-// impl Buildable for ValueWithBinding {
-//     fn build(&self) -> String {
-//         self.binding.to_vec
-//     }
-// }
-#[derive(Clone, Debug)]
-pub enum Expression {
-    SelectStatement(SelectStatement),
-    Value(ValueWithBinding),
-}
+impl std::ops::Deref for Expression {
+    type Target = Valuex;
 
-impl Buildable for Expression {
-    fn build(&self) -> String {
-        match self {
-            Expression::SelectStatement(s) => s.build().trim_end_matches(";").to_string(),
-            // Expression::SelectStatement(s) => format!("( {} )", s.build().trim_end_matches(";")),
-            Expression::Value(v) => v.binding.get_param_dollarised(),
-        }
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
 
-// impl Display for Expression {
-//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//         let expression = match self {
-//             Expression::SelectStatement(s) => s.build(),
-//             // Expression::SelectStatement(s) => s.get_bindings().first().unwrap().get_raw(),
-//             Expression::Value(v) => {
-//                 // let bindings = self.get_bindings();
-//                 let bindings = v.to_string();
-//                 // assert_eq!(bindings.len(), 1);
-//                 format!("{}", self.get_bindings().first().expect("Param must have been generated for value. This is a bug. Please report here: ").get_param())
-//             }
-//         };
-//         write!(f, "{}", expression)
-//     }
-// }
-//
-impl Parametric for Expression {
-    fn get_bindings(&self) -> BindingsList {
-        match self {
-            Expression::SelectStatement(s) => s.get_bindings(),
-            Expression::Value(sql_value) => {
-                // let sql_value = sql::json(&serde_json::to_string(&v).unwrap()).unwrap();
-                // let sql_value: sql::Value = sql_value.to_owned();
-                vec![sql_value.binding.clone()]
-            }
-        }
+impl<T> From<T> for Expression
+where
+    T: Into<Valuex>,
+{
+    fn from(value: T) -> Self {
+        Expression(value.into())
     }
 }
 
 impl From<SelectStatement> for Expression {
-    fn from(value: SelectStatement) -> Self {
-        Self::SelectStatement(value)
-    }
-}
-
-impl<T: Into<sql::Value>> From<T> for Expression {
-    fn from(value: T) -> Self {
-        let binding = Binding::new(value.into());
-        Self::Value(ValueWithBinding {
-            value: binding.get_param_dollarised(),
-            binding,
+    fn from(select_statement: SelectStatement) -> Self {
+        Expression(Valuex {
+            string: format!("( {} )", select_statement.build().trim_end_matches(";")),
+            bindings: select_statement.get_bindings(),
         })
     }
 }
