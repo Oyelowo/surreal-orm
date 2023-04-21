e25465a8f837049586de293762b8a4814b2e46ca
oyelowo <oyelowooyedayo@gmail.com>
2023-04-21 14:22:55 +0300

    Add rust doc for general traits

--- rust/surrealdb-query-builder/src/traits/general.rs
+++ rust/surrealdb-query-builder/src/traits/general.rs
@@ -5,13 +5,14 @@
  * Licensed under the MIT license
  */
 
-use std::fmt::Display;
-
 use super::{Erroneous, Parametric};
 
+/// A trait for building a query string
 pub trait Buildable {
+    /// Build a query string
     fn build(&self) -> String;
 
+    /// Make query string param consistent. Useful in testing.
     fn fine_tune_params(&self) -> String {
         // replace_params(&self.build())
         let mut count = 0;
@@ -24,10 +25,12 @@ pub trait Buildable {
     }
 }
 
-// pub trait Queryable: Parametric + Buildable + Display + Erroneous {}
+/// Used for statements
 pub trait Queryable: Parametric + Buildable + Erroneous {}
 
+/// Used for filters
 pub trait Conditional: Parametric + Buildable + Erroneous {
+    /// Get query string of the filter
     fn get_condition_query_string(&self) -> String {
         self.build()
     }
