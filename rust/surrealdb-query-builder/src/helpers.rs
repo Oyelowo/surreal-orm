ee16e4f07716b18762f89499eae7cbf10624424c
oyelowo <oyelowooyedayo@gmail.com>
2023-04-21 22:06:30 +0300

    Add rust doc to helpers and lib

--- rust/surrealdb-query-builder/src/helpers.rs
+++ rust/surrealdb-query-builder/src/helpers.rs
@@ -1,9 +1,11 @@
 use surrealdb::sql;
 
+/// A macro to create a surrealdb::sql::Value from a value
 pub fn val(val: impl Into<sql::Value>) -> sql::Value {
     val.into()
 }
 
+/// A macro to create a vector of heterogeous/diverse surrealdb value
 #[macro_export]
 macro_rules! array {
     ($( $val:expr ),*) => {{
--- rust/surrealdb-query-builder/src/lib.rs
+++ rust/surrealdb-query-builder/src/lib.rs
@@ -16,6 +16,8 @@ mod errors;
 pub mod functions;
 mod helpers;
 mod operators_macros;
+/// This module contains the different types of statements that can be used to query the
+/// database.
 pub mod statements;
 mod traits;
 mod types;
@@ -25,13 +27,11 @@ pub use helpers::*;
 pub use traits::*;
 pub use types::*;
 
+pub use surrealdb::opt::RecordId;
 pub use surrealdb::sql::json;
 pub use surrealdb::sql::Value;
 
-pub use surrealdb::opt::RecordId;
-pub mod utils {
-    pub use super::helpers::*;
-}
+/// Exports everything from the prelude
 pub mod prelude {
     use super::errors::*;
     use super::function::*;
