17486f9ba854abd12657ac9bcc0d26574812b1cb
oyelowo <oyelowooyedayo@gmail.com>
2023-04-22 08:26:09 +0300

    Support and add rust doc for array prepend

--- rust/surrealdb-query-builder/src/functions/array.rs
+++ rust/surrealdb-query-builder/src/functions/array.rs
@@ -813,6 +813,58 @@ macro_rules! array_pop {
 }
 pub use array_pop as pop;
 
+/// The array::prepend function prepends a value to the end of an array.
+///
+/// array::prepend(array, value) -> array
+/// The following example shows this function, and its output, when used in a select statement:
+///
+/// SELECT * FROM array::prepend([1,2,3,4], 5);
+/// [5,1,2,3,4]
+pub fn prepend_fn(arr: impl Into<ArrayLike>, value: impl Into<Valuex>) -> Function {
+    let arr: ArrayLike = arr.into();
+    let value: Valuex = value.into();
+
+    Function {
+        query_string: format!("array::prepend({}, {})", arr.build(), value.build()),
+        bindings: arr
+            .get_bindings()
+            .into_iter()
+            .chain(value.get_bindings())
+            .collect(),
+    }
+}
+
+/// The array::prepend function prepends a value to the end of an array.
+/// array::prepend(array, value) -> array
+/// The following example shows this function, and its output, when used in a select statement:
+/// SELECT * FROM array::prepend([1,2,3,4], 5);
+/// [5,1,2,3,4]
+/// j
+/// # Arguments
+/// * `arr` -  An array, `Field` or `Param`
+/// * `value` -  A value, `Field` or `Param`
+///
+/// # Examples
+/// ```rust
+/// # use surrealdb_query_builder as  surrealdb_orm;
+/// use surrealdb_orm::{*, functions::array};
+/// array::prepend!(vec![1, 2, 3], 4);
+/// array::prepend!(&[1, 2, 3], 4);
+/// # let own_goals_field = Field::new("own_goals_field");
+/// # let goals_param = Param::new("goals_param");
+/// array::prepend!(own_goals_field, 4);
+/// array::prepend!(goals_param, 4);
+/// // It is also aliased as array_prepend;
+/// array_prepend!(vec![1, 2, 3], 4);
+/// ```
+#[macro_export]
+macro_rules! array_prepend {
+    ( $arr:expr, $value:expr ) => {
+        $crate::functions::array::prepend_fn($arr, $value)
+    };
+}
+pub use array_prepend as prepend;
+
 /// The ordering of the array.
 pub enum Ordering {
     /// Sort the array in ascending order.
