f2bd10a9f0765fe4fdf02c0edd8ee4d4fd846cdf
oyelowo <oyelowooyedayo@gmail.com>
2023-04-29 11:05:12 +0300

    Test conditional updates

--- rust/surrealdb-orm-tests/tests/query_update.rs
+++ rust/surrealdb-orm-tests/tests/query_update.rs
@@ -24,6 +24,30 @@ use surrealdb_orm::{
 // UPDATE person:tobie SET name = 'Tobie', company = 'SurrealDB', skills = ['Rust', 'Go', 'JavaScript'];
 //
 
+fn create_test_alien(age: u8, name: String) -> Alien {
+    let point = point! {
+        x: 40.02f64,
+        y: 116.34,
+    };
+    let territory = line_string![(x: 40.02, y: 116.34), (x: 40.02, y: 116.35), (x: 40.03, y: 116.35), (x: 40.03, y: 116.34), (x: 40.02, y: 116.34)];
+    let polygon = polygon![(x: 40.02, y: 116.34), (x: 40.02, y: 116.35), (x: 40.03, y: 116.35), (x: 40.03, y: 116.34), (x: 40.02, y: 116.34)];
+    Alien {
+        id: None,
+        name,
+        age,
+        created: Utc::now(),
+        line_polygon: territory.into(),
+        life_expectancy: Duration::from_secs(100),
+        territory_area: polygon.into(),
+        home: point.into(),
+        tags: vec!["tag1".into(), "tag2".into()],
+        ally: LinkSelf::null(),
+        weapon: LinkOne::null(),
+        space_ships: LinkMany::null(),
+        planets_to_visit: Relate::null(),
+    }
+}
+
 // test Increment update and decrement update
 #[tokio::test]
 async fn test_increment_and_decrement_update() -> SurrealdbOrmResult<()> {
@@ -82,33 +106,173 @@ async fn test_increment_and_decrement_update() -> SurrealdbOrmResult<()> {
     Ok(())
 }
 
+// // conditionlly link any of the weapons
+// if i % 2 == 0 {
+//     unsaved_alien.weapon = LinkOne::from(weapon1);
+// } else if i % 3 == 0 {
+//     unsaved_alien.weapon = LinkOne::from(weapon2);
+// } else {
+//     unsaved_alien.weapon = LinkOne::from(weapon3);
+// }
+#[tokio::test]
+async fn test_increment_and_decrement_update_conditionally() -> SurrealdbOrmResult<()> {
+    let db = Surreal::new::<Mem>(()).await.unwrap();
+    db.use_ns("test").use_db("test").await.unwrap();
+
+    let weapon1 = Weapon {
+        name: "Laser".to_string(),
+        created: Utc::now(),
+        strength: 5,
+        ..Default::default()
+    };
+    let weapon2 = Weapon {
+        name: "Weapon2".to_string(),
+        created: Utc::now(),
+        strength: 20,
+        ..Default::default()
+    };
+    let weapon3 = Weapon {
+        name: "Weapon3".to_string(),
+        created: Utc::now(),
+        strength: 42,
+        ..Default::default()
+    };
+    let weapons = insert(vec![weapon1, weapon2, weapon3])
+        .return_many(db.clone())
+        .await?;
+
+    // generate test aliens
+    let generated_aliens = (0..20)
+        .map(|i| {
+            let mut unsaved_alien = create_test_alien(i, format!("Oyelowo{}", i));
+            // Set ally for some
+            if i % 2 == 0 {
+                unsaved_alien.ally = LinkSelf::from(unsaved_alien.clone());
+            }
+            // 0 (fulfills the condition) => weapon1 which has a strength of 5.
+            // 1
+            // 2
+            // 3 (fulfills the condition) => weapon1
+            // 4
+            // 5
+            // 6 (fulfills the condition) => weapon1
+            // 7
+            // 8
+            // 9 (fulfills the condition) => weapon1
+            // 10
+            // 11
+            // 12 (fulfills the condition) => weapon1
+            // 13
+            // 14
+            // 15 (fulfills the condition) => weapon1
+            // 16
+            // 17
+            // 18 (fulfills the condition) => weapon1
+            // 19
+            // 20
+            unsaved_alien.weapon = LinkOne::from(weapons[i as usize % 3].clone());
+            unsaved_alien
+        })
+        .collect::<Vec<_>>();
+
+    let created_aliens = insert(generated_aliens)
+        .load_link_ones()?
+        .return_many(db.clone())
+        .await?;
+    assert_eq!(created_aliens.len(), 20);
+    assert_eq!(created_aliens.last().unwrap().age, 19);
+    assert_eq!(created_aliens[0].weapon.value().unwrap().strength, 5);
+    assert_eq!(created_aliens[1].weapon.value().unwrap().strength, 20);
+
+    let alien_schema::Alien {
+        ref age,
+        ref name,
+        ref tags,
+        ..
+    } = Alien::schema();
+    let alien = Alien::schema();
+
+    // Select all aliens with weapon strength 5
+    let select_weak_aliens = || async {
+        let weak_aliens: Vec<Alien> = select(All)
+            .from(Alien::table_name())
+            .where_(cond(alien.weapon(E).strength.equal(5)).and(age.greater_than(3)))
+            .return_many(db.clone())
+            .await
+            .unwrap();
+        weak_aliens
+    };
+    // None should be rook here
+    assert!(select_weak_aliens()
+        .await
+        .iter()
+        .all(|alien| alien.name != "Rook"));
+    // assert that none have tag street yet
+    assert!(select_weak_aliens()
+        .await
+        .iter()
+        .all(|alien| !alien.tags.contains(&"street".to_string())));
+    // assert numbers of tags
+    assert!(select_weak_aliens()
+        .await
+        .iter()
+        .all(|alien| alien.tags.len() == 2));
+
+    let weak_aliens = update::<Alien>(Alien::table_name())
+        .set(updater(name).equal("Rook"))
+        .set(updater(tags).append("street"))
+        .where_(cond(alien.weapon(E).strength.equal(5)).and(age.greater_than(3)))
+        .return_many(db.clone())
+        .await?;
+
+    // Based on the modulo above in the linking, we should have 7 weapons with strength 5.
+    // Out of the 7, only 5 have age greater than 3.
+    assert_eq!(weak_aliens.len(), 5);
+    // Assert that they all now have the name rook
+    assert!(weak_aliens.iter().all(|alien| alien.name == "Rook"));
+    // Assert that they all now have the tag street
+    assert!(weak_aliens
+        .iter()
+        .all(|alien| alien.tags.contains(&"street".to_string())));
+
+    // They should now all be rooks
+    assert!(select_weak_aliens()
+        .await
+        .iter()
+        .all(|alien| alien.name == "Rook"));
+    assert!(select_weak_aliens()
+        .await
+        .iter()
+        .all(|alien| alien.tags.len() == 3));
+
+    let weak_aliens = update::<Alien>(Alien::table_name())
+        .set(updater(name).equal("Kiwi"))
+        .set(updater(tags).remove("street"))
+        .where_(cond(alien.weapon(E).strength.equal(5)).and(age.greater_than(3)))
+        .return_many(db.clone())
+        .await?;
+    assert!(weak_aliens
+        .iter()
+        .all(|alien| !alien.tags.contains(&"street".to_string())));
+
+    // They should now all be kiwi
+    assert!(select_weak_aliens()
+        .await
+        .iter()
+        .all(|alien| alien.name == "Kiwi"));
+    assert!(select_weak_aliens()
+        .await
+        .iter()
+        .all(|alien| alien.tags.len() == 2));
+    Ok(())
+}
+
 #[tokio::test]
 async fn test_add_and_remove_to_array() -> SurrealdbOrmResult<()> {
     let db = Surreal::new::<Mem>(()).await.unwrap();
     db.use_ns("test").use_db("test").await.unwrap();
 
-    let point = point! {
-        x: 40.02f64,
-        y: 116.34,
-    };
-    let territory = line_string![(x: 40.02, y: 116.34), (x: 40.02, y: 116.35), (x: 40.03, y: 116.35), (x: 40.03, y: 116.34), (x: 40.02, y: 116.34)];
-    let polygon = polygon![(x: 40.02, y: 116.34), (x: 40.02, y: 116.35), (x: 40.03, y: 116.35), (x: 40.03, y: 116.34), (x: 40.02, y: 116.34)];
-    let unsaved_alien = Alien {
-        id: None,
-        name: "Oyelowo".to_string(),
-        age: 20,
-        created: Utc::now(),
-        line_polygon: territory.into(),
-        life_expectancy: Duration::from_secs(100),
-        territory_area: polygon.into(),
-        home: point.into(),
-        tags: vec!["tag1".into(), "tag2".into()],
-        ally: LinkSelf::null(),
-        weapon: LinkOne::null(),
-        space_ships: LinkMany::null(),
-        planets_to_visit: Relate::null(),
-    };
-
+    let unsaved_alien = create_test_alien(20, "Oyelowo".into());
     let created_alien = create(unsaved_alien).return_one(db.clone()).await.unwrap();
     assert_eq!(created_alien.as_ref().unwrap().name, "Oyelowo");
     assert_eq!(
@@ -194,6 +358,7 @@ async fn test_add_and_remove_to_array() -> SurrealdbOrmResult<()> {
     // Try removing
     let updated = update::<Alien>(alien_id)
         .set(updater(tags).remove("tag1"))
+        // removes one of calis. There should be 2 before this
         .set(updater(spaceShips).remove(SpaceShip::create_id("cali")))
         .set(updater(spaceShips).remove(SpaceShip::create_id("nonexistent")))
         .return_one(db.clone())
