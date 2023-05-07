c21be375464c51f3bdff9082e87f71eab6e63e11
oyelowo <oyelowooyedayo@gmail.com>
2023-05-07 15:52:12 +0300

    Cleanup setter test

--- rust/surrealdb-orm-tests/tests/setter.rs
+++ rust/surrealdb-orm-tests/tests/setter.rs
@@ -1,18 +1,17 @@
 use surrealdb_models::{organization_schema, Organization};
-use surrealdb_orm::{index, Buildable, SetterAssignable, SurrealdbNode, E};
+use surrealdb_orm::{index, Buildable, SetterAssignable, SurrealdbNode, ToRaw, E};
 
 #[test]
 fn test_setter() {
     let organization_schema::Organization { name, time, .. } = Organization::schema();
-    // let xx = time.equal(Time::default());
     let org = Organization::schema();
 
     org.age.equal(34);
     let org = org.time(index(0)).connected.equal(chrono::Utc::now());
-    assert_eq!(org.build(), "time[0].connected = '2021-08-22T15:00:00Z'");
-    // time.connected = "255"
-    // let xx = org.time(E).connected.equal(chrono::Utc::now());
-    // let xx = org.name.equal("".to_string());
-    // let xx = org.age.equal(3999943);
-    // let xx = org.age.equal(3999943.34);
+
+    assert_eq!(
+        org.fine_tune_params(),
+        "time[$_param_00000001].connected = $_param_00000002"
+    );
+    assert!(org.to_raw().build().len() > 60,);
 }
