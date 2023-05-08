a5ec2b2796ab5578c62f0932bb631189bb3ebc71
oyelowo <oyelowooyedayo@gmail.com>
2023-05-08 16:09:48 +0300

    Add more tests for param traversal and cleanup

--- rust/surrealdb-orm-tests/tests/param_with_path.rs
+++ rust/surrealdb-orm-tests/tests/param_with_path.rs
@@ -3,12 +3,24 @@ use surrealdb_orm::{index, this, Buildable, ToRaw, E};
 
 #[test]
 fn test_param_with_path() {
-    // let param_with_path = this()
-    //     .with_path(E,
-    //     .bestFriend()
-    //     .bestFriend()
-    //     .course())
-    //     .title;
+    let param_with_path = this()
+        .with_path::<Student>(index(2))
+        .bestFriend()
+        .bestFriend()
+        .course()
+        .title;
+    assert_eq!(
+        param_with_path.fine_tune_params(),
+        "$this[$_param_00000001].bestFriend.bestFriend.course.title"
+    );
+    assert_eq!(
+        param_with_path.to_raw().build(),
+        "$this[2].bestFriend.bestFriend.course.title"
+    );
+}
+
+#[test]
+fn test_param_with_path_no_clause() {
     let param_with_path = this()
         .with_path::<Student>(index(2))
         .bestFriend()
@@ -28,10 +40,23 @@ fn test_param_with_path() {
 #[test]
 fn test_param_with_path_simple() {
     let param_with_path = this().with_path::<Student>(index(2)).firstName;
-    dbg!(&param_with_path);
     assert_eq!(
         param_with_path.fine_tune_params(),
         "$this[$_param_00000001].firstName"
     );
     assert_eq!(param_with_path.to_raw().build(), "$this[2].firstName");
+}
+
+#[test]
+fn test_param_simple_clause() {
+    let param_with_path = this().with_path::<Student>(E).lastName;
+    assert_eq!(param_with_path.fine_tune_params(), "$this.lastName");
+    assert_eq!(param_with_path.to_raw().build(), "$this.lastName");
+}
+
+#[test]
+fn basic() {
+    let param_with_path = this();
+    assert_eq!(param_with_path.fine_tune_params(), "$this");
+    assert_eq!(param_with_path.to_raw().build(), "$this");
 }
