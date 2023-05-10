dc6233a682a84e8a7ca70a9b5f9b517423af4b76
oyelowo <oyelowooyedayo@gmail.com>
2023-05-10 22:46:45 +0300

    Use new subquery in expression

--- rust/surrealdb-query-builder/src/types/expression.rs
+++ rust/surrealdb-query-builder/src/types/expression.rs
@@ -5,17 +5,47 @@
  * Licensed under the MIT license
  */
 
-use crate::{statements::SelectStatement, Buildable, Parametric, Valuex};
+use serde::{de::DeserializeOwned, Serialize};
+
+use crate::{
+    statements::{
+        CreateStatement, DeleteStatement, IfElseStatement, InsertStatement, RelateStatement,
+        SelectStatement, Subquery, UpdateStatement,
+    },
+    Buildable, Erroneous, ErrorList, Parametric, SurrealdbEdge, SurrealdbNode, Valuex,
+};
 
 /// An expression is a value or statement that can be used within another query.
 #[derive(Clone, Debug)]
-pub struct Expression(Valuex);
+pub enum Expression {
+    Value(Valuex),
+    Subquery(Subquery),
+}
 
-impl std::ops::Deref for Expression {
-    type Target = Valuex;
+impl Buildable for Expression {
+    fn build(&self) -> String {
+        match self {
+            Expression::Value(value) => value.build(),
+            Expression::Subquery(subquery) => subquery.build(),
+        }
+    }
+}
 
-    fn deref(&self) -> &Self::Target {
-        &self.0
+impl Parametric for Expression {
+    fn get_bindings(&self) -> Vec<crate::traits::Binding> {
+        match self {
+            Expression::Value(value) => value.get_bindings(),
+            Expression::Subquery(subquery) => subquery.get_bindings(),
+        }
+    }
+}
+
+impl Erroneous for Expression {
+    fn get_errors(&self) -> ErrorList {
+        match self {
+            Expression::Value(value) => vec![],
+            Expression::Subquery(subquery) => subquery.get_errors(),
+        }
     }
 }
 
@@ -24,17 +54,64 @@ where
     T: Into<Valuex>,
 {
     fn from(value: T) -> Self {
-        Expression(value.into())
+        Expression::Value(value.into())
     }
 }
 
 impl From<SelectStatement> for Expression {
     fn from(select_statement: SelectStatement) -> Self {
-        Expression(Valuex {
-            // statement already bound
-            string: format!("( {} )", select_statement.build().trim_end_matches(";")),
-            bindings: select_statement.get_bindings(),
-        })
+        Self::Subquery(select_statement.into())
+    }
+}
+
+impl<T> From<CreateStatement<T>> for Expression
+where
+    T: SurrealdbNode + Serialize + DeserializeOwned,
+{
+    fn from(create_statement: CreateStatement<T>) -> Self {
+        Self::Subquery(create_statement.into())
+    }
+}
+
+impl<T> From<UpdateStatement<T>> for Expression
+where
+    T: SurrealdbNode + Serialize + DeserializeOwned,
+{
+    fn from(update_statement: UpdateStatement<T>) -> Self {
+        Self::Subquery(update_statement.into())
+    }
+}
+
+impl<T> From<DeleteStatement<T>> for Expression
+where
+    T: SurrealdbNode + Serialize + DeserializeOwned,
+{
+    fn from(delete_statement: DeleteStatement<T>) -> Self {
+        Self::Subquery(delete_statement.into())
+    }
+}
+
+impl<T> From<InsertStatement<T>> for Expression
+where
+    T: SurrealdbNode + Serialize + DeserializeOwned,
+{
+    fn from(insert_statement: InsertStatement<T>) -> Self {
+        Self::Subquery(insert_statement.into())
+    }
+}
+
+impl<T> From<RelateStatement<T>> for Expression
+where
+    T: SurrealdbEdge + Serialize + DeserializeOwned,
+{
+    fn from(relate_statement: RelateStatement<T>) -> Self {
+        Self::Subquery(relate_statement.into())
+    }
+}
+
+impl From<IfElseStatement> for Expression {
+    fn from(ifelse_statement: IfElseStatement) -> Self {
+        Self::Subquery(ifelse_statement.into())
     }
 }
 
@@ -49,8 +126,8 @@ mod tests {
         let users = Table::new("users");
         let select_statement = select(All).from(users);
         let expression = Expression::from(select_statement);
-        assert_eq!(expression.fine_tune_params(), "( SELECT * FROM users )");
-        assert_eq!(expression.to_raw().build(), "( SELECT * FROM users )");
+        assert_eq!(expression.fine_tune_params(), "$_param_00000001");
+        assert_eq!(expression.to_raw().build(), "(SELECT * FROM users)");
     }
 
     #[test]
