36c89d80297633f75799b3a125db2768436c9b15
oyelowo <oyelowooyedayo@gmail.com>
2023-05-10 06:53:22 +0300

    Test transaction cancellation

--- rust/surrealdb-orm-tests/tests/query_transaction.rs
+++ rust/surrealdb-orm-tests/tests/query_transaction.rs
@@ -11,7 +11,47 @@ use surrealdb_orm::{
 // UPDATE account:two SET balance -= 300.00;
 // test Increment update and decrement update
 #[tokio::test]
-async fn test_increment_and_decrement_update() -> SurrealdbOrmResult<()> {
+async fn test_transaction_commit_increment_and_decrement_update() -> SurrealdbOrmResult<()> {
+    let db = Surreal::new::<Mem>(()).await.unwrap();
+    db.use_ns("test").use_db("test").await.unwrap();
+
+    let ref id1 = Account::create_id("one".into());
+    let ref id2 = Account::create_id("two".into());
+    let amount_to_transfer = 300.00;
+
+    let acc = Account::schema();
+
+    begin_transaction()
+        .query(create(Account {
+            id: id1.clone(),
+            balance: 135_605.16,
+        }))
+        .query(create(Account {
+            id: id2.clone(),
+            balance: 91_031.31,
+        }))
+        .query(update::<Account>(id1).set(acc.balance.increment_by(amount_to_transfer)))
+        .query(update::<Account>(id2).set(acc.balance.decrement_by(amount_to_transfer)))
+        .commit_transaction()
+        .run(db.clone())
+        .await?;
+
+    let accounts = select(All)
+        .from(id1..=id2)
+        .return_many::<Account>(db.clone())
+        .await?;
+
+    assert_eq!(accounts.len(), 2);
+    assert_eq!(accounts[0].balance, 135_905.16);
+    assert_eq!(accounts[1].balance, 90_731.31);
+    assert_eq!(accounts[0].id.to_string(), "account:one");
+    assert_eq!(accounts[1].id.to_string(), "account:two");
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_transaction_cancellation_increment_and_decrement_update() -> SurrealdbOrmResult<()> {
     let db = Surreal::new::<Mem>(()).await.unwrap();
     db.use_ns("test").use_db("test").await.unwrap();
 
@@ -31,7 +71,7 @@ async fn test_increment_and_decrement_update() -> SurrealdbOrmResult<()> {
         }))
         .query(update::<Account>(id1).set(acc.balance.increment_by(300.00)))
         .query(update::<Account>(id2).set(acc.balance.decrement_by(300.00)))
-        .commit_transaction()
+        .cancel_transaction()
         .run(db.clone())
         .await?;
 
@@ -40,11 +80,6 @@ async fn test_increment_and_decrement_update() -> SurrealdbOrmResult<()> {
         .return_many::<Account>(db.clone())
         .await?;
 
-    assert_eq!(accounts.len(), 2);
-    assert_eq!(accounts[0].balance, 135_905.16);
-    assert_eq!(accounts[1].balance, 90_731.31);
-    assert_eq!(accounts[0].id.to_string(), "account:one");
-    assert_eq!(accounts[1].id.to_string(), "account:two");
-
+    assert_eq!(accounts.len(), 0);
     Ok(())
 }
