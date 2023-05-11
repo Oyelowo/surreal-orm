8de19d0554f2c8a856d864dc8897c1eaf555822e
oyelowo <oyelowooyedayo@gmail.com>
2023-05-11 14:05:55 +0300

    Implement More types into setter

--- rust/surrealdb-query-builder/src/traits/setter.rs
+++ rust/surrealdb-query-builder/src/traits/setter.rs
@@ -20,73 +20,6 @@ impl std::fmt::Display for Setter {
     }
 }
 
-// struct SetArg {
-//     string: String,
-//     bindings: BindingsList,
-//     errors: ErrorList,
-// }
-//
-// impl Buildable for SetArg {
-//     fn build(&self) -> String {
-//         self.string.to_string()
-//     }
-// }
-//
-// impl Parametric for SetArg {
-//     fn get_bindings(&self) -> BindingsList {
-//         self.bindings.to_vec()
-//     }
-// }
-//
-// impl Erroneous for SetArg {
-//     fn get_errors(&self) -> ErrorList {
-//         self.errors.to_vec()
-//     }
-// }
-//
-// impl<T: Serialize> From<T> for SetArg {
-//     fn from(value: T) -> Self {
-//         let sql_value = sql::json(&serde_json::to_string(&value).unwrap()).unwrap();
-//         let binding = Binding::new(sql_value);
-//
-//         Self {
-//             string: binding.get_param_dollarised(),
-//             bindings: vec![binding],
-//             errors: vec![],
-//         }
-//     }
-// }
-//
-// impl From<Field> for SetArg {
-//     fn from(value: Field) -> Self {
-//         Self {
-//             string: value.build(),
-//             bindings: value.get_bindings(),
-//             errors: value.get_errors(),
-//         }
-//     }
-// }
-//
-// impl From<Param> for SetArg {
-//     fn from(value: Param) -> Self {
-//         Self {
-//             string: value.build(),
-//             bindings: value.get_bindings(),
-//             errors: value.get_errors(),
-//         }
-//     }
-// }
-//
-// impl From<LetStatement> for SetArg {
-//     fn from(value: LetStatement) -> Self {
-//         Self {
-//             string: value.get_param().build(),
-//             bindings: value.get_bindings(),
-//             errors: value.get_errors(),
-//         }
-//     }
-// }
-
 impl From<Setter> for Vec<Setter> {
     fn from(value: Setter) -> Self {
         vec![value]
@@ -135,6 +68,8 @@ impl Erroneous for Vec<Setter> {
     }
 }
 
+/// A helper struct for generating SQL update statements.
+#[allow(missing_docs)]
 pub enum SetterArg<T>
 where
     T: Serialize,
@@ -146,16 +81,9 @@ where
     LetStatement(LetStatement),
 }
 
-// impl<T: Serialize> From<T> for SetterArg<T> {
-//     fn from(value: T) -> Self {
-//         // Self::Value(value)
-//         todo!()
-//     }
-// }
 impl<T: Serialize, V: Into<T> + Serialize> From<V> for SetterArg<T> {
     fn from(value: V) -> Self {
-        // Self::Value(value)
-        todo!()
+        Self::Value(value.into())
     }
 }
 
@@ -175,42 +103,47 @@ where
 {
     /// Assigns the given value to the field.
     fn equal_to(&self, value: impl Into<SetterArg<T>>) -> Setter {
-        let operator = sql::Operator::Equal;
-        let field = self.deref();
-        // let set_arg: SetArg = value.into().into();
-        let set_arg: SetterArg<T> = value.into().into();
-
-        let (build, bindings, errors) = match set_arg {
-            SetterArg::Value(value) => {
-                let sql_value = sql::json(&serde_json::to_string(&value).unwrap()).unwrap();
-                let binding = Binding::new(sql_value);
-                (binding.get_param_dollarised(), vec![binding], vec![])
-            }
-            SetterArg::Field(field) => (field.build(), field.get_bindings(), field.get_errors()),
-            SetterArg::Subquery(subquery) => (
-                subquery.build(),
-                subquery.get_bindings(),
-                subquery.get_errors(),
-            ),
-            SetterArg::Param(param) => (param.build(), param.get_bindings(), param.get_errors()),
-            SetterArg::LetStatement(let_statement) => (
-                let_statement.get_param().build(),
-                let_statement.get_bindings(),
-                let_statement.get_errors(),
-            ),
-        };
-
-        let column_updater_string = format!("{field} {operator} {}", build);
-        Setter {
-            query_string: column_updater_string,
-            bindings,
-            errors,
-        }
+        get_setter(value, self.deref(), sql::Operator::Equal)
     }
 
     /// Derefs to field type.
     fn to_field(&self) -> Field {
         self.deref().clone()
+    }
+}
+
+fn get_setter<T: Serialize>(
+    value: impl Into<SetterArg<T>>,
+    field: &Field,
+    operator: sql::Operator,
+) -> Setter {
+    let set_arg: SetterArg<T> = value.into();
+
+    let (build, bindings, errors) = match set_arg {
+        SetterArg::Value(value) => {
+            let sql_value = sql::json(&serde_json::to_string(&value).unwrap()).unwrap();
+            let binding = Binding::new(sql_value);
+            (binding.get_param_dollarised(), vec![binding], vec![])
+        }
+        SetterArg::Field(field) => (field.build(), field.get_bindings(), field.get_errors()),
+        SetterArg::Subquery(subquery) => (
+            subquery.build(),
+            subquery.get_bindings(),
+            subquery.get_errors(),
+        ),
+        SetterArg::Param(param) => (param.build(), param.get_bindings(), param.get_errors()),
+        SetterArg::LetStatement(let_statement) => (
+            let_statement.get_param().build(),
+            let_statement.get_bindings(),
+            let_statement.get_errors(),
+        ),
+    };
+
+    let column_updater_string = format!("{field} {operator} {}", build);
+    Setter {
+        query_string: column_updater_string,
+        bindings,
+        errors,
     }
 }
 
@@ -221,31 +154,13 @@ where
     Self: std::ops::Deref<Target = Field>,
 {
     /// Increments the value of the field by the given value.
-    fn increment_by(&self, value: impl Into<T>) -> Setter {
-        let operator = sql::Operator::Inc;
-        let field = self.deref();
-        let set_arg: SetArg = value.into().into();
-
-        let column_updater_string = format!("{field} {operator} {}", set_arg.build());
-        Setter {
-            query_string: column_updater_string,
-            bindings: set_arg.get_bindings(),
-            errors: set_arg.get_errors(),
-        }
+    fn increment_by(&self, value: impl Into<SetterArg<T>>) -> Setter {
+        get_setter(value, self.deref(), sql::Operator::Inc)
     }
 
     /// Decrements the value of the field by the given value.
-    fn decrement_by(&self, value: impl Into<T>) -> Setter {
-        let operator = sql::Operator::Dec;
-        let field = self.deref();
-        let set_arg: SetArg = value.into().into();
-
-        let column_updater_string = format!("{field} {operator} {}", set_arg.build());
-        Setter {
-            query_string: column_updater_string,
-            bindings: set_arg.get_bindings(),
-            errors: set_arg.get_errors(),
-        }
+    fn decrement_by(&self, value: impl Into<SetterArg<T>>) -> Setter {
+        get_setter(value, self.deref(), sql::Operator::Dec)
     }
 
     /// Derefs to field type.
@@ -260,31 +175,13 @@ where
     Self: std::ops::Deref<Target = Field>,
 {
     /// Appends the given value to the array.
-    fn append(&self, value: impl Into<T>) -> Setter {
-        let operator = sql::Operator::Inc;
-        let field = self.deref();
-        let set_arg: SetArg = value.into().into();
-
-        let column_updater_string = format!("{field} {operator} {}", set_arg.build());
-        Setter {
-            query_string: column_updater_string,
-            bindings: set_arg.get_bindings(),
-            errors: set_arg.get_errors(),
-        }
+    fn append(&self, value: impl Into<SetterArg<T>>) -> Setter {
+        get_setter(value, self.deref(), sql::Operator::Inc)
     }
 
     /// Removes the given value from the array.
-    fn remove(&self, value: impl Into<T>) -> Setter {
-        let operator = sql::Operator::Dec;
-        let field = self.deref();
-        let set_arg: SetArg = value.into().into();
-
-        let column_updater_string = format!("{field} {operator} {}", set_arg.build());
-        Setter {
-            query_string: column_updater_string,
-            bindings: set_arg.get_bindings(),
-            errors: set_arg.get_errors(),
-        }
+    fn remove(&self, value: impl Into<SetterArg<T>>) -> Setter {
+        get_setter(value, self.deref(), sql::Operator::Dec)
     }
 
     /// Derefs to field type.
