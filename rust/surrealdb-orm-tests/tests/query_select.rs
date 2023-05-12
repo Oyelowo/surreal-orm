bdbd353a39d400423ea56892895fe2e694e0e693
oyelowo <oyelowooyedayo@gmail.com>
2023-05-12 23:50:26 +0300

    Add test for subquery

---
+++ rust/surrealdb-orm-tests/tests/query_select.rs
@@ -0,0 +1,65 @@
+use chrono::Utc;
+use surrealdb::{engine::local::Mem, Surreal};
+use surrealdb_models::{spaceship_schema, weapon_schema, SpaceShip, Weapon};
+use surrealdb_orm::{
+    arr,
+    statements::{insert, order, select, select_value},
+    Aliasable, All, Buildable, Operatable, ReturnableSelect, Runnable, SchemaGetter,
+    SurrealdbModel, SurrealdbOrmResult, ToRaw,
+};
+
+#[tokio::test]
+async fn test_subquery_in_select_statement() -> SurrealdbOrmResult<()> {
+    let db = Surreal::new::<Mem>(()).await.unwrap();
+    db.use_ns("test").use_db("test").await.unwrap();
+
+    let generated_spaceships = (1..=10)
+        .map(|i| Weapon {
+            name: format!("Weapon {}", i),
+            strength: i * 10,
+            created: Utc::now(),
+            ..Default::default()
+        })
+        .collect::<Vec<_>>();
+    insert(generated_spaceships.clone()).run(db.clone()).await?;
+
+    let ref weapon = Weapon::table_name();
+    let weapon_schema::Weapon { ref strength, .. } = Weapon::schema();
+
+    let statement = select(All)
+        .from(weapon)
+        .where_(
+            strength.inside(
+                select_value(strength)
+                    .from(weapon)
+                    .order_by(order(strength).asc())
+                    .limit(6),
+            ),
+        )
+        .order_by(order(strength).desc())
+        .start(2)
+        .limit(10);
+
+    assert_eq!(
+        statement.fine_tune_params(),
+        "SELECT * FROM weapon WHERE strength INSIDE $_param_00000001 \
+            ORDER BY strength  DESC LIMIT $_param_00000002 START AT $_param_00000003;"
+    );
+    assert_eq!(
+        statement.to_raw().build(),
+        "SELECT * FROM weapon WHERE strength INSIDE \
+            (SELECT VALUE strength FROM weapon ORDER BY strength LIMIT 6) \
+            ORDER BY strength  DESC LIMIT 10 START AT 2;"
+    );
+    let statement = statement.return_many::<SpaceShip>(db.clone()).await?;
+
+    dbg!(&statement[0]);
+    assert_eq!(&statement[0].name, "Weapon 4");
+    assert_eq!(&statement[1].name, "Weapon 3");
+    assert_eq!(&statement[2].name, "Weapon 2");
+    assert_eq!(&statement[3].name, "Weapon 1");
+
+    assert_eq!(statement.len(), 4);
+    assert!(statement[0].id.to_string().starts_with("weapon:"));
+    Ok(())
+}
