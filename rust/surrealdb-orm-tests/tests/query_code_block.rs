5265b1c990e18e4c9196f55f87457319135f8892
oyelowo <oyelowooyedayo@gmail.com>
2023-05-14 22:52:22 +0300

    Support syntactic sugar code block usign macro

--- rust/surrealdb-orm-tests/tests/query_code_block.rs
+++ rust/surrealdb-orm-tests/tests/query_code_block.rs
@@ -6,6 +6,70 @@ use surrealdb_orm::{
     *,
 };
 
+macro_rules! code_block {
+    ($(let $var:ident = $value:expr;)* return $expr:expr;) => {
+        {
+            $(
+                let $var = let_(stringify!($var)).equal_to($value);
+            )*
+
+            $(
+                chain(&$var).
+            )*
+
+            chain(return_($expr)).as_block()
+        }
+    };
+    // () => {};
+}
+
+#[tokio::test]
+async fn test_code_block_with_sweet_macro_block() -> SurrealdbOrmResult<()> {
+    let db = Surreal::new::<Mem>(()).await.unwrap();
+    db.use_ns("test").use_db("test").await.unwrap();
+
+    let weapon = Weapon::table_name();
+    let weapon_schema::Weapon { name, strength, .. } = Weapon::schema();
+    let weaponstats_schema::WeaponStats {
+        averageStrength, ..
+    } = WeaponStats::schema();
+
+    let generated_weapons = (0..=14)
+        .map(|i| Weapon {
+            name: format!("weapon_{}", i),
+            strength: i,
+            ..Default::default()
+        })
+        .collect::<Vec<_>>();
+
+    insert(generated_weapons).return_many(db.clone()).await?;
+
+    let code_block = code_block! {
+        let strengths = select_value(strength).from(weapon);
+        let total = math::sum!(&strengths);
+        let count = count!(&strengths);
+        return total.divide(count);
+    };
+
+    let created_stats_statement = create::<WeaponStats>(averageStrength.equal_to(code_block));
+
+    insta::assert_display_snapshot!(created_stats_statement.to_raw());
+    insta::assert_display_snapshot!(created_stats_statement.fine_tune_params());
+    assert_eq!(
+        created_stats_statement.to_raw().build(),
+        "CREATE weapon_stats SET averageStrength = {\n\
+                LET $strengths = (SELECT VALUE strength FROM weapon);\n\n\
+                LET $total = math::sum($strengths);\n\n\
+                LET $count = count($strengths);\n\n\
+                RETURN $total / $count;\n\
+                };"
+    );
+
+    let result = created_stats_statement.get_one(db.clone()).await.unwrap();
+    assert_eq!(result.average_strength, 7.5);
+
+    Ok(())
+}
 #[tokio::test]
 async fn test_code_block_with_macro() -> SurrealdbOrmResult<()> {
     let db = Surreal::new::<Mem>(()).await.unwrap();
@@ -28,12 +92,9 @@ async fn test_code_block_with_macro() -> SurrealdbOrmResult<()> {
     insert(generated_weapons).return_many(db.clone()).await?;
 
     let_!(strengths = select_value(strength).from(weapon));
-
     let_!(total = math::sum!(&strengths));
-
     let_!(count = count!(&strengths));
-
-    let returned = return_(bracket(total.get_param().divide(count.get_param())));
+    let returned = return_(bracket(total.divide(&count)));
     let code_block = block(chain(strengths).chain(total).chain(count).chain(returned));
 
     let created_stats_statement = create::<WeaponStats>(averageStrength.equal_to(code_block));
