db20ec344a69d71e315b0e72923945c1928aec42
oyelowo <oyelowooyedayo@gmail.com>
2023-05-17 14:39:28 +0300

    Document define function macro

--- rust/surrealdb-query-builder/src/statements/define_function.rs
+++ rust/surrealdb-query-builder/src/statements/define_function.rs
@@ -93,7 +93,49 @@ impl Buildable for DefineFunctionStatement {
     }
 }
 
-/// Define a function
+/// A macro to generate a function definition statement and the corresponding helper function.
+/// e.g. `define_function!(get_it(first: bool, last: string, birthday: string) { let person = "43"; return person; });`
+/// generates a `get_it_statement` itself and `get_it` helper function created by the macro.
+///
+/// # Arguments
+/// * `function definition` - The function definition
+///
+/// # Example
+/// ```rust
+/// # use surrealdb_query_builder as surreal_orm;
+/// use surreal_orm::{*, statements::define_function};
+///
+/// // The below generates a `get_it_statement` itself and `get_it` helper function created by the macro.
+/// define_function!(get_it(first: bool, last: string, birthday: string) {
+///     let person = "43";
+///     return person;
+/// });
+/// ```
+///
+/// ```rust, ignore   
+/// // The below generates a `get_person_statement` itself and `get_person` helper function created by the macro.
+/// define_function!(get_person(first_arg: string, last_arg: string, birthday_arg: string) {
+///     let person = select(All)
+///         .from(SpaceShip::table_name())
+///         .where_(
+///             cond(SpaceShip::schema().id.equal(&first_arg))
+///                 .and(SpaceShip::schema().name.equal(&last_arg))
+///                 .and(SpaceShip::schema().created.equal(&birthday_arg)),
+///         );
+///
+///     return if_(person.with_path::<SpaceShip>(index(0)).id.is_not(NONE))
+///                 .then(person.with_path::<SpaceShip>(index(0)))
+///             .else_(
+///                 create::<SpaceShip>(
+///                     vec![
+///                         SpaceShip::schema().id.equal_to(&first_arg),
+///                         SpaceShip::schema().name.equal_to(&last_arg),
+///                         SpaceShip::schema().created.equal_to(&birthday_arg),
+///                     ]
+///                 )
+///             ).end();
+/// });
+/// ```
 #[macro_export]
 macro_rules! define_function_ {
     ($function_name:ident ($($param:ident : $type:ident),* ) {$(let $var:ident = $value:expr;)* return $expr:expr;}) => {
@@ -201,9 +243,8 @@ pub use define_function_ as define_function;
 
 #[cfg(test)]
 mod tests {
-    use crate::{statements::select, All, Field, SurrealdbModel, TestUser, ToRaw};
-
     use super::*;
+    use crate::*;
 
     define_function!(get_it(first: bool, last: string, birthday: string) {
         let person = "43";
