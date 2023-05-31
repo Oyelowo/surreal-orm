b23a9f295f169b36bddb34034fe2ff094e058fc7
oyelowo <oyelowooyedayo@gmail.com>
2023-05-30 22:33:28 -0400

    Test delete where helper method

--- rust/surrealdb-orm-tests/tests/crud_helpers.rs
+++ rust/surrealdb-orm-tests/tests/crud_helpers.rs
@@ -147,26 +147,32 @@ async fn test_delete_by_id() -> SurrealdbOrmResult<()> {
     Ok(())
 }
 
-// #[tokio::test]
-// async fn test_delete_where() {
-//     let db = Surreal::new::<Mem>(()).await.unwrap();
-//     db.use_ns("test").use_db("test").await.unwrap();
-//
-//     let spaceship = SpaceShip {
-//         id: SpaceShip::create_id(format!("num-{}", 1)),
-//         name: format!("spaceship-{}", 1),
-//         created: chrono::Utc::now(),
-//     };
-//
-//     spaceship.save().run(db.clone()).await.unwrap();
-//     SpaceShip::delete_where(Condition::Equal("name", spaceship.name.clone()))
-//         .run(db.clone())
-//         .await
-//         .unwrap();
-//
-//     let found_spaceships = SpaceShip::find_where(Condition::Equal("name", spaceship.name.clone()))
-//         .run(db.clone())
-//         .await
-//         .unwrap();
-//     assert_eq!(found_spaceships.len(), 0);
-// }
+#[tokio::test]
+async fn test_delete_where() -> SurrealdbOrmResult<()> {
+    let db = Surreal::new::<Mem>(()).await.unwrap();
+    db.use_ns("test").use_db("test").await.unwrap();
+
+    let spaceship = SpaceShip {
+        id: SpaceShip::create_id(format!("num-{}", 1)),
+        name: format!("spaceship-{}", 1),
+        created: chrono::Utc::now(),
+    };
+
+    spaceship.save().run(db.clone()).await.unwrap();
+    let spaceship_schema::SpaceShip { name, .. } = SpaceShip::schema();
+
+    let found_spaceships = SpaceShip::find_where(name.like("spaceship-1"))
+        .return_many(db.clone())
+        .await?;
+    assert_eq!(found_spaceships.len(), 1);
+
+    SpaceShip::delete_where(name.like("spaceship"))
+        .run(db.clone())
+        .await?;
+
+    let found_spaceships = SpaceShip::find_where(name.like("spaceship-1"))
+        .return_many(db.clone())
+        .await?;
+    assert_eq!(found_spaceships.len(), 0);
+    Ok(())
+}
