02afd0b434ab762e3253aab990cacca6695e19fb
oyelowo <oyelowooyedayo@gmail.com>
2023-05-31 18:28:47 -0400

    Reorg and add more complex test

--- rust/surrealdb-orm-tests/tests/arithmetic_operations.rs
+++ rust/surrealdb-orm-tests/tests/arithmetic_operations.rs
@@ -2,58 +2,22 @@ use surrealdb_models::{rocket, spaceship_schema, weapon_schema, Rocket, SpaceShi
 use surrealdb_orm::*;
 
 #[test]
-fn test_add() {
-    let age = Field::new("age");
-    let name = Field::new("name");
-    let email = Field::new("email");
-    let surname = Field::new("surname");
+fn test_rocket_add_field_to_real_number_complex() {
+    let rocket::Rocket {
+        ref strength,
+        ref bunchOfOtherFields,
+        ..
+    } = Rocket::schema();
+    let operation = ((strength + 5) / (strength + bunchOfOtherFields)) * 32;
 
-    let operation = (age + name) + (email + surname);
-
-    assert_eq!(operation.query_string, "(age + name) + (email + surname)");
-}
-
-#[test]
-fn test_sub() {
-    let age = Field::new("age");
-    let name = Field::new("name");
-
-    let operation = age - name;
-
-    assert_eq!(operation.query_string, "age - name");
-}
-
-#[test]
-fn test_mul() {
-    let age = Field::new("age");
-    let name = Field::new("name");
-
-    let operation = age * name;
-
-    assert_eq!(operation.query_string, "age * name");
-}
-
-#[test]
-fn test_div() {
-    let age = Field::new("age");
-    let name = Field::new("name");
-
-    let operation = age / name;
-
-    assert_eq!(operation.query_string, "age / name");
-}
-
-// Test more complex expressions
-#[test]
-fn test_complex() {
-    let age = Field::new("age");
-    let name = Field::new("name");
-    let email = Field::new("email");
-    let surname = Field::new("surname");
-
-    let operation = (age + surname) / (name + email);
-
-    assert_eq!(operation.query_string, "(age + surname) / (name + email)");
+    assert_eq!(
+        operation.to_raw().build(),
+        "(strength + 5) / (strength + bunchOfOtherFields) * 32"
+    );
+    assert_eq!(
+        operation.fine_tune_params(),
+        "(strength + $_param_00000001) / (strength + bunchOfOtherFields) * $_param_00000002"
+    );
 }
 
 #[test]
@@ -358,4 +322,59 @@ fn test_rocket_div_field_to_field_owned_plus_borrowed() {
         operation.fine_tune_params(),
         "strength / bunchOfOtherFields"
     );
+}
+
+#[test]
+fn test_add() {
+    let age = Field::new("age");
+    let name = Field::new("name");
+    let email = Field::new("email");
+    let surname = Field::new("surname");
+
+    let operation = (age + name) + (email + surname);
+
+    assert_eq!(operation.query_string, "(age + name) + (email + surname)");
+}
+
+#[test]
+fn test_sub() {
+    let age = Field::new("age");
+    let name = Field::new("name");
+
+    let operation = age - name;
+
+    assert_eq!(operation.query_string, "age - name");
+}
+
+#[test]
+fn test_mul() {
+    let age = Field::new("age");
+    let name = Field::new("name");
+
+    let operation = age * name;
+
+    assert_eq!(operation.query_string, "age * name");
+}
+
+#[test]
+fn test_div() {
+    let age = Field::new("age");
+    let name = Field::new("name");
+
+    let operation = age / name;
+
+    assert_eq!(operation.query_string, "age / name");
+}
+
+// Test more complex expressions
+#[test]
+fn test_complex() {
+    let age = Field::new("age");
+    let name = Field::new("name");
+    let email = Field::new("email");
+    let surname = Field::new("surname");
+
+    let operation = (age + surname) / (name + email);
+
+    assert_eq!(operation.query_string, "(age + surname) / (name + email)");
 }
