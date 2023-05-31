d6b20b83ff1626418fff4dc87c80ed3b961447e0
oyelowo <oyelowooyedayo@gmail.com>
2023-05-31 18:17:52 -0400

    Add more tests

--- rust/surrealdb-orm-tests/tests/ops.rs
+++ rust/surrealdb-orm-tests/tests/ops.rs
@@ -1,27 +1,182 @@
-use surrealdb_models::{spaceship_schema, weapon_schema, SpaceShip, Weapon};
-use surrealdb_orm::{Field, SchemaGetter};
+use surrealdb_models::{rocket, spaceship_schema, weapon_schema, Rocket, SpaceShip, Weapon};
+use surrealdb_orm::*;
 
 #[test]
-fn rerer() {
+fn test_add() {
     let age = Field::new("age");
     let name = Field::new("name");
     let email = Field::new("email");
     let surname = Field::new("surname");
-    // let age_name = age + name + email + surname;
-    // let age_name = (age + surname) / 3;
-    // let age_name = (age + surname) / (name + email);
-    // let age_name = (age + surname) * (name + email);
-    // let age_name = (age + surname) + (name + email);
-    // dbg!(age_name);
-    // assert!(false);
-    // let age_name = (age + surname) / name;
-    // let age_name = age + name + email + surname;
-    let weapon_schema::Weapon {
-        id, ref strength, ..
-    } = Weapon::schema();
 
-    // let age_name = id + created + email;
-    let age_name = strength + strength;
-    // let age_name = id + created + email;
-    // let age_name = age + name + email + surname;
+    let operation = (age + name) + (email + surname);
+
+    assert_eq!(operation.query_string, "(age + name) + (email + surname)");
 }
+
+#[test]
+fn test_sub() {
+    let age = Field::new("age");
+    let name = Field::new("name");
+
+    let operation = age - name;
+
+    assert_eq!(operation.query_string, "age - name");
+}
+
+#[test]
+fn test_mul() {
+    let age = Field::new("age");
+    let name = Field::new("name");
+
+    let operation = age * name;
+
+    assert_eq!(operation.query_string, "age * name");
+}
+
+#[test]
+fn test_div() {
+    let age = Field::new("age");
+    let name = Field::new("name");
+
+    let operation = age / name;
+
+    assert_eq!(operation.query_string, "age / name");
+}
+
+// Test more complex expressions
+#[test]
+fn test_complex() {
+    let age = Field::new("age");
+    let name = Field::new("name");
+    let email = Field::new("email");
+    let surname = Field::new("surname");
+
+    let operation = (age + surname) / (name + email);
+
+    assert_eq!(operation.query_string, "(age + surname) / (name + email)");
+}
+
+#[test]
+fn test_rocket_add_field_to_real_number() {
+    let rocket::Rocket {
+        strength,
+        bunchOfOtherFields,
+        ..
+    } = Rocket::schema();
+    let operation = strength + 5;
+
+    assert_eq!(operation.to_raw().build(), "strength + 5");
+    assert_eq!(operation.fine_tune_params(), "strength + $_param_00000001");
+}
+
+#[test]
+fn test_rocket_add_field_to_field_owned() {
+    let rocket::Rocket {
+        strength,
+        bunchOfOtherFields,
+        ..
+    } = Rocket::schema();
+    let operation = strength + bunchOfOtherFields;
+
+    assert_eq!(operation.to_raw().build(), "strength + bunchOfOtherFields");
+    assert_eq!(
+        operation.fine_tune_params(),
+        "strength + bunchOfOtherFields"
+    );
+}
+
+#[test]
+fn test_rocket_add_field_to_field_borrowed_plus_borrowed() {
+    let rocket::Rocket {
+        ref strength,
+        ref bunchOfOtherFields,
+        ..
+    } = Rocket::schema();
+    let operation = strength + bunchOfOtherFields;
+
+    assert_eq!(operation.to_raw().build(), "strength + bunchOfOtherFields");
+    assert_eq!(
+        operation.fine_tune_params(),
+        "strength + bunchOfOtherFields"
+    );
+}
+
+#[test]
+fn test_rocket_add_field_to_field_borrowed_plus_owned() {
+    let rocket::Rocket {
+        ref strength,
+        bunchOfOtherFields,
+        ..
+    } = Rocket::schema();
+    let operation = strength + bunchOfOtherFields;
+
+    assert_eq!(operation.to_raw().build(), "strength + bunchOfOtherFields");
+    assert_eq!(
+        operation.fine_tune_params(),
+        "strength + bunchOfOtherFields"
+    );
+}
+
+#[test]
+fn test_rocket_add_field_to_field_owned_plus_borrowed() {
+    let rocket::Rocket {
+        strength,
+        ref bunchOfOtherFields,
+        ..
+    } = Rocket::schema();
+    let operation = strength + bunchOfOtherFields;
+
+    assert_eq!(operation.to_raw().build(), "strength + bunchOfOtherFields");
+    assert_eq!(
+        operation.fine_tune_params(),
+        "strength + bunchOfOtherFields"
+    );
+}
+
+// #[test]
+// fn test_rocket_sub() {
+//     let rocket = Rocket::schema();
+//     let operation = rocket.strength - 5;
+//
+//     assert_eq!(operation.query_string, "strength - 5");
+//     assert_eq!(
+//         operation.bindings,
+//         vec![
+//             rocket.strength.get_bindings(),
+//             NumberLike::Int(5).get_bindings()
+//         ]
+//         .concat()
+//     );
+// }
+//
+// #[test]
+// fn test_rocket_mul() {
+//     let rocket = Rocket::schema();
+//     let operation = rocket.strength * 5;
+//
+//     assert_eq!(operation.query_string, "strength * 5");
+//     assert_eq!(
+//         operation.bindings,
+//         vec![
+//             rocket.strength.get_bindings(),
+//             NumberLike::Int(5).get_bindings()
+//         ]
+//         .concat()
+//     );
+// }
+//
+// #[test]
+// fn test_rocket_div() {
+//     let rocket = Rocket::schema();
+//     let operation = rocket.strength / 5;
+//
+//     assert_eq!(operation.query_string, "strength / 5");
+//     assert_eq!(
+//         operation.bindings,
+//         vec![
+//             rocket.strength.get_bindings(),
+//             NumberLike::Int(5).get_bindings()
+//         ]
+//         .concat()
+//     );
+// }
