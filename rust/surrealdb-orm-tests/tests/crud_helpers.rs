fcad58fa602594292a47f3f74bb7a7a111157400
oyelowo <oyelowooyedayo@gmail.com>
2023-05-30 22:30:57 -0400

    Test delete_by_id helper method

--- rust/surrealdb-orm-tests/tests/crud_helpers.rs
+++ rust/surrealdb-orm-tests/tests/crud_helpers.rs
@@ -119,30 +119,34 @@ async fn test_delete() -> SurrealdbOrmResult<()> {
     Ok(())
 }
 
-// #[tokio::test]
-// async fn test_delete_by_id() {
-//     let db = Surreal::new::<Mem>(()).await.unwrap();
-//     db.use_ns("test").use_db("test").await.unwrap();
-//
-//     let spaceship = SpaceShip {
-//         id: SpaceShip::create_id(format!("num-{}", 1)),
-//         name: format!("spaceship-{}", 1),
-//         created: chrono::Utc::now(),
-//     };
-//
-//     spaceship.save().run(db.clone()).await.unwrap();
-//     SpaceShip::delete_by_id(spaceship.id.clone())
-//         .run(db.clone())
-//         .await
-//         .unwrap();
-//
-//     let found_spaceship = SpaceShip::find_by_id(spaceship.id.clone())
-//         .run(db.clone())
-//         .await
-//         .unwrap();
-//     assert!(found_spaceship.is_none());
-// }
-//
+#[tokio::test]
+async fn test_delete_by_id() -> SurrealdbOrmResult<()> {
+    let db = Surreal::new::<Mem>(()).await.unwrap();
+    db.use_ns("test").use_db("test").await.unwrap();
+
+    let spaceship = SpaceShip {
+        id: SpaceShip::create_id(format!("num-{}", 1)),
+        name: format!("spaceship-{}", 1),
+        created: chrono::Utc::now(),
+    };
+
+    let spaceship = spaceship.save().get_one(db.clone()).await?;
+    let found_spaceships = SpaceShip::find_by_id(spaceship.id.clone())
+        .return_many(db.clone())
+        .await?;
+    assert_eq!(found_spaceships.len(), 1);
+
+    SpaceShip::delete_by_id(spaceship.id.clone())
+        .run(db.clone())
+        .await?;
+
+    let found_spaceships = SpaceShip::find_by_id(spaceship.id.clone())
+        .return_many(db.clone())
+        .await?;
+    assert_eq!(found_spaceships.len(), 0);
+    Ok(())
+}
+
 // #[tokio::test]
 // async fn test_delete_where() {
 //     let db = Surreal::new::<Mem>(()).await.unwrap();
