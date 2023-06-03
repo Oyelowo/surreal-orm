82c8a6f1aa712559df6cfab4e3b8f2828548870b
oyelowo <oyelowooyedayo@gmail.com>
2023-06-03 10:18:04 -0400

    Disintegrate Create method to separate content from set into different methods for API intuition

--- rust/surrealdb-query-builder/src/statements/create.rs
+++ rust/surrealdb-query-builder/src/statements/create.rs
@@ -91,32 +91,14 @@ pub fn create<T>(content: impl Into<ContentOrSets<T>>) -> CreateStatement<T>
 where
     T: Serialize + DeserializeOwned + SurrealdbNode,
 {
-    let content: ContentOrSets<T> = content.into();
-    let mut errors = vec![];
-    let mut bindings = vec![];
-    let content_or_set;
-
-    match content {
-        ContentOrSets::Content(content) => {
-            let sql_value = sql::json(&serde_json::to_string(&content).unwrap()).unwrap();
-            let binding = Binding::new(sql_value);
-            content_or_set = ContentOrSetString::Content(binding.get_param_dollarised());
-            bindings.push(binding);
-        }
-        ContentOrSets::Sets(sets) => {
-            bindings.extend(sets.get_bindings());
-            errors.extend(sets.get_errors());
-            content_or_set = ContentOrSetString::Sets(sets.build());
-        }
-    };
-
     CreateStatement::<T> {
         target: T::table_name().to_string(),
-        content: content_or_set,
+        content: "".to_string(),
+        set: vec![],
         return_type: None,
         timeout: None,
         parallel: false,
-        bindings,
+        bindings: vec![],
         errors: vec![],
         __model_return_type: PhantomData,
     }
@@ -136,7 +118,8 @@ where
     T: Serialize + DeserializeOwned + SurrealdbNode,
 {
     target: String,
-    content: ContentOrSetString,
+    content: String,
+    set: Vec<String>,
     return_type: Option<ReturnType>,
     timeout: Option<String>,
     parallel: bool,
@@ -151,6 +134,80 @@ impl<T> CreateStatement<T>
 where
     T: Serialize + DeserializeOwned + SurrealdbNode,
 {
+    /// Sets the content of the record to be created.
+    /// When using this, the type can be automatically inferred unlike the `set` method.
+    ///
+    /// # Arguments
+    ///
+    /// * `content` - a serializable surrealdb node model.
+    /// # Examples
+    ///
+    /// ```rust, ignore
+    /// create().content(User{
+    ///         name: "Oylowo".to_string(),
+    ///         age: 192
+    ///     });
+    /// ```
+    pub fn content(mut self, content: T) -> CreateStatement<T> {
+        let sql_value = sql::json(&serde_json::to_string(&content).unwrap()).unwrap();
+        let binding = Binding::new(sql_value);
+        self.content = binding.get_param().to_owned();
+        self.bindings.push(binding);
+        self
+    }
+
+    /// Sets the values of the fields to be updated in the record.
+    ///
+    /// # Arguments
+    ///
+    /// * `settables` - an instance of `Setter` trait. This can be created using a single
+    /// `equal_to` helper method on a field or a list of `equal_to` methods for multiple fields
+    ///
+    /// # Examples
+    ///
+    /// Setting single field
+    /// ```rust, ignore
+    /// assert_eq!(create::<User>().set(name.equal("Oyelowo")).to_raw().build(), "CREATE user SET name='Oyelowo'")
+    /// ```
+    ///
+    /// Setting multiple fields by chaining `set` method
+    /// ```rust, ignore
+    /// assert_eq!(create::<User>()
+    ///             .set(name.equal_to("Oyelowo"))
+    ///             .set(age.equal_to(192))
+    ///         ).to_raw().build(), "Create user SET name='Oyelowo', age=192")
+    /// ```
+    ///
+    /// Setting multiple fields by using a list of updaters in a single `set` method
+    /// ```rust, ignore
+    /// assert_eq!(create::<User>()
+    ///             .set(vec![
+    ///                     name.equal_to("Oyelowo"),
+    ///                     age.equal_to(192)
+    ///                 ],
+    ///         ).to_raw().build(), "CREATE user SET name='Oyelowo', age=192")
+    /// ```
+
+    pub fn set(mut self, settables: impl Into<Vec<Setter>>) -> Self {
+        let settable: Vec<Setter> = settables.into();
+
+        let (settable, bindings, errors) = settable.into_iter().fold(
+            (Vec::new(), Vec::new(), Vec::new()),
+            |(mut settable, mut bindings, mut errors), s| {
+                settable.push(s.build());
+                bindings.extend(s.get_bindings());
+                errors.extend(s.get_errors());
+                (settable, bindings, errors)
+            },
+        );
+
+        self.bindings.extend(bindings);
+        self.errors.extend(errors);
+        self.set.extend(settable);
+
+        self
+    }
+
     /// Sets the return type for the query.
     ///
     /// # Arguments
@@ -233,13 +290,10 @@ where
     fn build(&self) -> String {
         let mut query = format!("CREATE {}", &self.target);
 
-        match &self.content {
-            ContentOrSetString::Content(content) => {
-                query = format!("{query} CONTENT {content}");
-            }
-            ContentOrSetString::Sets(sets) => {
-                query = format!("{query} SET {sets}");
-            }
+        if !self.content.is_empty() {
+            query = format!("{query} CONTENT {content}", content = &self.content);
+        } else if !self.set.is_empty() {
+            query = format!("{query} SET {set}", set = &self.set.join(", "));
         }
 
         if let Some(return_type) = &self.return_type {
