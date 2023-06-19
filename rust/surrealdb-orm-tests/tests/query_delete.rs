e6b5257dc900bd001de36d9aab91bf5031b4b070
oyelowo <oyelowooyedayo@gmail.com>
2023-06-18 22:20:47 -0600

    Add more tests for delete methods helpers

--- rust/surrealdb-orm-tests/tests/query_delete.rs
+++ rust/surrealdb-orm-tests/tests/query_delete.rs
@@ -20,6 +20,32 @@ async fn create_test_data(db: Surreal<Db>) -> Vec<Weapon> {
 }
 
 #[tokio::test]
+async fn test_delete_by_id_helper_function() -> SurrealdbOrmResult<()> {
+    let db = Surreal::new::<Mem>(()).await.unwrap();
+    db.use_ns("test").use_db("test").await.unwrap();
+
+    let weapons = create_test_data(db.clone()).await;
+    let weapon1 = weapons.first().unwrap();
+    let ref weapon1_id = weapon1.id.clone();
+
+    let weapon_schema::Weapon { id, .. } = &Weapon::schema();
+
+    let deleted_weapon_count = || async {
+        Weapon::count_where(id.eq(weapon1_id))
+            .get(db.clone())
+            .await
+            .unwrap()
+    };
+    assert_eq!(deleted_weapon_count().await, 1);
+
+    Weapon::delete_by_id(weapon1_id).run(db.clone()).await?;
+
+    assert_eq!(deleted_weapon_count().await, 0);
+
+    Ok(())
+}
+
+#[tokio::test]
 async fn test_delete_one_by_id() -> SurrealdbOrmResult<()> {
     let db = Surreal::new::<Mem>(()).await.unwrap();
     db.use_ns("test").use_db("test").await.unwrap();
@@ -40,19 +66,85 @@ async fn test_delete_one_by_id() -> SurrealdbOrmResult<()> {
 
     delete::<Weapon>(weapon1_id).run(db.clone()).await?;
 
-    // let found_spaceships = Weapon::find_by_id(weapon1_id);
-    // assert_eq!(found_spaceships.return_many(db.clone()).await?.len(), 0);
     assert_eq!(deleted_weapon_count().await, 0);
 
     Ok(())
 }
 
 #[tokio::test]
-async fn test_delete_one_by_model() -> SurrealdbOrmResult<()> {
+async fn test_delete_one_by_model_instance() -> SurrealdbOrmResult<()> {
+    let db = Surreal::new::<Mem>(()).await.unwrap();
+    db.use_ns("test").use_db("test").await.unwrap();
+
+    let weapons = create_test_data(db.clone()).await;
+    let weapon1 = weapons.first().unwrap();
+    let ref weapon1_id = weapon1.id.clone();
+
+    let weapon_schema::Weapon { id, .. } = &Weapon::schema();
+
+    let deleted_weapon_count = || async {
+        Weapon::count_where(id.eq(weapon1_id))
+            .get(db.clone())
+            .await
+            .unwrap()
+    };
+    let deleted_weapon = || async {
+        Weapon::find_by_id(weapon1_id)
+            .return_one(db.clone())
+            .await
+            .unwrap()
+    };
+
+    assert_eq!(deleted_weapon().await.is_some(), true);
+    assert_eq!(deleted_weapon_count().await, 1);
+
+    weapon1.delete().run(db.clone()).await?;
+
+    assert_eq!(deleted_weapon().await.is_some(), false);
+    assert_eq!(deleted_weapon_count().await, 0);
+
     Ok(())
 }
 
 #[tokio::test]
-async fn test_delete_many_by_condition() -> SurrealdbOrmResult<()> {
+async fn test_delete_where_model_helper_function() -> SurrealdbOrmResult<()> {
+    let db = Surreal::new::<Mem>(()).await.unwrap();
+    db.use_ns("test").use_db("test").await.unwrap();
+
+    create_test_data(db.clone()).await;
+
+    let weapon_schema::Weapon { strength, .. } = &Weapon::schema();
+
+    let weapons_count = || async { Weapon::count_all().get(db.clone()).await.unwrap() };
+    assert_eq!(weapons_count().await, 1000);
+
+    Weapon::delete_where(cond(strength.gte(500)).and(strength.lt(600)))
+        .run(db.clone())
+        .await?;
+
+    assert_eq!(weapons_count().await, 900);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_delete_many_query_by_condition() -> SurrealdbOrmResult<()> {
+    let db = Surreal::new::<Mem>(()).await.unwrap();
+    db.use_ns("test").use_db("test").await.unwrap();
+
+    create_test_data(db.clone()).await;
+
+    let weapon_schema::Weapon { strength, .. } = &Weapon::schema();
+
+    let weapons_count = || async { Weapon::count_all().get(db.clone()).await.unwrap() };
+    assert_eq!(weapons_count().await, 1000);
+
+    delete::<Weapon>(Weapon::table_name())
+        .where_(cond(strength.gte(500)).and(strength.lt(600)))
+        .run(db.clone())
+        .await?;
+
+    assert_eq!(weapons_count().await, 900);
+
     Ok(())
 }
