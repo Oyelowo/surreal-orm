d8b1ac2f126c185d4bf452fcda811663739e5747
oyelowo <oyelowooyedayo@gmail.com>
2023-08-15 17:26:36 -0600

    Add more para_with_path tests

--- rust/surreal-orm-tests/tests/param_with_path.rs
+++ rust/surreal-orm-tests/tests/param_with_path.rs
@@ -6,8 +6,8 @@
  */
 
 use pretty_assertions::assert_eq;
-use surreal_models::Student;
-use surreal_orm::{index, this, Buildable, ToRaw, E};
+use surreal_models::{student_schema, Student};
+use surreal_orm::{index, this, where_, All, Buildable, Operatable, SchemaGetter, ToRaw, E};
 
 #[test]
 fn test_param_with_path() {
@@ -42,6 +42,62 @@ fn test_param_with_path_no_clause() {
     assert_eq!(
         param_with_path.to_raw().build(),
         "$this[2].bestFriend.bestFriend.course.title"
+    );
+}
+
+#[test]
+fn test_param_with_path_with_clause() {
+    let student_schema::Student { age, .. } = Student::schema();
+
+    let param_with_path = this()
+        .with_path::<Student>(where_(age.greater_than(18)))
+        .bestFriend()
+        .allSemesterCourses(index(5))
+        .title;
+
+    assert_eq!(
+        param_with_path.fine_tune_params(),
+        "$this[WHERE age > $_param_00000001].bestFriend.allSemesterCourses[$_param_00000002].title"
+    );
+    assert_eq!(
+        param_with_path.to_raw().build(),
+        "$this[WHERE age > 18].bestFriend.allSemesterCourses[5].title"
+    );
+}
+
+#[test]
+fn test_param_with_path_with_all_wildcard() {
+    let param_with_path = this()
+        .with_path::<Student>(All)
+        .bestFriend()
+        .allSemesterCourses(index(5))
+        .title;
+
+    assert_eq!(
+        param_with_path.fine_tune_params(),
+        "$this[*].bestFriend.allSemesterCourses[$_param_00000001].title"
+    );
+    assert_eq!(
+        param_with_path.to_raw().build(),
+        "$this[*].bestFriend.allSemesterCourses[5].title"
+    );
+}
+
+#[test]
+fn test_param_with_path_multiple_indexes() {
+    let param_with_path = this()
+        .with_path::<Student>(index(2))
+        .bestFriend()
+        .allSemesterCourses(index(5))
+        .title;
+
+    assert_eq!(
+        param_with_path.fine_tune_params(),
+        "$this[$_param_00000001].bestFriend.allSemesterCourses[$_param_00000002].title"
+    );
+    assert_eq!(
+        param_with_path.to_raw().build(),
+        "$this[2].bestFriend.allSemesterCourses[5].title"
     );
 }
 
