8b807a7c748153e13a40184c781b4689664a4598
oyelowo <oyelowooyedayo@gmail.com>
2023-08-14 18:00:56 -0600

    Add initial edge_attributes setup

---
+++ rust/surreal-orm-tests/tests/edge_attributes.rs
@@ -0,0 +1,73 @@
+use surreal_models::AlienVisitsPlanet;
+use surreal_orm::*;
+// use surreal_orm::{Model, SurrealOrmResult};
+use surrealdb::{engine::local::Mem, Surreal};
+
+// #[tokio::test]
+// async fn test_node_atttributes_auto_inferred() -> SurrealOrmResult<()> {
+//     let db = Surreal::new::<Mem>(()).await.unwrap();
+//
+//     db.use_ns("test").use_db("test").await.unwrap();
+//     assert_eq!(
+//         AlienVisitsPlanet::define_table().to_raw().build(),
+//         "DEFINE TABLE alien;"
+//     );
+//
+//     assert_eq!(
+//         AlienVisitsPlanet::define_fields()
+//             .iter()
+//             .map(|x| x.to_raw().build())
+//             .collect::<Vec<_>>()
+//             .join("\n"),
+//         "DEFINE FIELD id ON TABLE alien TYPE record (alien);
+// DEFINE FIELD name ON TABLE alien TYPE string;
+// DEFINE FIELD age ON TABLE alien TYPE int;
+// DEFINE FIELD created ON TABLE alien TYPE datetime;
+// DEFINE FIELD lifeExpectancy ON TABLE alien TYPE duration;
+// DEFINE FIELD linePolygon ON TABLE alien TYPE geometry (feature);
+// DEFINE FIELD territoryArea ON TABLE alien TYPE geometry (feature);
+// DEFINE FIELD home ON TABLE alien TYPE geometry (feature);
+// DEFINE FIELD tags ON TABLE alien TYPE array;
+// DEFINE FIELD ally ON TABLE alien TYPE record (alien);
+// DEFINE FIELD weapon ON TABLE alien TYPE record (weapon);
+// DEFINE FIELD spaceShips ON TABLE alien TYPE array;
+// DEFINE FIELD spaceShips.* ON TABLE alien TYPE record (space_ship);"
+//     );
+//
+//     Ok(())
+// }
+
+//
+// #[tokio::test]
+// async fn test_node_atttributes_explicit() -> SurrealOrmResult<()> {
+//     let db = Surreal::new::<Mem>(()).await.unwrap();
+//     db.use_ns("test").use_db("test").await.unwrap();
+//     assert_eq!(
+//         AlienWithExplicitAttributes::define_table().to_raw().build(),
+//         "DEFINE TABLE alien_with_explicit_attributes;"
+//     );
+//
+//     assert_eq!(
+//         AlienWithExplicitAttributes::define_fields()
+//             .iter()
+//             .map(|x| x.to_raw().build())
+//             .collect::<Vec<_>>()
+//             .join("\n"),
+//         "DEFINE FIELD id ON TABLE alien_with_explicit_attributes TYPE record (alien_with_explicit_attributes);
+// DEFINE FIELD name ON TABLE alien_with_explicit_attributes TYPE string;
+// DEFINE FIELD age ON TABLE alien_with_explicit_attributes TYPE int;
+// DEFINE FIELD created ON TABLE alien_with_explicit_attributes TYPE datetime;
+// DEFINE FIELD lifeExpectancy ON TABLE alien_with_explicit_attributes TYPE duration;
+// DEFINE FIELD territoryArea ON TABLE alien_with_explicit_attributes TYPE geometry (feature);
+// DEFINE FIELD home ON TABLE alien_with_explicit_attributes TYPE geometry (feature);
+// DEFINE FIELD tags ON TABLE alien_with_explicit_attributes TYPE array;
+// DEFINE FIELD tags.* ON TABLE alien_with_explicit_attributes TYPE string;
+// DEFINE FIELD ally ON TABLE alien_with_explicit_attributes TYPE record (alien_with_explicit_attributes);
+// DEFINE FIELD weapon ON TABLE alien_with_explicit_attributes TYPE record (weapon);
+// DEFINE FIELD spaceShips ON TABLE alien_with_explicit_attributes TYPE array;
+// DEFINE FIELD spaceShips.* ON TABLE alien_with_explicit_attributes TYPE record (space_ship);"
+//     );
+//
+//     Ok(())
+// }
+//
