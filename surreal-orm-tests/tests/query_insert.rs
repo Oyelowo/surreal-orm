9847a4cf29ecee7fd6243ba1ef9a352842f5de55
oyelowo <oyelowooyedayo@gmail.com>
2023-08-25 12:57:46 -0600

    Remove unnecessary use of defaults

--- surreal-orm-tests/tests/query_insert.rs
+++ surreal-orm-tests/tests/query_insert.rs
@@ -480,14 +480,12 @@ async fn test_create_fetch_values_of_one_to_many_record_links_with_alias() -> Su
         id: spaceship_id_2,
         name: "SpaceShip2".to_string(),
         created: Utc::now(),
-        ..Default::default()
     };
 
     let space_ship3 = SpaceShip {
         id: spaceship_id_3,
         name: "Oyelowo".to_string(),
         created: Utc::now(),
-        ..Default::default()
     };
 
     let point = point! {
@@ -643,14 +641,12 @@ async fn test_alien_build_output() -> SurrealOrmResult<()> {
         id: spaceship_id_2,
         name: "SpaceShip2".to_string(),
         created: Utc::now(),
-        ..Default::default()
     };
 
     let space_ship3 = SpaceShip {
         id: spaceship_id_3,
         name: "Oyelowo".to_string(),
         created: Utc::now(),
-        ..Default::default()
     };
 
     let created_spaceship1 = insert(space_ship1.clone()).get_one(db.clone()).await?;
@@ -720,14 +716,12 @@ async fn test_access_array_record_links_with_some_null_links() -> SurrealOrmResu
         id: spaceship_id_2,
         name: "SpaceShip2".to_string(),
         created: Utc::now(),
-        ..Default::default()
     };
 
     let space_ship3 = SpaceShip {
         id: spaceship_id_3,
         name: "Oyelowo".to_string(),
         created: Utc::now(),
-        ..Default::default()
     };
 
     let point = point! {
@@ -843,14 +837,12 @@ async fn test_return_non_null_links() -> SurrealOrmResult<()> {
         id: spaceship_id_2,
         name: "SpaceShip2".to_string(),
         created: Utc::now(),
-        ..Default::default()
     };
 
     let space_ship3 = SpaceShip {
         id: spaceship_id_3,
         name: "Oyelowo".to_string(),
         created: Utc::now(),
-        ..Default::default()
     };
 
     let point = point! {
@@ -988,14 +980,12 @@ async fn test_insert_multiple_nodes_return_non_null_links() -> SurrealOrmResult<
         id: spaceship_id_2,
         name: "SpaceShip2".to_string(),
         created: Utc::now(),
-        ..Default::default()
     };
 
     let space_ship3 = SpaceShip {
         id: spaceship_id_3,
         name: "Oyelowo".to_string(),
         created: Utc::now(),
-        ..Default::default()
     };
 
     let point = point! {
