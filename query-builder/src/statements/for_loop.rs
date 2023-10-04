7b97fd76ba322c313fa5977b77f200722f2a02af
oyelowo <oyelowooyedayo@gmail.com>
2023-10-04 17:14:49 -0600

    Support For loop within block macro

--- query-builder/src/statements/for_loop.rs
+++ query-builder/src/statements/for_loop.rs
@@ -62,6 +62,17 @@ impl From<&Param> for ForParam {
     }
 }
 
+// #[macro_export]
+// macro_rules! FOR {
+//     ($param:ident IN $iterable:expr { $($stmt:expr;)+ }) => {{
+//         let ref $param = $crate::Param::new(stringify!($param));
+//         $crate::for_($param).in_($iterable).block($crate::block! {
+//             $($stmt;)+
+//         })
+//     }};
+// }
+
+
 /// A helper function to create a for loop
 /// ```
 /// use surreal_query_builder as surreal_orm;
@@ -200,8 +211,60 @@ impl fmt::Display for ForLoopStatement {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{statements::select::{select, select_value}, *};
+    use crate::{statements::{if_, select::{select, select_value}}, *};
 
+    #[test]
+    fn test_for_in_blockx() {
+        let ref person_table = Table::from("person");
+        let ref user_name = Field::from("user_name");
+
+        let for_loop = block! {
+            FOR (__name IN vec!["Oyelowo", "Oyedayo"]) {
+                select(All).from(person_table).where_(user_name.eq(__name));
+                select(All).from(person_table).where_(user_name.eq(__name));
+            
+
+                for_(__name).in_(vec!["Oyelowo", "Oyedayo"])
+                    .block(block! {
+                        select(All).from(person_table).where_(user_name.eq(__name));
+                });
+
+            };
+            
+            FOR (__name IN vec!["Oyelowo", "Oyedayo"]) {
+                select(All).from(person_table).where_(user_name.eq(__name));
+                select(All).from(person_table).where_(user_name.eq(__name));
+            };
+            
+            FOR (__name IN vec!["Oyelowo", "Oyedayo"]) {
+                select(All).from(person_table).where_(user_name.eq(__name));
+                select(All).from(person_table).where_(user_name.eq(__name));
+            };
+
+            if_(__name.eq("Oyelowo")).then(6).end();
+
+
+            
+        };
+            // IF (__name.eq("Oyelowo")) THEN {
+            //     select(All).from(person_table).where_(user_name.eq(__name));
+            //     select(All).from(person_table).where_(user_name.eq(__name));
+            // } ELSE IF (__name.eq("Oyelowo")) THEN{
+            //     select(All).from(person_table).where_(user_name.eq(__name));
+            //     select(All).from(person_table).where_(user_name.eq(__name));
+            // };
+        
+
+        assert_eq!(
+            for_loop.fine_tune_params(),
+            "FOR $name IN $_param_00000001 {\nSELECT * FROM person WHERE user_name = $name;\n}"
+        );
+        assert_eq!(
+            for_loop.to_raw().build(),
+            "FOR $name IN ['Oyelowo', 'Oyedayo'] {\nSELECT * FROM person WHERE user_name = $name;\n}"
+        );
+    }
+    
     #[test]
     fn test_for_in_block() {
         let ref __name = Param::new("name");
