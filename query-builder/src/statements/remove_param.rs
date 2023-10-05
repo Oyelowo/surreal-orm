b1497ff25e891d37a7758a8835f1b742f0720112
oyelowo <oyelowooyedayo@gmail.com>
2023-10-05 06:11:43 -0600

    Implement remove analuzer and remove param statements

--- query-builder/src/statements/remove_param.rs
+++ query-builder/src/statements/remove_param.rs
@@ -1,3 +1,9 @@
+/*
+ * Author: Oyelowo Oyedayo
+ * Email: oyelowooyedayo@gmail.com
+ * Copyright (c) 2023 Oyelowo Oyedayo
+ * Licensed under the MIT license
+ */
 use std::fmt::{self, Display};
 
 use crate::{
