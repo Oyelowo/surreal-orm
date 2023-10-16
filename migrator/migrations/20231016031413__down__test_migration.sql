419678c7ade31c449a0aaf0928bfd16fd5df5e6b
oyelowo <oyelowooyedayo@gmail.com>
2023-10-15 21:17:07 -0600

    Initial refactoring successful

---
+++ migrator/migrations/20231016031413__down__test_migration.sql
@@ -0,0 +1,6 @@
+REMOVE TABLE eats;;
+REMOVE FIELD place ON TABLE eats;;
+REMOVE FIELD createdAt ON TABLE eats;;
+REMOVE FIELD in ON TABLE eats;;
+REMOVE FIELD out ON TABLE eats;;
+REMOVE FIELD id ON TABLE eats;
