c3e8a19ee919ea30054c91d5b8c7d65a0c1507a7
oyelowo <oyelowooyedayo@gmail.com>
2023-10-17 09:43:49 -0600

    Add new migration files

--- migrator/migrations/20231015043252__down__test_migration.sql
+++
@@ -1,6 +0,0 @@
-REMOVE FIELD species ON TABLE animal;;
-REMOVE FIELD id ON TABLE animal;;
-REMOVE FIELD createdAt ON TABLE animal;;
-REMOVE FIELD attributes ON TABLE animal;;
-REMOVE FIELD color ON TABLE crop;;
-REMOVE FIELD id ON TABLE crop;
--- migrator/migrations/20231015224536__down__test_migration.sql
+++
@@ -1,6 +0,0 @@
-REMOVE FIELD attributes ON TABLE animal;;
-REMOVE FIELD createdAt ON TABLE animal;;
-REMOVE FIELD id ON TABLE animal;;
-REMOVE FIELD species ON TABLE animal;;
-REMOVE FIELD id ON TABLE crop;;
-REMOVE FIELD color ON TABLE crop;
