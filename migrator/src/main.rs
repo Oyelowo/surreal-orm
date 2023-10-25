e7626bac64c82a372b4a8cd1037320bb7eaf3705
oyelowo <oyelowooyedayo@gmail.com>
2023-10-25 13:37:33 -0600

    Rename migration running function to generate function

--- migrator/src/main.rs
+++ migrator/src/main.rs
@@ -14,7 +14,7 @@ use surrealdb::sql::{
 
 #[tokio::main]
 async fn main() {
-    if let Err(e) = m::Database::run_migrations(&"create_new_stuff".to_string(), true).await {
+    if let Err(e) = m::Database::generate_migrations(&"create_new_stuff".to_string(), true).await {
         println!("Error: {}", e);
     }
 }
