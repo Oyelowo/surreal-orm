b909a629d5f127cd0ed341f19c2493c23560a4b9
oyelowo <oyelowooyedayo@gmail.com>
2023-10-29 14:26:29 -0600

    Include examples for generating one way and two way migrations for embedded

--- examples/migration-embedded/src/main.rs
+++ examples/migration-embedded/src/main.rs
@@ -1,30 +1,52 @@
 use surreal_models::migrations::Resources;
-use surreal_orm::migrator::{FileManager, MigrationFlag, MigratorDatabase, Mode};
 use surreal_orm::migrator::{self, embed_migrations};
+use surreal_orm::migrator::{FileManager, MigrationFlag, MigratorDatabase, Mode};
 
-// const MIGRATIONS: migrator::EmbeddedMigrationsOneWay =
-//     embed_migrations!("migrations", one_way, strict);
+// Embed migrations as constant
+const MIGRATIONS_ONE_WAY: migrator::EmbeddedMigrationsOneWay =
+    embed_migrations!("migrations-oneway", one_way, strict);
+const MIGRATIONS_TWO_WAY: migrator::EmbeddedMigrationsTwoWay = embed_migrations!("migrations");
 
-// const MIGRATIONS2: migrator::EmbeddedMigrationsTwoWay = embed_migrations!("../migrator/migrations");
-// const MIGRATIONS2: migrator::EmbeddedMigrationsTwoWay =
-const MIGRATIONS2: migrator::EmbeddedMigrationsTwoWay = embed_migrations!("kaod");
-
-
-#[tokio::main]
-async fn main() {
-    // GENERATE MIGRATIONS
+async fn generate_one_way_migrations() {
     let file_manager = FileManager {
         mode: Mode::Strict,
-        custom_path: Some("../kaod".to_string()),
+        // custom_path: None,
+        custom_path: Some("migrations-oneway".to_string()),
         //  Defaults to 'migrations'
         // custom_path: None,
-        migration_flag: MigrationFlag::TwoWay,
+        migration_flag: MigrationFlag::OneWay,
     };
-    let file_manager = FileManager::default();
+    // let file_manager = FileManager::default();
     if let Err(e) =
         MigratorDatabase::generate_migrations("create_new_stuff".into(), &file_manager, Resources)
             .await
     {
         println!("Error: {}", e);
     }
+}
+
+async fn generate_two_way_migrations() {
+    let file_manager = FileManager {
+        mode: Mode::Strict,
+        // custom_path: None,
+        custom_path: Some("migrations-oneway".to_string()),
+        //  Defaults to 'migrations'
+        // custom_path: None,
+        migration_flag: MigrationFlag::OneWay,
+    };
+    // let file_manager = FileManager::default();
+    if let Err(e) =
+        MigratorDatabase::generate_migrations("create_new_stuff".into(), &file_manager, Resources)
+            .await
+    {
+        println!("Error: {}", e);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // GENERATE MIGRATIONS
+    // Comment out one of the following to generate migrations
+    // generate_two_way_migrations().await;
+    // generate_one_way_migrations().await;
 }
