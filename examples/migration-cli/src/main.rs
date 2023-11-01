953bcd44b97e5beda52a521902db7b075037e38c
oyelowo <oyelowooyedayo@gmail.com>
2023-10-31 22:29:22 -0600

    Document cli command args

--- examples/migration-cli/src/main.rs
+++ examples/migration-cli/src/main.rs
@@ -5,17 +5,8 @@ use surrealdb::engine::any::{connect, Any};
 use surrealdb::opt::auth::Root;
 use surrealdb::Surreal;
 
-// async fn initialize_db() -> Surreal<surrealdb::engine::remote::ws::Client> {
 async fn initialize_db() -> Surreal<Any> {
-    // let db = Surreal::new::<Ws>("localhost:8000")
-    //     .await
-    //     .expect("Failed to connect to db");
-
     let db = connect("http://localhost:8000").await.unwrap();
-    // let db = Surreal::new::<Ws>("localhost:8000")
-    //     .await
-    //     .expect("Failed to connect to db");
-    // db.connect(address)
     db.signin(Root {
         username: "root",
         password: "root",
@@ -26,12 +17,9 @@ async fn initialize_db() -> Surreal<Any> {
     db
 }
 
-// static DB: Lazy<Surreal<Db>> = Lazy::new(Surreal::init);
-// static DB: Lazy<Surreal<Client>> = Lazy::new(Surreal::init);
-// Can be one of memory, file:<path>, tikv:<addr>, file://<path>, tikv://<addr>
 #[tokio::main]
 async fn main() {
-    let db = initialize_db().await;
-    // include example usage as rust doc
-    cli::migration_cli(Some(db), Resources).await;
+    let _db = initialize_db().await;
+    // cli::migration_cli(Resources, Some(db)).await;
+    cli::migration_cli(Resources, None).await;
 }
