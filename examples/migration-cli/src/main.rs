701ed81c26cb4d259a2338374a038ff6a2a3bc63
oyelowo <oyelowooyedayo@gmail.com>
2023-11-02 19:35:00 -0600

    Add cli to examples

--- examples/migration-cli/src/main.rs
+++ examples/migration-cli/src/main.rs
@@ -1,3 +1,5 @@
+use std::process::{Command, Stdio};
+
 // use pretty_env_logger;
 use surreal_models::migrations::Resources;
 use surreal_orm::migrator::cli;
@@ -22,6 +24,86 @@ async fn initialize_db() -> Surreal<Any> {
 async fn main() {
     // pretty_env_logger::init();
     let _db = initialize_db().await;
+    // Directly run the cli
     // cli::migration_cli(Resources, Some(db)).await;
     cli::migration_cli(Resources, None).await;
+
+    // Run the cli through cargo
+    // _generate();
+    // _run();
+    // _rollback();
+}
+
+fn _generate() {
+    // create
+    let cmd = Command::new("cargo")
+        .arg("run")
+        .arg("--")
+        .arg("generate")
+        .arg("--name")
+        .arg("test migration")
+        // .arg("--migrations-dir")
+        // .arg("migrations")
+        .arg("-r")
+        .stdin(Stdio::piped())
+        .spawn()
+        .expect("Failed to run command");
+
+    // Wait for the command to finish
+    let output = cmd.wait_with_output().expect("Failed to read stdout");
+
+    // Validate output (replace this with your actual validation)
+    assert!(output.status.success());
+}
+
+fn _run() {
+    // RUN AGAINST DB
+    let db_url = format!("http://localhost:8000");
+
+    let cmd = Command::new("cargo")
+        .arg("run")
+        .arg("--")
+        .arg("run")
+        .arg("--migrations-dir")
+        .arg("migrations")
+        .arg("--db")
+        .arg("test")
+        .arg("--ns")
+        .arg("test")
+        .arg("--path")
+        .arg(&db_url)
+        .arg("-r")
+        .spawn()
+        .expect("Failed to run command");
+
+    let output = cmd.wait_with_output().expect("Failed to read stdout");
+    // Validate output (replace this with your actual validation)
+    assert!(output.status.success());
+}
+
+fn _rollback() {
+    let db_url = format!("http://localhost:8000");
+    // Rollback
+    let cmd = Command::new("cargo")
+        .arg("run")
+        .arg("--")
+        .arg("rollback")
+        .arg("--migrations-dir")
+        .arg("migrations")
+        .arg("--db")
+        .arg("test")
+        .arg("--ns")
+        .arg("test")
+        .arg("--path")
+        .arg(&db_url)
+        .arg("-r")
+        .stdin(Stdio::piped())
+        .spawn()
+        .expect("Failed to run command");
+
+    // Wait for the command to finish
+    let output = cmd.wait_with_output().expect("Failed to read stdout");
+
+    // Validate output (replace this with your actual validation)
+    assert!(output.status.success());
 }
