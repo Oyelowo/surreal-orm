c66b520a3c2e935a7cffd766e0ff9502243f4b18
oyelowo <oyelowooyedayo@gmail.com>
2023-11-04 17:47:47 -0600

    Add query macro to integration tests

--- orm-tests/tests/query_macro.rs
+++ orm-tests/tests/query_macro.rs
@@ -1,6 +1,27 @@
 use surreal_orm::{query, query_raw};
 use surrealdb::{engine::local::Mem, Surreal};
 
+#[tokio::test]
+async fn test_query_macro_with_bindigns() {
+    let db = Surreal::new::<Mem>(()).await.unwrap();
+    db.use_ns("test").use_db("test").await.unwrap();
+
+    let _query = query!(db, "SELECT * FROM users").await;
+    let _query = query!(db, "SELECT * FROM users", {}).await;
+    let _query = query!(db, "SELECT * FROM users WHERE id = $id", {id : 1} ).await;
+    let username = "Oyelowo";
+    let _query = query!(db, "SELECT name, age FROM users WHERE id = $id AND name = $name", {
+        id : 1,
+        name : username
+    })
+    .await;
+
+    // db.query("SELECT name, age, * FROM users WHERE id = $id AND name = $name")
+    //     .bind(("id", 1))
+    //     .bind(("name", "Oyelowo"));
+    // assert_eq!(query, "SELECT * FROM users");
+}
+
 #[test]
 fn test_query_macro() {
     let query = query_raw!("SELECT name, age, * FROM users");
@@ -23,25 +44,4 @@ fn test_query_macro_with_graph() {
         query,
         "SELECT ->purchased->product<-purchased<-person->purchased->product FROM person:tobie PARALLEL"
     );
-}
-
-#[tokio::test]
-async fn test_query_simple() {
-    let db = Surreal::new::<Mem>(()).await.unwrap();
-    db.use_db("test").await;
-    db.use_ns("test").await;
-
-    let query = query!(db, "SELECT * FROM users");
-    let query = query!(db, "SELECT * FROM users", {});
-    let query = query!(db, "SELECT * FROM users WHERE id = $id", {id : 1} );
-    let username = "Oyelowo";
-    let query = query!(db, "SELECT name, age FROM users WHERE id = $id AND name = $name", {
-        id : 1,
-        name : username
-    });
-
-    // db.query("SELECT name, age, * FROM users WHERE id = $id AND name = $name")
-    //     .bind(("id", 1))
-    //     .bind(("name", "Oyelowo"));
-    // assert_eq!(query, "SELECT * FROM users");
 }
