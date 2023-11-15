1c81c41f7bec68d301fef5d2f06f9dc59be76d39
oyelowo <oyelowooyedayo@gmail.com>
2023-11-14 20:04:46 -0700

    Add complex test example

--- orm-tests/tests/query_turbo.rs
+++ orm-tests/tests/query_turbo.rs
@@ -23,66 +23,6 @@ async fn test_transaction_with_block_macro() -> SurrealOrmResult<()> {
     let amount_to_transfer = 300.00;
 
     let acc = Account::schema();
-    let xxxx = {
-        #[allow(non_snake_case)]
-        let ref balance1 =
-            surreal_orm::statements::let_("balance1").equal_to(create().content(Balance {
-                id: Balance::create_id("balance1".to_string()),
-                amount: amount_to_transfer,
-            }));
-        let _surreal_orm__private__internal_variable_prefix__e6dab861d67247bf9143a127f81a0eba =
-            create().content(Balance {
-                id: Balance::create_id("balance1".to_string()),
-                amount: amount_to_transfer,
-            });
-        let _surreal_orm__private__internal_variable_prefix__6710e39e970f4b25b9d16eff46c9025d = {
-            let name = surreal_orm::Param::new("name");
-            let ref first = surreal_orm::statements::let_("first").equal_to("Oyelowo");
-            let _surreal_orm__private__internal_variable_prefix__cc7896b084aa43028a3fbb970ca13cc9 =
-                select(All)
-                    .from(Account::table_name())
-                    .where_(acc.balance.eq(5));
-            let ref good_stmt = surreal_orm::statements::let_("good_stmt").equal_to(
-                select(All)
-                    .from(Account::table_name())
-                    .where_(acc.balance.eq(64)),
-            );
-            let _surreal_orm__private__internal_variable_prefix__e1073ae4b43a4ecd84d96fa5fd7ed96d =
-                select(All)
-                    .from(Account::table_name())
-                    .where_(acc.balance.eq(34));
-            surreal_orm::statements::for_(name).in_(
-                vec!["Oyelowo","Oyedayo"]).block(surreal_orm::chain(first)
-                    .chain(_surreal_orm__private__internal_variable_prefix__cc7896b084aa43028a3fbb970ca13cc9)
-                    .chain(good_stmt).chain(_surreal_orm__private__internal_variable_prefix__e1073ae4b43a4ecd84d96fa5fd7ed96d).parenthesized())
-        };
-        let ref balance3 =
-            surreal_orm::statements::let_("balance3").equal_to(create().content(Balance {
-                id: Balance::create_id("balance1".into()),
-                amount: amount_to_transfer,
-            }));
-        let ref accounts =
-            surreal_orm::statements::let_("accounts").equal_to(select(All).from(id1..=id2));
-        let ref updated1 = surreal_orm::statements::let_("updated1").equal_to(
-            update::<Account>(id1).set(
-                acc.balance
-                    .increment_by(balance1.with_path::<Balance>(E).amount),
-            ),
-        );
-        let ref update2 = surreal_orm::statements::let_("update2")
-            .equal_to(update::<Account>(id2).set(acc.balance.decrement_by(amount_to_transfer)));
-        surreal_orm::chain(balance1)
-            .chain(
-                _surreal_orm__private__internal_variable_prefix__e6dab861d67247bf9143a127f81a0eba,
-            )
-            .chain(
-                _surreal_orm__private__internal_variable_prefix__6710e39e970f4b25b9d16eff46c9025d,
-            )
-            .chain(balance3)
-            .chain(accounts)
-            .chain(updated1)
-            .chain(update2)
-    };
 
     let query_chain = query_turbo! {
 
