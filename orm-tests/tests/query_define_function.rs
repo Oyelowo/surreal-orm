e1c1410aa09f73ef7621f5fce4ccc5856b3eedbc
oyelowo <oyelowooyedayo@gmail.com>
2023-11-30 12:06:47 -0700

    Parse define function argument manually

--- orm-tests/tests/query_define_function.rs
+++ orm-tests/tests/query_define_function.rs
@@ -14,7 +14,7 @@ use surreal_orm::{
     All, Buildable, Model, Operatable, SchemaGetter, SetterAssignable, ToRaw, NONE,
 };
 
-define_function!(get_person(first_arg: string, last_arg: string, birthday_arg: string) {
+define_function!(get_person(first_arg: option<array<bytes, 6>> , last_arg: string, birthday_arg: string) {
     let person = select(All)
         .from(SpaceShip::table_name())
         .where_(
@@ -75,40 +75,117 @@ fn test_function_definition() {
     );
 }
 
+/*
+*
+*
+* // define_function!(get_person(first_arg: string, last_arg: string, birthday_arg: string) {
+//     let person = select(All)
+//         .from(SpaceShip::table_name())
+//         .where_(
+//             cond(SpaceShip::schema().id.equal(&first_arg))
+//                 .and(SpaceShip::schema().name.equal(&last_arg))
+//                 .and(SpaceShip::schema().created.equal(&birthday_arg)),
+//         );
 //
-// use super::*;
-// use crate::*;
-//
-// define_function!(get_it(first: bool, last: string, birthday: string) {
-//     let person = "43";
-//     return person;
+//     return if_(person.with_path::<SpaceShip>(index(0)).id.is_not(NONE))
+//                 .then(person)
+//             .else_(
+//                 create::<SpaceShip>().set(
+//                     vec![
+//                         SpaceShip::schema().id.equal_to(&first_arg),
+//                         SpaceShip::schema().name.equal_to(&last_arg),
+//                         SpaceShip::schema().created.equal_to(&birthday_arg),
+//                     ]
+//                 )
+//             ).end();
 // });
 //
 // #[test]
-// fn test_define_function() {
-//     let fn_statement = get_it_statement();
-//
-//     insta::assert_display_snapshot!(fn_statement.to_raw().build());
-//     insta::assert_display_snapshot!(fn_statement.fine_tune_params());
+// fn test_function_definition() {
+//     let person = get_person("Oyelowo", "Oyedayo", "2022-09-21");
+//     insta::assert_display_snapshot!(person.to_raw().build());
+//     insta::assert_display_snapshot!(person.fine_tune_params());
 //     assert_eq!(
-//         fn_statement.to_raw().build(),
-//         "DEFINE FUNCTION get_it($first: bool, $last: string, $birthday: string) {\n\
-//             LET $person = '43';\n\n\
-//             RETURN $person;\n\
+//         person.to_raw().build(),
+//         "get_person('Oyelowo', 'Oyedayo', '2022-09-21')"
+//     );
+//     assert_eq!(
+//         person.fine_tune_params(),
+//         "get_person($_param_00000001, $_param_00000002, $_param_00000003)"
+//     );
+//
+//     let person_statement = get_person_statement();
+//     insta::assert_display_snapshot!(person_statement.to_raw().build());
+//     insta::assert_display_snapshot!(person_statement.fine_tune_params());
+//
+//     assert_eq!(
+//         person_statement.to_raw().build(),
+//         "DEFINE FUNCTION get_person($first_arg: string, $last_arg: string, $birthday_arg: string) {\n\
+//             LET $person = (SELECT * FROM space_ship WHERE (id = $first_arg) AND (name = $last_arg) AND \
+//             (created = $birthday_arg));\n\n\
+//             RETURN IF $person[0].id != NONE THEN \
+//             $person[0] \
+//             ELSE \
+//             (CREATE space_ship SET id = $first_arg, name = $last_arg, created = $birthday_arg) \
+//             END;\n\
 //             };"
 //     );
-//     assert_eq!(
-//         fn_statement.fine_tune_params(),
-//         "DEFINE FUNCTION get_it($first: bool, $last: string, $birthday: string) {\n\
-//         LET $person = $_param_00000001;\n\n\
-//         RETURN $person;\n\
-//         };"
-//     );
-//     let get_it_function = get_it(false, "3".to_string(), "3".to_string());
-//     assert_eq!(get_it_function.to_raw().build(), "get_it(false, '3', '3')");
-//     assert_eq!(
-//         get_it_function.fine_tune_params(),
-//         "get_it($_param_00000001, $_param_00000002, $_param_00000003)"
+//
+//     assert_eq!(person_statement.fine_tune_params(),
+//     "DEFINE FUNCTION get_person($first_arg: string, $last_arg: string, $birthday_arg: string) {\n\
+//             LET $person = $_param_00000001;\n\n\
+//             RETURN $_param_00000002;\n\
+//             };"
 //     );
 // }
 //
+//
+
+// define_function!(get_person(first_arg: string, last_arg: string, birthday_arg: string) {
+//     let person = select(All)
+//         .from(SpaceShip::table_name())
+//         .where_(
+//             cond(SpaceShip::schema().id.equal(&first_arg))
+//                 .and(SpaceShip::schema().name.equal(&last_arg))
+//                 .and(SpaceShip::schema().created.equal(&birthday_arg)),
+//         );
+//
+//     return if_(person.with_path::<SpaceShip>(index(0)).id.is_not(NONE))
+//                 .then(person)
+//             .else_(
+//                 create::<SpaceShip>().set(
+//                     vec![
+//                         SpaceShip::schema().id.equal_to(&first_arg),
+//                         SpaceShip::schema().name.equal_to(&last_arg),
+//                         SpaceShip::schema().created.equal_to(&birthday_arg),
+//                     ]
+//                 )
+//             ).end();
+// });
+//
+//
+// define_function!(get_person(first_arg: string, last_arg: string, birthday_arg: string) {
+//     let person = select(All)
+//         .from(SpaceShip::table_name())
+//         .where_(
+//             cond(SpaceShip::schema().id.equal(&first_arg))
+//                 .and(SpaceShip::schema().name.equal(&last_arg))
+//                 .and(SpaceShip::schema().created.equal(&birthday_arg)),
+//         );
+//
+//     if person.with_path::<SpaceShip>(index(0)).id.is_not(NONE) {
+//         return person;
+//     } else {
+//         return create::<SpaceShip>().set(
+//             vec![
+//                 SpaceShip::schema().id.equal_to(&first_arg),
+//                 SpaceShip::schema().name.equal_to(&last_arg),
+//                 SpaceShip::schema().created.equal_to(&birthday_arg),
+//             ]
+//         );
+//     };
+// });
+// //
+
+*
+* */
