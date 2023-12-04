eb4315ea9b9b8a1a4c28ddc8ae63186e46ba5cd2
oyelowo <oyelowooyedayo@gmail.com>
2023-12-04 16:08:12 -0700

    Support if else as an expression to let statement

--- query-builder-macros/src/statement_parser/let_.rs
+++ query-builder-macros/src/statement_parser/let_.rs
@@ -1,5 +1,6 @@
 use std::fmt::Display;
 
+use super::return_::ReturnExpr;
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{
@@ -13,18 +14,23 @@ use proc_macros_helpers::get_crate_name;
 pub(crate) struct LetStatementParser {
     pub ident: Ident,
     pub _eq: Token![=],
-    pub expr: Expr,
+    pub expr: ReturnExpr,
 }
 
 impl Parse for LetStatementParser {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let _let: Token![let] = input.parse()?;
-        let let_statement = LetStatementParser {
-            ident: input.parse()?,
-            _eq: input.parse()?,
-            expr: input.parse()?,
-        };
-        let _semi: Token![;] = input.parse()?;
+        let ident = input.parse()?;
+        let _eq: Token![=] = input.parse()?;
+
+        let expr = input.parse()?;
+        let is_if_else = matches!(expr, ReturnExpr::IfElse(_));
+
+        let let_statement = LetStatementParser { ident, _eq, expr };
+        // if else already has a semicolon
+        if !is_if_else {
+            let _semi: Token![;] = input.parse()?;
+        }
         Ok(let_statement)
     }
 }
