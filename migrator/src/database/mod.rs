20f72677f77f16c058b960213ceefb997ec4a522
oyelowo <oyelowooyedayo@gmail.com>
2023-12-20 18:55:55 -0700

    Rename and reorg mig files

--- migrator/src/database/mod.rs
+++ migrator/src/database/mod.rs
@@ -6,31 +6,31 @@
  */
 
 mod checksum;
+mod db_left_mig_dir;
+mod db_right_codebase;
+mod db_runner;
 mod embedded_migration;
 mod file_manager;
 mod file_metadata;
 mod filecontent;
 mod filename;
 mod full_db_info;
-mod left_db;
-mod live_db;
 mod migration_model;
 mod migrator_db;
 mod queries;
-mod right_db;
 mod settings;
 
 pub use checksum::*;
+pub use db_left_mig_dir::*;
+pub use db_right_codebase::*;
+pub use db_runner::*;
 pub use embedded_migration::*;
 pub use file_manager::*;
 pub use file_metadata::*;
 pub use filecontent::*;
 pub use filename::*;
 pub use full_db_info::*;
-pub use left_db::*;
-pub use live_db::*;
 pub use migration_model::*;
 pub use migrator_db::*;
 pub use queries::*;
-pub use right_db::*;
 pub use settings::*;
