14d705001fad25d7a7396c49335fd14edbc02803
oyelowo <oyelowooyedayo@gmail.com>
2023-12-23 08:25:27 -0700

    Complete second init integration test

--- migrator-tests/tests/init.rs
+++ migrator-tests/tests/init.rs
@@ -3,6 +3,8 @@ use std::{
     path::PathBuf,
 };
 
+use surrealdb::engine::any::Any;
+use surrealdb::Surreal;
 // use std::{
 //     io::Write,
 //     process::{Command, Stdio},
@@ -147,3 +149,82 @@ async fn test_duplicate_up_only_init_without_run() {
     assert_migration_files_presence_and_format(migration_files, test_migration_name);
 
 }
+
+
+#[tokio::test]
+async fn test_duplicate_up_only_init_and_run() {
+    let mig_dir = tempdir().expect("Failed to create temp directory");
+    let temp_test_migration_dir = &mig_dir.path().join("migrations-tests");
+    fs::create_dir_all(temp_test_migration_dir).expect("Failed to create dir");
+    let test_migration_name = "test_migration";
+    let migration_files = read_migs_from_dir(temp_test_migration_dir.clone());
+    assert_eq!(migration_files.len(), 0);
+
+    let runtime_config = RuntimeConfig::builder()
+        .db("test".into())
+        .ns("test".into())
+        .user("root".into())
+        .pass("root".into())
+        .mode(Mode::Strict)
+        .prune(false)
+        .url(UrlDb::Memory)
+        .build();
+
+    let shared_all = SharedAll::builder()
+        .migrations_dir(temp_test_migration_dir.into())
+        .verbose(3)
+        .build();
+
+    let init = Init::builder()
+        .name(test_migration_name.to_string())
+        .reversible(false) // when false, it's up only or one way or unidirectional
+        // We are setting run to true here to run the newly
+        // generated migration files against the specified database instance.
+        .run(true)
+        .runtime_config(runtime_config)
+        .shared_all(shared_all)
+        .build();
+
+    let cli = Cli::new(SubCommand::Init(init));
+    let resources = Resources;
+    let resourcesV2 = ResourcesV2;
+    let mock_prompter = MockPrompter { confirmation: true };
+    
+    let assert_with_db_instance = |db: Surreal<Any>, mig_files_count: u8, db_mig_count: u8| async move {
+        let migrations = Migration::get_all(db).await;
+        let migration_files = read_migs_from_dir(temp_test_migration_dir.clone());
+
+        assert_eq!(
+            migrations.len() as u8,
+            db_mig_count,
+            "No migrations should be created in the database because we set run to false"
+        );
+        assert_eq!(
+            migration_files.len() as u8,
+            mig_files_count,
+            "New migration files should not be created on second init. They must be reset instead if you want to change the reversible type."
+        );
+        assert_migration_files_presence_and_format(migration_files, test_migration_name);
+    };
+
+
+    // 1st run
+    let db = migration_cli_fn(cli.clone(), resources.clone(), mock_prompter.clone()).await;
+
+    assert_with_db_instance(db.clone(), 1, 1);
+
+    // Initialize the 2nd time with same codebase resources. Should not allow creation the second time.
+    let db2 = migration_cli_fn(cli.clone(), resources.clone(), mock_prompter.clone()).await;
+
+    assert_with_db_instance(db.clone(), 1, 1);
+    assert_with_db_instance(db2.clone(), 1, 0);
+
+
+    // Initialize the 3rd time with different codebase resources. Should not allow creation the second time.
+    let db3 = migration_cli_fn(cli, resourcesV2, mock_prompter).await;
+
+    assert_with_db_instance(db.clone(), 1, 1);
+    assert_with_db_instance(db3.clone(), 1, 0);
+
+}
+
