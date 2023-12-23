5eecaaab83900fd17194d534a041400e6affc90a
oyelowo <oyelowooyedayo@gmail.com>
2023-12-22 17:08:42 -0700

    Consolidate mock prompters

--- migrator-tests/tests/cli-interface.rs
+++ migrator-tests/tests/cli-interface.rs
@@ -7,7 +7,7 @@ use surreal_models::migrations::{Resources, ResourcesV2};
 //
 use surreal_orm::migrator::{
     config::{RuntimeConfig, SetupDb, SharedAll, UrlDb},
-    migration_cli_fn, Cli, FalseMockPrompter, Init, SubCommand, TrueMockPrompter,
+    migration_cli_fn, Cli, Init, MockPrompter, SubCommand,
 };
 
 #[tokio::test]
@@ -30,9 +30,8 @@ async fn test_generate_command_success() {
     let cli = Cli::new(SubCommand::Init(init));
     let resources = Resources;
     let resourcesV2 = ResourcesV2;
-    let prompter_returns_true = TrueMockPrompter;
-    let prompter_returns_false = FalseMockPrompter;
-    let db = migration_cli_fn(cli, resources, TrueMockPrompter).await;
+    let mock_prompter = MockPrompter { confirmation: true };
+    let db = migration_cli_fn(cli, resources, mock_prompter).await;
     //
     // let migrations = select(All)
     //     .from(Migration::table_name())
