3fa9ffd20905f48c1545086ae9af44edd3a5935a
oyelowo <oyelowooyedayo@gmail.com>
2023-12-26 00:44:53 -0700

    Support rename arg in prompter

--- migrator/src/database/prompter.rs
+++ migrator/src/database/prompter.rs
@@ -1,16 +1,33 @@
 use inquire::error::InquireError;
 use typed_builder::TypedBuilder;
 
+use crate::table_fields::SingleFieldChangeType;
+
 pub trait Prompter
 where
     Self: std::fmt::Debug,
 {
+    // TODO: Rename to prompt_empty_migration_gen
     fn prompt(&self) -> Result<bool, InquireError> {
         let confirmation =
             inquire::Confirm::new("Are you sure you want to generate an empty migration? (y/n)")
                 .with_default(false)
                 .with_help_message("This is good if you want to write out some queries manually")
                 .prompt();
+        confirmation
+    }
+
+    fn prompt_field_rename(
+        &self,
+        delete_option: SingleFieldChangeType,
+        rename_option: SingleFieldChangeType,
+    ) -> Result<SingleFieldChangeType, InquireError> {
+        let confirmation = inquire::Select::new("Do you want to rename \
+                                this field or delete the old one completely without transferring the data",
+            vec![delete_option, rename_option]
+        )
+        .with_help_message("Use the arrow keys to navigate. Press enter to select.")
+        .prompt();
         confirmation
     }
 }
