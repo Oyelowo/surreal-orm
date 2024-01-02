65325f71132dfe945e87913cb2ae1456b8e9eb8b
oyelowo <oyelowooyedayo@gmail.com>
2024-01-02 15:52:07 -0700

    Simplify prompt base autodetection

--- migrator/src/resources/table_fields.rs
+++ migrator/src/resources/table_fields.rs
@@ -50,11 +50,71 @@ impl<'a, R: DbResources> TableResourcesMeta<Fields> for ComparisonFields<'a, R> 
         let diff_right = diff_right.into_iter().collect::<Vec<_>>();
 
         let mut acc = Queries::default();
-        for name in union {
-            let table = self.get_table();
 
+        let table = self.get_table();
+
+        let might_be_single_field_delete_or_renaming =
+            diff_left.len() == 1 && diff_right.len() == 1;
+
+        let old_name = diff_left.first().map(|n| Field::new(n.to_string()));
+        let new_name = diff_right.first().map(|n| Field::new(n.to_string()));
+
+        let left_defs = self.get_left();
+        let right_defs = self.get_right();
+
+        if might_be_single_field_delete_or_renaming {
+            let field_change_meta = FieldChangeMeta {
+                table: table.to_owned(),
+                old_name: old_name.clone().unwrap().to_owned(),
+                new_name: new_name.clone().unwrap().to_owned(),
+            };
+
+            let prompt = self.prompter.prompt_single_field_rename_or_delete(
+                SingleFieldChangeType::Delete(field_change_meta.clone()),
+                SingleFieldChangeType::Rename(field_change_meta),
+            )?;
+
+            match prompt {
+                SingleFieldChangeType::Rename(meta) => {
+                    let new_name = meta.new_name.to_string();
+                    let old_name = meta.old_name.to_string();
+                    let right_def = left_defs.get_definition(new_name.as_str()).unwrap();
+                    let left_def = right_defs.get_definition(old_name.as_str()).unwrap();
+
+                    acc.add_up(QueryType::Define(right_def.clone()));
+                    let copy_old_to_new = UpdateStatementRaw::from(
+                        Raw::new(format!("UPDATE {table} SET {new_name} = {old_name}")).build(),
+                    );
+                    acc.add_up(QueryType::Update(copy_old_to_new));
+                    acc.add_up(QueryType::Remove(left_def.as_remove_statement()?));
+
+                    acc.add_down(QueryType::Define(left_def.clone()));
+                    let copy_new_to_old = UpdateStatementRaw::from(
+                        Raw::new(format!("UPDATE {table} SET {old_name} = {new_name}",)).build(),
+                    );
+                    acc.add_down(QueryType::Update(copy_new_to_old));
+                    acc.add_down(QueryType::Remove(right_def.as_remove_statement()?));
+                }
+                SingleFieldChangeType::Delete(meta) => {
+                    let new_name = meta.new_name.to_string();
+                    let old_name = meta.old_name.to_string();
+                    let right_def = left_defs.get_definition(new_name.as_str()).unwrap();
+                    let left_def = right_defs.get_definition(old_name.as_str()).unwrap();
+
+                    acc.add_up(QueryType::Define(right_def.clone()));
+                    acc.add_up(QueryType::Remove(left_def.as_remove_statement()?));
+
+                    acc.add_down(QueryType::Define(left_def.clone()));
+                    acc.add_down(QueryType::Remove(right_def.as_remove_statement()?));
+                }
+            };
+
+            return Ok(acc);
+        }
+
+        for field_name in union {
             let change_meta = FieldChangeDetectionMeta {
-                field_name: name.to_string().into(),
+                field_name: field_name.to_string().into(),
                 table: table.to_owned(),
                 left_defs: self.get_left(),
                 right_defs: self.get_right(),
@@ -64,6 +124,7 @@ impl<'a, R: DbResources> TableResourcesMeta<Fields> for ComparisonFields<'a, R> 
                 prompter: self.prompter,
             };
 
+            //
             match DeltaTypeField::try_from(change_meta)? {
                 DeltaTypeField::NoChange => {}
                 DeltaTypeField::Create { right } => {
