a0acd81e1ca2e632dd38e45c4f40c13725c42b03
oyelowo <oyelowooyedayo@gmail.com>
2024-01-06 17:08:27 -0700

    Implement first prune cmd test

--- migrator-tests/tests/prune.rs
+++ migrator-tests/tests/prune.rs
@@ -4,3 +4,44 @@
  * Copyright (c) 2023 Oyelowo Oyedayo
  * Licensed under the MIT license
  */
+
+use migrator_tests::{current_function, AssertionArg, TestConfigNew};
+use surreal_models::migrations::{Resources, ResourcesV3};
+use surreal_orm::migrator::{
+    FastForwardDelta, Generate, Init, Migration, MigrationFilename, MockPrompter, Mode, Reset, Up,
+};
+use tempfile::tempdir;
+use test_case::test_case;
+
+#[test_case(Mode::Strict, true; "Reversible Strict")]
+#[test_case(Mode::Lax, true; "Reversible Lax")]
+#[test_case(Mode::Strict, false; "Non-Reversible Strict")]
+#[test_case(Mode::Lax, false; "Non-Reversible Lax")]
+#[tokio::test]
+async fn test_can_prune_unapplied_migrations(mode: Mode, reversible: bool) {
+    let migration_dir = tempdir().expect("Failed to create temp directory");
+    let migration_dir = &migration_dir.path().join("migrations-tests");
+    let mut conf = TestConfigNew::new(mode, migration_dir, current_function!()).await;
+    conf.generate_12_test_migrations_reversible(reversible)
+        .await;
+    conf.assert_with_db_instance(AssertionArg {
+        expected_mig_files_count: 12,
+        expected_db_mig_meta_count: 0,
+        expected_latest_migration_file_basename_normalized: Some(
+            "migration_12_gen_after_init".into(),
+        ),
+        expected_latest_db_migration_meta_basename_normalized: None,
+        code_origin_line: std::line!(),
+    })
+    .await;
+
+    conf.run_prune().await;
+    conf.assert_with_db_instance(AssertionArg {
+        expected_mig_files_count: 0,
+        expected_db_mig_meta_count: 0,
+        expected_latest_migration_file_basename_normalized: None,
+        expected_latest_db_migration_meta_basename_normalized: None,
+        code_origin_line: std::line!(),
+    })
+    .await;
+}
