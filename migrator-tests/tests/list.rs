fe49aec70845eaf0542c58626ace4bb6f9cc1c33
oyelowo <oyelowooyedayo@gmail.com>
2024-01-08 05:03:20 -0700

    Add more tests for migration listing cmd

--- migrator-tests/tests/list.rs
+++ migrator-tests/tests/list.rs
@@ -15,7 +15,45 @@ use test_case::test_case;
 #[test_case(Mode::Strict, false; "Non-Reversible Strict")]
 #[test_case(Mode::Lax, false; "Non-Reversible Lax")]
 #[tokio::test]
-async fn test_can_list(mode: Mode, reversible: bool) {
+async fn test_migration_listing_based_on_status(mode: Mode, reversible: bool) {
+    let migration_dir = tempdir().expect("Failed to create temp directory");
+    let migration_dir = &migration_dir.path().join("migrations-tests");
+    let mut conf = TestConfigNew::new(mode, migration_dir, current_function!()).await;
+    let db = conf.migrator.db().clone();
+    let fm = conf.migrator.file_manager();
+
+    conf.generate_test_migrations_arbitrary(89, reversible.into())
+        .await;
+
+    let migrations = |status| async move {
+        async move {
+            if reversible {
+                fm.two_way().list_migrations(db, status, mode).await
+            } else {
+                fm.one_way().list_migrations(db, status, mode).await
+            }
+        }
+        .await
+        .unwrap()
+    };
+
+    conf.run_up(&FastForwardDelta::builder().number(32).build())
+        .await;
+
+    assert_eq!(migrations.clone()(Status::All).await.len(), 89);
+    assert_eq!(migrations.clone()(Status::Pending).await.len(), 89 - 32);
+    assert_eq!(migrations(Status::Applied).await.len(), 32);
+}
+
+#[test_case(Mode::Strict, true; "Reversible Strict")]
+#[test_case(Mode::Lax, true; "Reversible Lax")]
+#[test_case(Mode::Strict, false; "Non-Reversible Strict")]
+#[test_case(Mode::Lax, false; "Non-Reversible Lax")]
+#[tokio::test]
+async fn test_migration_listing_is_idempotent_and_does_not_break_migration_state(
+    mode: Mode,
+    reversible: bool,
+) {
     let migration_dir = tempdir().expect("Failed to create temp directory");
     let migration_dir = &migration_dir.path().join("migrations-tests");
     let mut conf = TestConfigNew::new(mode, migration_dir, current_function!()).await;
@@ -48,7 +86,6 @@ async fn test_can_list(mode: Mode, reversible: bool) {
     .await;
 
     conf.run_list(Status::All).await;
-    // access the stdout buffer
     conf.run_list(Status::Pending).await;
     conf.run_list(Status::Applied).await;
 
