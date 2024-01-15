67c07604f78e5bab69d65b0bcdf2b6e830a595c8
oyelowo <oyelowooyedayo@gmail.com>
2024-01-14 17:40:26 -0800

    Support generics in object

--- derive/src/models/object.rs
+++ derive/src/models/object.rs
@@ -44,9 +44,11 @@ impl ToTokens for ObjectToken {
         let ObjectToken {
             ident: struct_name_ident,
             data,
+            generics,
             rename_all,
             ..
         } = &self;
+        let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
         let struct_level_casing = rename_all.as_ref().map(|case| {
             CaseString::from_str(case.serialize.as_str()).expect("Invalid casing, The options are")
@@ -117,7 +119,7 @@ impl ToTokens for ObjectToken {
         tokens.extend(quote!(
             use #crate_name::{ToRaw as _};
 
-            impl #crate_name::SchemaGetter for #struct_name_ident {
+            impl #impl_generics #crate_name::SchemaGetter for #struct_name_ident #ty_generics #where_clause {
                 type Schema = #module_name_internal::#struct_name_ident;
 
                 fn schema() -> #module_name_rexported::Schema {
@@ -129,7 +131,7 @@ impl ToTokens for ObjectToken {
                 }
             }
 
-            impl #crate_name::Object for #struct_name_ident {
+            impl #impl_generics #crate_name::Object for #struct_name_ident #ty_generics #where_clause {
                 // type Schema = #module_name::#struct_name_ident;
                 // type NonNullUpdater = #module_name::#non_null_updater_struct_name;
                 type NonNullUpdater = #non_null_updater_struct_name;
@@ -185,52 +187,52 @@ impl ToTokens for ObjectToken {
                 pub type #struct_name_ident = #_____schema_def::Schema;
 
 
-                impl #crate_name::Parametric for #struct_name_ident {
+                impl #crate_name::Parametric for #struct_name_ident #ty_generics #where_clause {
                     fn get_bindings(&self) -> #crate_name::BindingsList {
                         self.#___________bindings.to_vec()
                     }
                 }
 
-                impl #crate_name::Buildable for #struct_name_ident {
+                impl #crate_name::Buildable for #struct_name_ident #ty_generics #where_clause {
                     fn build(&self) -> ::std::string::String {
                         self.#___________graph_traversal_string.to_string()
                     }
                 }
 
-                impl #crate_name::Erroneous for #struct_name_ident {
+                impl #crate_name::Erroneous for #struct_name_ident #ty_generics #where_clause {
                     fn get_errors(&self) -> ::std::vec::Vec<::std::string::String> {
                         self.#___________errors.to_vec()
                     }
                 }
 
-                impl ::std::fmt::Display for #struct_name_ident {
+                impl ::std::fmt::Display for #struct_name_ident #ty_generics #where_clause {
                     fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
                         f.write_fmt(format_args!("{}", self.#___________graph_traversal_string))
                     }
                 }
 
-                impl #crate_name::Aliasable for &#struct_name_ident {}
+                impl #crate_name::Aliasable for &#struct_name_ident #ty_generics #where_clause {}
 
-                impl #crate_name::Parametric for &#struct_name_ident {
+                impl #crate_name::Parametric for &#struct_name_ident #ty_generics #where_clause {
                     fn get_bindings(&self) -> #crate_name::BindingsList {
                         self.#___________bindings.to_vec()
                     }
                 }
 
-                impl #crate_name::Buildable for &#struct_name_ident {
+                impl #crate_name::Buildable for &#struct_name_ident #ty_generics #where_clause {
                     fn build(&self) -> ::std::string::String {
                         self.#___________graph_traversal_string.to_string()
                     }
                 }
 
-                impl #crate_name::Erroneous for &#struct_name_ident {
+                impl #crate_name::Erroneous for &#struct_name_ident #ty_generics #where_clause {
                     fn get_errors(&self) -> ::std::vec::Vec<::std::string::String> {
                         self.#___________errors.to_vec()
                     }
                 }
 
 
-                impl #struct_name_ident {
+                impl #struct_name_ident #ty_generics #where_clause {
                     pub fn new() -> Self {
                         Self {
                            #( #schema_struct_fields_names_kv) *
@@ -312,10 +314,7 @@ impl ToTokens for ObjectToken {
 }
 
 pub fn generate_fields_getter_trait(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    // Construct a representation of Rust code as a syntax tree
-    // that we can manipulate
     let input = parse_macro_input!(input);
-    // let output = FieldsGetterOpts::from_derive_input(&input).expect("Wrong options");
     let output = match ObjectToken::from_derive_input(&input) {
         Ok(out) => out,
         Err(err) => return proc_macro::TokenStream::from(err.write_errors()),
