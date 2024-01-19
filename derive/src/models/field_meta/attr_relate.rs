1fefa7cc85af052611c38422349321da1c244a38
oyelowo <oyelowooyedayo@gmail.com>
2024-01-18 20:46:14 -0800

    Export field and table meta

--- derive/src/models/field_meta/attr_relate.rs
+++ derive/src/models/field_meta/attr_relate.rs
@@ -1,3 +1,37 @@
+#![allow(missing_docs)]
+#![allow(dead_code)]
+/*
+ * Author: Oyelowo Oyedayo
+ * Email: oyelowo.oss@gmail.com
+ * Copyright (c) 2023 Oyelowo Oyedayo
+ * Licensed under the MIT license
+ */
+
+use std::{
+    fmt::{Display, Formatter},
+    ops::Deref,
+};
+
+use crate::models::{
+    replace_lifetimes_with_underscore, replace_self_in_type_str, GenericTypeExtractor,
+};
+
+use super::{
+    casing::{CaseString, FieldIdentCased, FieldIdentUnCased},
+    errors::ExtractorResult,
+    field_rust_type::DbFieldTypeManager,
+    get_crate_name, parse_lit_to_tokenstream,
+    parser::DataType,
+    relations::{NodeType, NodeTypeName},
+    variables::VariablesModelMacro,
+    FieldNameNormalized,
+};
+use darling::{ast::Data, util, FromDeriveInput, FromField, FromMeta, ToTokens};
+use proc_macro2::TokenStream;
+use quote::{format_ident, quote};
+use surreal_query_builder::FieldType;
+use syn::{Generics, Ident, Lit, LitStr, Path, Type};
+
 #[derive(Debug, Clone)]
 pub struct Relate {
     /// e.g ->writes->book
