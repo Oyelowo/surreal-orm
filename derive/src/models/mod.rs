3380ed8576f494a83cf8be40e82f74e854ff617f
oyelowo <oyelowooyedayo@gmail.com>
2024-01-22 21:49:58 -0800

    WIP: Begin refactoring relate attribute's node_edge link/connection methods token generator. Suspend to focus on others in the meantime

--- derive/src/models/mod.rs
+++ derive/src/models/mod.rs
@@ -9,7 +9,6 @@ pub(crate) mod casing;
 pub(crate) mod edge;
 pub(crate) mod errors;
 pub(crate) mod field_meta;
-pub(crate) mod model_metadata;
 pub(crate) mod object;
 pub(crate) mod parser;
 pub(crate) mod table_meta;
@@ -18,7 +17,6 @@ pub(crate) mod variables;
 
 pub use casing::*;
 pub use field_meta::*;
-pub use model_metadata::*;
 pub use parser::*;
 pub use table_meta::*;
 pub(crate) use utils::*;
