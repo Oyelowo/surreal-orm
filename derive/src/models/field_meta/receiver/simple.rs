087ca0f7aca16ae868c39558da0897012f92a2a0
oyelowo <oyelowooyedayo@gmail.com>
2024-02-12 15:28:41 +0200

    Refactor and colocate simple/basic field meta aggregation

--- derive/src/models/field_meta/receiver/simple.rs
+++ derive/src/models/field_meta/receiver/simple.rs
@@ -1,6 +1,9 @@
 use quote::quote;
 
-use crate::models::{derive_attributes::TableDeriveAttributes, field_name_serialized, FieldsMeta};
+use crate::models::{
+    derive_attributes::TableDeriveAttributes, field_name_serialized,
+    variables::VariablesModelMacro, FieldsMeta,
+};
 
 use super::MyFieldReceiver;
 
@@ -31,5 +34,46 @@ impl MyFieldReceiver {
         }
 
         Ok(())
+    }
+
+    pub fn create_simple_meta(
+        &self,
+        store: &mut FieldsMeta,
+        table_derive_attrs: &TableDeriveAttributes,
+    ) -> ExtractorResult<()> {
+        // TODO: Decide if to use table_derive_attrs or store.table_derive_attributes()
+        // let table_derive_attrs = store.table_derive_attributes();
+        let field_ident_normalized = self.field_ident_normalized(&table_derive_attrs.casing()?)?;
+        let field_name_pascalized = self.field_name_pascalized(table_derive_attrs);
+        let field_name_serialized = self.field_name_serialized(&table_derive_attrs.casing()?)?;
+        let VariablesModelMacro {
+            _____field_names,
+            schema_instance,
+            ..
+        } = VariablesModelMacro::new();
+
+        store.schema_struct_fields_types_kv.push(
+            quote!(pub #field_ident_normalized: #_____field_names::#field_name_pascalized, ).into(),
+        );
+
+        store
+            .schema_struct_fields_names_kv
+            .push(quote!(#field_ident_normalized: #field_name_serialized.into(),).into());
+
+        store.schema_struct_fields_names_kv_prefixed
+                        .push(quote!(#field_ident_normalized:
+                                            #crate_name::Field::new(format!("{}.{}", prefix.build(), #field_name_serialized))
+                                            .with_bindings(prefix.get_bindings()).into(),).into());
+
+        store
+            .schema_struct_fields_names_kv_empty
+            .push(quote!(#field_ident_normalized: "".into(),).into());
+
+        store.connection_with_field_appended
+           .push(quote!(
+                                #schema_instance.#field_ident_normalized = #schema_instance.#field_ident_normalized
+                                  .set_graph_string(format!("{}.{}", #___________graph_traversal_string, #field_name_serialized))
+                                        .#____________update_many_bindings(#bindings).into();
+                            ).into());
     }
 }
