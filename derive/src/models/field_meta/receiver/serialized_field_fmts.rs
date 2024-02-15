cf58570d9ff4991733c773feb44646642a4f7f44
oyelowo <oyelowooyedayo@gmail.com>
2024-02-15 18:20:13 +0200

    Refactor: Update serialized fields tokens collation

--- derive/src/models/field_meta/receiver/serialized_field_fmts.rs
+++ derive/src/models/field_meta/receiver/serialized_field_fmts.rs
@@ -1,48 +1,44 @@
-use proc_macro::TokenStream;
 use proc_macro2::TokenStream;
 use quote::quote;
 
 use crate::{
     errors::ExtractorResult,
-    models::{derive_attributes::TableDeriveAttributes, FieldsMeta, Relate, RelationType},
+    models::{
+        derive_attributes::TableDeriveAttributes, FieldsMeta, Relate, RelationType,
+        SerializableField,
+    },
 };
 
 use super::{EdgeRelationModelSelectedIdent, MyFieldReceiver};
 
-impl MyFieldReceiver {
-    pub fn create_db_fields_for_links_and_loaders(
-        &self,
-        store: &mut FieldsMeta,
-        table_derive_attrs: &TableDeriveAttributes,
-    ) -> ExtractorResult<()> {
-        let relation_type = self.to_relation_type();
-        let db_field_name = self.db_field_name(&table_derive_attrs.casing()?)?;
+impl FieldsMeta {
+    pub fn create_db_fields_for_links_and_loaders(&mut self) -> ExtractorResult<()> {
+        let table_derive_attrs = self.table_derive_attributes();
+        let field_receiver = self.field_receiver();
+        let db_field_name = field_receiver.db_field_name(&table_derive_attrs.casing()?)?;
 
         let serialized_field_fmt = quote!(#crate_name::Field::new(#db_field_name));
 
-        &mut store
-            .serialized_fmt_db_field_names_instance
+        self.serialized_fmt_db_field_names_instance
             .push(serialized_field_fmt.into());
 
-        if !self.skip_serializing && !self.skip {
-            match relation_type {
+        if !field_receiver.skip_serializing && !field_receiver.skip {
+            match field_receiver.to_relation_type() {
                 RelationType::LinkOne(_) => {
-                    store.link_one_fields.push(serialized_field_fmt.into());
-                    store
-                        .link_one_and_self_fields
+                    self.link_one_fields.push(serialized_field_fmt.into());
+                    self.link_one_and_self_fields
                         .push(serialized_field_fmt.into());
-                    store.linked_fields.push(serialized_field_fmt.into());
+                    self.linked_fields.push(serialized_field_fmt.into());
                 }
                 RelationType::LinkSelf(_) => {
-                    store.link_self_fields.push(serialized_field_fmt.into());
-                    store
-                        .link_one_and_self_fields
+                    self.link_self_fields.push(serialized_field_fmt.into());
+                    self.link_one_and_self_fields
                         .push(serialized_field_fmt.into());
-                    store.linked_fields.push(serialized_field_fmt.into());
+                    self.linked_fields.push(serialized_field_fmt.into());
                 }
                 RelationType::LinkMany(_) => {
-                    store.link_many_fields.push(serialized_field_fmt.into());
-                    store.linked_fields.push(serialized_field_fmt.into());
+                    self.link_many_fields.push(serialized_field_fmt.into());
+                    self.linked_fields.push(serialized_field_fmt.into());
                 }
                 _ => {}
             }
