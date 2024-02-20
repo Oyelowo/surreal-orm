c9a8b05dc565f57fb52eac56384adf268e4c452c
oyelowo <oyelowooyedayo@gmail.com>
2024-02-20 19:42:14 +0200

    Update field_metadata module

--- derive/src/models/token_codegen/field_metadata.rs
+++ derive/src/models/token_codegen/field_metadata.rs
@@ -7,18 +7,18 @@
 
 use quote::quote;
 
-use crate::models::{
-    derive_attributes::TableDeriveAttributes, field_name_serialized, variables::VariablesModelMacro,
-};
+use crate::{errors::ExtractorResult, models::*};
 
 use super::Codegen;
 
-impl Codegen {
+impl<'a> Codegen<'a> {
     pub fn create_field_metadata_token(&mut self) -> ExtractorResult<()> {
+        let crate_name = get_crate_name(false);
         let field_receiver = self.field_receiver();
         let table_derive_attrs = self.table_derive_attributes();
         let field_definition = self.field_defintion_db()?;
         let field_name_serialized = field_receiver.db_field_name(&table_derive_attrs.casing()?)?;
+
         let old_field_name = match field_receiver.old_name.as_ref() {
             Some(old_name) if !old_name.to_string().is_empty() => {
                 let old_name = old_name.to_string();
