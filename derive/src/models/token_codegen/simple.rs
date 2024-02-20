2eecb3fc2a39213c9d04306194433e159aad7070
oyelowo <oyelowooyedayo@gmail.com>
2024-02-20 20:05:32 +0200

    Fix table name, aliases and simple code gen modules

--- derive/src/models/token_codegen/simple.rs
+++ derive/src/models/token_codegen/simple.rs
@@ -7,22 +7,17 @@
 
 use quote::quote;
 
-use crate::{
-    errors::ExtractorResult,
-    models::{
-        count_vec_nesting, derive_attributes::TableDeriveAttributes, field_name_serialized,
-        generate_nested_vec_type, variables::VariablesModelMacro, RelationType,
-    },
-};
+use crate::{errors::ExtractorResult, models::*};
 
 use super::Codegen;
 
-impl Codegen {
+impl<'a> Codegen<'a> {
     pub fn create_table_id_type_token(&mut self) -> ExtractorResult<()> {
-        let fr = self.field_receiver();
+        let field_receiver = self.field_receiver();
         let table_derive_attrs = self.table_derive_attributes();
-        let field_ident_serialized_fmt = fr.db_field_name(&table_derive_attrs.casing()?)?;
-        let field_type = &fr.field_type_rust();
+        let field_ident_serialized_fmt =
+            field_receiver.db_field_name(&table_derive_attrs.casing()?)?;
+        let field_type = &field_receiver.field_type_rust();
 
         if field_ident_serialized_fmt.is_id() {
             self.table_id_type = quote!(#field_type);
@@ -33,12 +28,12 @@ impl Codegen {
     }
 
     pub fn create_db_field_names_token(&mut self) -> ExtractorResult<()> {
-        let field_db_field_name = self
+        let db_field_name = self
             .field_receiver()
             .db_field_name(&self.table_derive_attributes().casing()?)?;
 
         self.serialized_field_names_normalised
-            .push(field_db_field_name.to_owned().into());
+            .push(db_field_name.to_owned().into());
 
         Ok(())
     }
