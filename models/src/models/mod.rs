b05d129bb46f806e422d8233a474ae79bd618c16
oyelowo <oyelowooyedayo@gmail.com>
2024-07-04 16:52:10 +0300

    unleash old user models module without generics. Now, have examples that dont use generics and one that uses fairly complex ones

--- models/src/models/mod.rs
+++ models/src/models/mod.rs
@@ -8,10 +8,11 @@
 // pub mod alien;
 // pub mod attributes;
 // pub mod combined;
-// pub mod configuration;
+pub mod configuration;
 pub mod planet;
 pub mod spaceship;
 pub mod user;
+pub mod user_with_generics;
 // pub mod visits;
 pub mod weapon;
 pub mod with_generics;
