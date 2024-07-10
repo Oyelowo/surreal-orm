2decb37d00065c3db4459deb3c970936519e1e64
oyelowo <oyelowooyedayo@gmail.com>
2024-07-10 15:54:51 +0300

    Remove unused code

--- derive/src/models/mod.rs
+++ derive/src/models/mod.rs
@@ -8,25 +8,3 @@
 pub mod edge;
 pub mod node;
 pub mod object;
-
-
-#[derive(Debug, Copy, Clone)]
-pub enum DataType {
-    Node,
-    Edge,
-    Object,
-}
-
-impl DataType {
-    pub fn is_node_or_edge(&self) -> bool {
-        matches!(self, Self::Node | Self::Edge)
-    }
-
-    pub fn is_object(&self) -> bool {
-        matches!(self, Self::Object)
-    }
-
-    pub fn is_edge(&self) -> bool {
-        matches!(self, Self::Edge)
-    }
-}
