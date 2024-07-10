4de3fc14a34b85a994e3f8d25c0eec2f52dfe552
oyelowo <oyelowooyedayo@gmail.com>
2024-07-10 12:51:20 +0300

    Test cmmon attributes of node and edge

--- derive_helpers/tests/generate_test.rs
+++ derive_helpers/tests/generate_test.rs
@@ -1,14 +1,26 @@
 
-
+use quote::{format_ident, quote, ToTokens};
 use darling::FromDeriveInput;
-use quote::{quote, ToTokens};
 use surreal_derive_helpers::models::NodeToken;
+use test_case::test_case;
+
+enum ModelType {
+    Node,
+    Edge,
+}
+use ModelType::*;
 
 // General test for the Node derive macro
-#[test]
-fn test_node_trait_derive() {
+#[test_case(Node ; "node model common attributes")]
+#[test_case(Edge ; "edge model common attributes")]
+fn test_node_trait_derive(model_type: ModelType) {
+    let model_type = match model_type {
+        Node => quote!(Node),
+        Edge => quote!(Edge),
+    };
+
     let input = quote!(
-        #[derive(Node)]
+        #[derive(#model_type)]
         #[surreal_orm(table = "student", drop, schemafull, permissions = perm)]
         pub struct Student {
             id: SurrealSimpleId<Self>,
@@ -29,7 +41,7 @@ fn test_node_trait_derive() {
     let derive_input = syn::parse2(input).unwrap();
     let node_token = NodeToken::from_derive_input(&derive_input).unwrap();
     insta::assert_snapshot!(
-        "node_trait_derive",
+        format!("{model_type}_trait_derive"),
         format!("{:#}", node_token.to_token_stream())
     );
 }
