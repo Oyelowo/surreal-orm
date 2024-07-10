d55b75b3918799de63540d4174df120c096eac14
oyelowo <oyelowooyedayo@gmail.com>
2024-07-10 12:57:18 +0300

    Add cases for edge

--- derive_helpers/tests/table_name.rs
+++ derive_helpers/tests/table_name.rs
@@ -65,23 +65,48 @@ impl Display for TableNameFormat {
     }
 }
 use TableNameFormat::*;
+enum ModelType {
+    Node,
+    Edge,
+}
+use ModelType::*;
 
-#[test_case("student_table", Raw, NoRelax, Valid; "snake case raw table name with struct name even without relax")]
-#[test_case("student_table", Raw, Relax, Valid; "snake case raw table name with struct name with relax")]
-#[test_case("student_table", Stringified, NoRelax, Valid; "snake case stringified table name version of struct name cannot be used without relax")]
-#[test_case("student_table", Stringified, Relax, Valid; "snake case stringified table name version of struct name can be used with relax")]
-#[test_case("snake_case_but_wrong_name", Stringified, NoRelax, Invalid; "snake case different from even snake case version of struct name cannot be used with relax")]
-#[test_case("snake_case_but_wrong_name", Stringified, Relax, Valid; "snake case different from even snake case version of struct name can be used with relax")]
-#[test_case("StudentTable",  Raw, NoRelax, Invalid; "non snake case raw table name version of struct name cannot be used without relax")]
-#[test_case("StudentTable",  Raw, Relax, Valid; "non snake case raw table name version of struct name cann be used with relax")]
-#[test_case("StudentTable",  Stringified, NoRelax, Invalid; "non snake case stringified table name version of struct name cannot be used without relax")]
-#[test_case("StudentTable",  Stringified, Relax, Valid; "non snake case stringified table name version of struct name cann be used with relax")]
+#[test_case(Node, "student_table", Raw, NoRelax, Valid; "Node table snake case raw table name with struct name even without relax")]
+#[test_case(Node, "student_table", Raw, Relax, Valid; "Node table snake case raw table name with struct name with relax")]
+#[test_case(Node, "student_table", Stringified, NoRelax, Valid; "Node table snake case stringified table name version of struct name cannot be used without relax")]
+#[test_case(Node, "student_table", Stringified, Relax, Valid; "Node table snake case stringified table name version of struct name can be used with relax")]
+
+#[test_case(Edge, "student_table", Raw, NoRelax, Valid; "Edge table's snake case raw table name with struct name even without relax")]
+#[test_case(Edge, "student_table", Raw, Relax, Valid; "Edge table's snake case raw table name with struct name with relax")]
+#[test_case(Edge, "student_table", Stringified, NoRelax, Valid; "Edge table's snake case stringified table name version of struct name cannot be used without relax")]
+#[test_case(Edge, "student_table", Stringified, Relax, Valid; "Edge table's snake case stringified table name version of struct name can be used with relax")]
+
+#[test_case(Node, "snake_case_but_wrong_name", Stringified, NoRelax, Invalid; "snake case different from even snake case version of struct name cannot be used with relax")]
+#[test_case(Node, "snake_case_but_wrong_name", Stringified, Relax, Valid; "snake case different from even snake case version of struct name can be used with relax")]
+
+#[test_case(Edge, "snake_case_but_wrong_name", Stringified, NoRelax, Invalid; "Edge table's snake case different from even snake case version of struct name cannot be used with relax")]
+#[test_case(Edge, "snake_case_but_wrong_name", Stringified, Relax, Valid; "Edge table's snake case different from even snake case version of struct name can be used with relax")]
+
+#[test_case(Node, "StudentTable",  Raw, NoRelax, Invalid; "non snake case raw table name version of struct name cannot be used without relax")]
+#[test_case(Node, "StudentTable",  Raw, Relax, Valid; "non snake case raw table name version of struct name cann be used with relax")]
+#[test_case(Node, "StudentTable",  Stringified, NoRelax, Invalid; "non snake case stringified table name version of struct name cannot be used without relax")]
+#[test_case(Node, "StudentTable",  Stringified, Relax, Valid; "non snake case stringified table name version of struct name cann be used with relax")]
+
+#[test_case(Edge, "StudentTable",  Raw, NoRelax, Invalid; "Edge table's non snake case raw table name version of struct name cannot be used without relax")]
+#[test_case(Edge, "StudentTable",  Raw, Relax, Valid; "Edge table's non snake case raw table name version of struct name cann be used with relax")]
+#[test_case(Edge, "StudentTable",  Stringified, NoRelax, Invalid; "Edge table's non snake case stringified table name version of struct name cannot be used without relax")]
+#[test_case(Edge, "StudentTable",  Stringified, Relax, Valid; "Edge table's non snake case stringified table name version of struct name cann be used with relax")]
 fn test_table_name(
+    model_type: ModelType,
     table_name: &str,
     table_name_format: TableNameFormat,
     relaxation: RelaxTable,
     validity: Validity,
 ) {
+    let model_type = match model_type {
+        Node => quote!(Node),
+        Edge => quote!(Edge),
+    };
     let relax_table = match relaxation {
         Relax => quote!(, relax_table),
         NoRelax => quote!(),
@@ -94,7 +119,7 @@ fn test_table_name(
         Stringified => quote!(#table_name),
     };
     let input = quote!(
-        #[derive(Node)]
+        #[derive(#model_type)]
         #[surreal_orm(table = #table_name #relax_table)]
         pub struct StudentTable {
             id: SurrealSimpleId<Self>,
@@ -105,7 +130,7 @@ fn test_table_name(
     let node_token = NodeToken::from_derive_input(&derive_input).unwrap();
 
     insta::assert_snapshot!(
-        format!("node_table_name_tests-{table_name}-{table_name_format}-{relaxation}-{validity}"),
+        format!("{model_type}_table_name_tests-{table_name}-{table_name_format}-{relaxation}-{validity}"),
         format!("{:#}", node_token.to_token_stream())
     );
 
