32c3624c1d479c3a0496aeba0dc41fc3901e3a64
oyelowo <oyelowooyedayo@gmail.com>
2024-07-11 17:56:09 +0300

    Only require skip_serializing attribute and not default for relation fields

--- derive_helpers/src/models/field_meta/derive_attributes/attributes.rs
+++ derive_helpers/src/models/field_meta/derive_attributes/attributes.rs
@@ -9,14 +9,13 @@ impl MyFieldReceiver {
 
     pub fn validate_attributes(&self) -> ExtractorResult<()> {
         if self.relate.is_some() {
-            let has_required_attrs = self.default && self.skip_serializing;
-            if !has_required_attrs {
+            if !self.skip_serializing {
                 let field_name = self.ident()?.to_string();
                 return Err(syn::Error::new(
                     self.ident()?.span(),
                         format!(
-                            "Missing required serde attribute on `{field_name}. set `#[serde(skip_serializing, default)]` on the field.
-                                This is because this field is a readonly relate field",
+                            "Missing required serde attribute on `{field_name}. set `#[serde(skip_serializing)]` on the field. \
+                                \nThis is because this field is a readonly derived relational field and we don't want to store it in the data"
                         ),
                     )
                     .into());
