1f0a84bf3f4d56e9d90eeb9d848ac04eedb77aff
oyelowo <oyelowooyedayo@gmail.com>
2024-07-11 18:19:54 +0300

    Updata and test readonly relational fields

--- derive_helpers/tests/generate_test.rs
+++ derive_helpers/tests/generate_test.rs
@@ -20,6 +20,7 @@ fn test_node_trait_derive() {
             all_semester_courses: LinkMany<Book>,
 
             #[surreal_orm(relate(model = "StudentWritesBlog", connection = "->writes->Blog"))]
+            #[serde(skip_serializing)]
             written_blogs: Relate<Blog>,
         }
     );
@@ -34,6 +35,35 @@ fn test_node_trait_derive() {
         "node_trait_derive",
         format!("{:#}", node_token.to_token_stream())
     );
+}
+
+#[test]
+fn test_relate_field_must_have_serde_skip_serializing_attribute() {
+    let input = quote!(
+        #[derive(Node)]
+        #[surreal_orm(table = "student", drop, schemafull, permissions = perm)]
+        pub struct Student {
+            id: SurrealSimpleId<Self>,
+            first_name: String,
+
+            #[surreal_orm(link_one = Book)]
+            course: LinkOne<Book>,
+
+            #[surreal_orm(link_many = Book)]
+            #[serde(rename = "lowo")]
+            all_semester_courses: LinkMany<Book>,
+
+            #[surreal_orm(relate(model = "StudentWritesBlog", connection = "->writes->Blog"))]
+            written_blogs: Relate<Blog>,
+        }
+    );
+
+    let derive_input = syn::parse2(input).unwrap();
+    let node_token = NodeToken::from_derive_input(&derive_input).unwrap();
+    let node_token = node_token.to_token_stream().to_string();
+
+    assert!(node_token.contains("Missing required serde attribute on `written_blogs`"));
+    assert_not!(node_token.contains("impl surreal_orm :: Node for Student "));
 }
 
 #[test]
